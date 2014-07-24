713d217cef49796216679657490c75d0e4847a10
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2014-07-24 08:13:15 +0200

    Adding permalinks

--- packages/clay/lib/handle_nav.js
+++ packages/clay/lib/handle_nav.js
@@ -15,22 +15,83 @@ var toTitleCase = Y.cached(
 	}
 );
 
+var TOKEN_DOT = '\u2024';
+
+var setRootPath = function(file) {
+	var fullPath = file.path;
+	var rootPath = '.';
+
+	if (fullPath) {
+		rootPath = path.relative(fullPath, '.');
+	}
+
+	file.rootPath = rootPath;
+
+	return file;
+};
+
+var getNavItem = function(filePath, file) {
+	var title = file.title || toTitleCase(path.basename(filePath, '.html'));
+
+	return {
+		toTitleCase: toTitleCase,
+		title: title
+	};
+};
+
+var iterateFiles = function(files, metalsmith, done) {
+	var nav = {};
+	var navData = {};
+
+	metalsmith.metadata().nav = nav;
+
+	// for (var i in files) {files[i].rootPath = '';}return;
+
+	metalsmith.metadata().navData = navData;
+
+	for (var i in files) {
+		var file = files[i];
+		if (file.template) {
+			var title = file.title || toTitleCase(path.basename(i, '.html'));
+
+			var parent = nav;
+			var navTitle = title;
+
+			var fullPath = file.path;
+			var rootPath = '.';
+
+			if (fullPath) {
+				rootPath = path.relative(fullPath, '.');
+				// path.replace(/[^\/]+/g, '..')
+			}
+
+			file.rootPath = rootPath;
+// console.log(file.path);
+			var section = path.dirname(i).replace('content/', '');
+
+			var formattedSections = section.split('/');
+
+			if (formattedSections.length > 1) {
+				navTitle = toTitleCase(formattedSections.pop());
+
+				formattedSections = formattedSections.map(
+					function(item, index) {
+						return toTitleCase(item.replace(/\./g, TOKEN_DOT));
+					}
+				).join('.');
+
+				parent = Y.namespace.call(nav, formattedSections);
+			}
+
+			parent[navTitle] = i;
+
+			navData[i] = file;
+		}
+	}
+};
+
 module.exports = function() {
 	return function(files, metalsmith, done) {
-		var nav = {};
-
-		metalsmith.metadata().nav = nav;
-
-		for (var i in files) {
-			var file = files[i];
-
-			if (file.template) {
-				var title = file.title || toTitleCase(path.basename(i, '.html'));
-
-				nav[title] = i;
-			}
-		}
-
+		iterateFiles(files, metalsmith, done);
 		done();
-	}
 };
