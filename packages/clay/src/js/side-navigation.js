2742777c2b2ba2cad4205d901d65cd56ab40d7cf
Patrick Yeo <patrick.yeo@liferay.com>
2015-02-27 14:53:22 -0800

    Add equal height to side-navigation plugin

--- packages/clay/src/js/side-navigation.js
+++ packages/clay/src/js/side-navigation.js
@@ -1,58 +1,89 @@
 +function($) {
-	$.fn.sideNavigation = function(options) {
-		var defaults = {
-			content: '.sidenav-content',
-			navigation: '.sidenav-menu-slider',
-			toggle: '.sidenav-toggler'
-		};
+	var SideNavigation = function(element, options) {
+		this.init(element, options);
+	};
 
-		var sidenavs = $(this);
+	SideNavigation.prototype = {
+		init: function(element, options) {
+			this.options = $.extend($.fn.sideNavigation.defaults, options);
 
-		options = $.extend(defaults, options);
+			this.options.selector = element.selector;
 
-		removeEqualHeight = function(element1, element2) {
-			element1.css('min-height', '');
-			element2.css('min-height', '');
-		};
+			this._handleClick(element);
+		},
 
-		setEqualHeight = function(element1, element2) {
-			var tallest = Math.max(element1.height(), element2.height());
+		_handleClick: function(element) {
+			var instance = this;
 
-			element1.css('min-height', tallest);
-			element2.css('min-height', tallest);
-		};
-
-		toggleNavigation = function(container) {
-			if (container.hasClass('closed')) {
-				container.removeClass('closed').addClass('sidenav-transition');
-			}
-			else {
-				container.addClass('closed').addClass('sidenav-transition');
-			}
-		};
-
-		sidenavs.find(options.toggle).on(
-			'click',
-			function(event) {
-				var container = $(this).closest(sidenavs.selector);
-				var content = container.find(options.content);
-				var navigation = container.find(options.navigation);
+			element.find('.sidenav-toggler').on('click', function(event) {
+				var container = $(this).closest(instance.options.selector);
+				var content = container.find(instance.options.content);
+				var navigation = container.find(instance.options.navigation);
 				var transitions = 'webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend';
 
 				container.one(transitions, function(event) {
 					container.removeClass('sidenav-transition');
 
 					if (container.hasClass('closed')) {
-						removeEqualHeight(navigation, content);
+						instance.removeEqualHeight(navigation, content);
 					}
 				});
 
-				toggleNavigation(container);
+				instance.toggleNavigation(container);
 
-				setEqualHeight(navigation, content);
+				instance.setEqualHeight(navigation, content, container);
+			});
+		},
+
+		removeEqualHeight: function(element1, element2) {
+			element1.css('min-height', '');
+			element2.css('min-height', '');
+		},
+
+		setEqualHeight: function(element1, element2, container) {
+			var containerClone = container.clone();
+			var element1;
+			var element2;
+			var tallest;
+
+			containerClone.removeClass('closed').css({
+				opacity: 0,
+				position: 'absolute'
+			});
+
+			containerClone.insertBefore(container);
+
+			element1 = containerClone.find(this.options.navigation);
+			element2 = containerClone.find(this.options.content);
+			tallest = Math.max(element1.height(), element2.height());
+
+			containerClone.remove();
+
+			container.find(this.options.navigation).css('min-height', tallest);
+			container.find(this.options.content).css('min-height', tallest);
+		},
+
+		toggleNavigation: function(container) {
+			if (container.hasClass('closed')) {
+				container.removeClass('closed').addClass('sidenav-transition');
 			}
-		);
+			else {
+				container.addClass('closed').addClass('sidenav-transition');
+			}
+		}
+	};
+
+	$.fn.sideNavigation = function(options) {
+		new SideNavigation(this, options);
 
 		return this;
-	};
+	}
+
+	$.fn.sideNavigation.defaults = {
+		content: '.sidenav-content',
+		navigation: '.sidenav-menu-slider',
+		toggle: '.sidenav-toggler'
+	}
+
+	$.fn.sideNavigation.Constructor = SideNavigation;
 }(jQuery);
