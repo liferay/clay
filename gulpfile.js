1aa2525c96fde1b202a4de3ec0c9956b1123b767
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2015-06-24 14:17:26 -0700

    Adding initial gulp tasks to patch the bootstrap variables file to include lexicon last

--- gulpfile.js
+++ gulpfile.js
@@ -32,10 +32,43 @@ var REGEX_SCSS = /^scss\//;
 
 var SRC_GLOB = 'src/**/*';
 
+var BOOTSTRAP_VAR_FILE = path.join('src','scss', 'bootstrap', '_variables.scss');
+
+var BOOTSTRAP_VAR_DIR = path.dirname(BOOTSTRAP_VAR_FILE);
+
 gulp.task('default', ['build']);
 
-gulp.task('build', function() {
+gulp.task('build', function(cb) {
+	runSequence(
+		'build:patch-bootstrap',
+		'build:metalsmith',
+		'build:clean-bootstrap-patch',
+		cb
+	);
+});
+
+var LEXICON_IMPORT = '\n\n@import "../lexicon/variables";';
+
+var patchBootstrap = function(content) {
+	content += LEXICON_IMPORT;
+
+	return content;
+};
+
+var cleanBootstrapPatch = function(content) {
+	return content.replace(LEXICON_IMPORT, '');
+};
+
+gulp.task('build:patch-bootstrap', function(cb) {
+	return gulp.src(BOOTSTRAP_VAR_FILE)
+	.pipe(plugins.change(patchBootstrap))
+	.pipe(gulp.dest(BOOTSTRAP_VAR_DIR));
+});
+
+gulp.task('build:metalsmith', function(cb) {
 	var filter = plugins.filter(['**/*.md', '**/*.html']);
+
+	var REGEX_VAR_FILEPATH = new RegExp(BOOTSTRAP_VAR_FILE + '$');
 
 	return gulp.src(SRC_GLOB)
 			.pipe(filter)
@@ -83,6 +116,13 @@ gulp.task('build', function() {
 					.use(templates('ejs'))
 			)
 			.pipe(gulp.dest('./build'));
+
+});
+
+gulp.task('build:clean-bootstrap-patch', function(cb) {
+	return gulp.src(BOOTSTRAP_VAR_FILE)
+	.pipe(plugins.change(cleanBootstrapPatch))
+	.pipe(gulp.dest(BOOTSTRAP_VAR_DIR));
 });
 
 gulp.task('watch', ['build:watch']);
@@ -91,7 +131,7 @@ gulp.task(
 	'build:watch',
 	function(done) {
 		gulp.watch(
-			SRC_GLOB,
+			[SRC_GLOB, '!src/scss/bootstrap/_variables.scss'],
 			['build']
 		);
 	}
@@ -101,9 +141,11 @@ gulp.task(
 	'release',
 	function(cb) {
 		runSequence(
+			'build:patch-bootstrap',
 			'release:clean',
 			'release:build',
 			'release:zip',
+			'build:clean-bootstrap-patch',
 			cb
 		);
 	}
