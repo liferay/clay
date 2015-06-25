72487aeb08f964c7f0203a97e569d4a4b6b86a33
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2015-06-25 10:10:12 -0700

    Splitting tasks up into individual files to make it easier to manage

--- packages/clay/gulpfile.js
+++ packages/clay/gulpfile.js
@@ -1,40 +1,24 @@
-var gulp = require('gulp-help')(require('gulp'));
-var plugins = require('gulp-load-plugins')();
-var gulpsmith = require('gulpsmith');
-var gulp_front_matter = require('gulp-front-matter');
-var runSequence = require('run-sequence');
-
 var path = require('path');
 
-var bourbon = require('node-bourbon');
-
-var define = require('metalsmith-define');
-var encodeHTML = require('metalsmith-encode-html');
-var markdown = require('metalsmith-markdown');
-var permalinks = require('metalsmith-permalinks');
-var sass = require('metalsmith-sass');
-var templates = require('metalsmith-templates');
+var gulp = require('gulp-help')(require('gulp'));
+var plugins = require('gulp-load-plugins')();
+var runSequence = require('run-sequence');
 
 var _ = require('./lib/lodash_utils');
-
-var handleNav = require('./lib/handle_nav');
-var handlePath = require('./lib/handle_path');
-var handlePermalink = require('./lib/handle_permalink');
-var handleTemplate = require('./lib/handle_template');
-
-var metadata = {
-	_: _,
-	heading: '',
-	subHeading: ''
-};
-
-var REGEX_SCSS = /^scss(\/|$)/;
-
-var SRC_GLOB = 'src/**/*';
 
 var BOOTSTRAP_VAR_FILE = path.join('src','scss', 'bootstrap', '_variables.scss');
 
 var BOOTSTRAP_VAR_DIR = path.dirname(BOOTSTRAP_VAR_FILE);
+
+var config = {
+	BOOTSTRAP_VAR_FILE: BOOTSTRAP_VAR_FILE,
+	BOOTSTRAP_VAR_DIR: BOOTSTRAP_VAR_DIR,
+	SRC_GLOB: 'src/**/*'
+};
+
+var tasks = require('require-dir')('./tasks');
+
+_.invoke(tasks, 'call', tasks, gulp, plugins, _, config);
 
 gulp.task('default', ['build']);
 
@@ -47,96 +31,6 @@ gulp.task('build', function(cb) {
 	);
 });
 
-var LEXICON_IMPORT = '\n\n@import "../lexicon/variables";';
-
-var patchBootstrap = function(content) {
-	content += LEXICON_IMPORT;
-
-	return content;
-};
-
-var cleanBootstrapPatch = function(content) {
-	return content.replace(LEXICON_IMPORT, '');
-};
-
-gulp.task('build:patch-bootstrap', function(cb) {
-	return gulp.src(BOOTSTRAP_VAR_FILE)
-	.pipe(plugins.change(patchBootstrap))
-	.pipe(gulp.dest(BOOTSTRAP_VAR_DIR));
-});
-
-gulp.task('build:metalsmith', function(cb) {
-	var filter = plugins.filter(['**/*.md', '**/*.html']);
-
-	var REGEX_VAR_FILEPATH = new RegExp(BOOTSTRAP_VAR_FILE + '$');
-
-	return gulp.src(SRC_GLOB)
-			.pipe(filter)
-			.pipe(gulp_front_matter()).on('data', function(file) {
-				_.assign(file, file.frontMatter);
-				delete file.frontMatter;
-			})
-			.pipe(filter.restore())
-			.pipe(
-				gulpsmith()
-					.use(define(metadata))
-					.use(handleTemplate())
-					.use(encodeHTML())
-					.use(
-						markdown(
-							{
-								smartypants: true,
-								gfm: true,
-								tables: true
-							}
-						)
-					)
-					.use(handlePermalink())
-					.use(
-						permalinks(
-							{
-								relative: false
-							}
-						)
-					)
-					.use(handleNav())
-					.use(handlePath())
-					.use(
-						sass(
-							{
-								includePaths: bourbon.includePaths,
-								outputDir: function(dir) {
-									return dir.replace(/^scss(\/|$)/, 'css$1');
-								},
-								outputStyle: 'expanded',
-								sourceMap: true
-							}
-						)
-					)
-					.use(templates('ejs'))
-			)
-			.pipe(gulp.dest('./build'));
-
-});
-
-gulp.task('build:clean-bootstrap-patch', function(cb) {
-	return gulp.src(BOOTSTRAP_VAR_FILE)
-	.pipe(plugins.change(cleanBootstrapPatch))
-	.pipe(gulp.dest(BOOTSTRAP_VAR_DIR));
-});
-
-gulp.task('watch', ['build:watch']);
-
-gulp.task(
-	'build:watch',
-	function(done) {
-		gulp.watch(
-			[SRC_GLOB, '!src/scss/bootstrap/_variables.scss'],
-			['build']
-		);
-	}
-);
-
 gulp.task(
 	'release',
 	function(cb) {
@@ -148,46 +42,5 @@ gulp.task(
 			'build:clean-bootstrap-patch',
 			cb
 		);
-	}
-);
-
-gulp.task(
-	'release:clean',
-	function() {
-		return gulp.src('./release')
-		// .pipe(plugins.debug())
-		.pipe(plugins.clean({read: false}));
-	}
-);
-
-gulp.task(
-	'release:build',
-	function() {
-
-		return gulp.src([
-			// 'src/fonts/**/*',
-			'src/scss/+(atlas-theme|bootstrap|lexicon)/**/*',
-			'src/scss/+(atlas|bootstrap|lexicon).scss'
-			], {base: './src'})
-		// .pipe(plugins.debug())
-		.pipe(
-			plugins.rename(
-				function(path) {
-					path.dirname = path.dirname.replace(REGEX_SCSS, '');
-				}
-			)
-		)
-		.pipe(gulp.dest('./release'));
-	}
-);
-
-gulp.task(
-	'release:zip',
-	function() {
-		var pkg = require('package.json');
-
-		return gulp.src('release/**/*')
-		.pipe(plugins.zip(pkg.name + '-' + pkg.version + '.zip'))
-		.pipe(gulp.dest('.'));
 	}
 );
---
+++ packages/clay/tasks/patch.js
@@ -0,0 +1,25 @@
+module.exports = function(gulp, plugins, _, config) {
+	var LEXICON_IMPORT = '\n\n@import "../lexicon/variables";';
+
+	var patchBootstrap = function(content) {
+		content += LEXICON_IMPORT;
+
+		return content;
+	};
+
+	var cleanBootstrapPatch = function(content) {
+		return content.replace(LEXICON_IMPORT, '');
+	};
+
+	gulp.task('build:patch-bootstrap', function(cb) {
+		return gulp.src(config.BOOTSTRAP_VAR_FILE)
+			.pipe(plugins.change(patchBootstrap))
+			.pipe(gulp.dest(config.BOOTSTRAP_VAR_DIR));
+	});
+
+	gulp.task('build:clean-bootstrap-patch', function(cb) {
+		return gulp.src(config.BOOTSTRAP_VAR_FILE)
+			.pipe(plugins.change(cleanBootstrapPatch))
+			.pipe(gulp.dest(config.BOOTSTRAP_VAR_DIR));
+	});
+};
