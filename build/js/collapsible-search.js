2f227c052c6d2b2b92705a20c343fd1f81ebca5c
Patrick Yeo <patrick.yeo@liferay.com>
2015-07-22 13:16:49 -0700

    Rebuild

--- build/js/collapsible-search.js
+++ build/js/collapsible-search.js
@@ -7,83 +7,142 @@
 		init: function(element, options) {
 			var instance = this;
 
-			instance.options = $.extend({}, $.fn.collapsibleSearch.defaults, options);
+			options = $.extend({}, $.fn.collapsibleSearch.defaults, options);
+			options.selector = element.selector;
+			options.transitionEnd = 'webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend';
 
-			instance.options.selector = element.selector;
+			instance.options = options;
 
-			instance._handleBlur(element);
-			instance._handleClick(element);
-			instance._handleFocus(element);
+			instance.basicSearch = element.find('.basic-search');
+			instance.searchInput = instance.basicSearch.find('input[type="text"]');
+
+			instance._bindUI(element);
 		},
 
-		_handleBlur: function(element) {
-			element.find('.basic-search .form-control').on('blur', function(event) {
+		_bindUI: function(element) {
+			var instance = this;
+
+			instance._onSearchInputBlur(element);
+			instance._onClickSubmitButton(element);
+			instance._onSearchInputFocus(element);
+		},
+
+		_onSearchInputBlur: function(element) {
+			var instance = this;
+
+			var searchInput = instance.searchInput;
+
+			searchInput.on('blur', function(event) {
 				$(this).closest('.basic-search').removeClass('focus');
 			});
 		},
 
-		_handleFocus: function(element) {
-			element.find('.basic-search .form-control').on('focus', function(event) {
+		_onSearchInputFocus: function(element) {
+			var instance = this;
+
+			var searchInput = instance.searchInput;
+
+			searchInput.on('focus', function(event) {
 				$(this).closest('.basic-search').addClass('focus');
 			});
 		},
 
-		_handleClick: function(element) {
+		_onTransitionEnd: function(basicSearch) {
 			var instance = this;
 
-			element.find('.basic-search button[type="submit"]').on('click', function(event) {
-				if (window.innerWidth < 768) {
+			var transitionEnd = instance.options.transitionEnd;
+
+			basicSearch.find('.basic-search-slider').on(transitionEnd, function(event) {
+				var basicSearch = $(this).closest('.basic-search');
+
+				basicSearch.removeClass('basic-search-transition');
+
+				if (basicSearch.data('lexicon.collapsible-search-open')) {
+					basicSearch.find('input[type="text"]').focus();
+				}
+
+				$(this).off(transitionEnd);
+			});
+		},
+
+		_onClickCloseButton: function(basicSearch) {
+			var instance = this;
+
+			basicSearch.on('click', '.basic-search-close', function(event) {
+				var basicSearch = $(this).closest('.basic-search');
+
+				// Webkit needs time to unfocus input
+				setTimeout(function() {
+					basicSearch.data('lexicon.collapsible-search-open', false);
+
+					basicSearch.addClass('basic-search-transition');
+
+					instance._onTransitionEnd(basicSearch);
+
+					basicSearch.removeClass('open');
+					basicSearch.off('click');
+				}, 200);
+			});
+		},
+
+		_onClickSubmitButton: function(element) {
+			var instance = this;
+
+			var submitButton = instance.basicSearch.find('button[type="submit"]');
+
+			submitButton.on('click', function(event) {
+				if (window.innerWidth < instance.options.breakpoint) {
 					var basicSearch = $(this).parents('.basic-search');
-					var transitions = 'webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend';
 
-					basicSearch.find('.basic-search-slider').on(transitions, function(event) {
-						basicSearch.removeClass('basic-search-transition');
-						basicSearch.find('.basic-search-slider').off(transitions);
-					});
+					instance._onTransitionEnd(basicSearch);
 
-					// close search
-					basicSearch.on('click', '.basic-search-close', function(event) {
-						var basicSearchSlider = $(this).closest('.basic-search-slider');
-
-						$(this).closest('.basic-search-slider').on(transitions, function(event) {
-							setTimeout(function() {
-								basicSearch.removeClass('basic-search-transition');
-							}, 500);
-
-							basicSearchSlider.off(transitions);
-						});
-
-						basicSearch.addClass('basic-search-transition');
-						basicSearch.removeClass('open');
-						basicSearch.off('click');
-					});
+					instance._onClickCloseButton(basicSearch);
 
 					if (!basicSearch.hasClass('open')) {
 						event.preventDefault();
 
-						basicSearch.addClass('basic-search-transition');
-
+						basicSearch.data('lexicon.collapsible-search-open', true);
 						basicSearch.addClass('open');
-
-						setTimeout(function() {
-							basicSearch.find('input[type="text"]').focus();
-						}, 500);
+						basicSearch.addClass('basic-search-transition');
 					}
 				}
 			});
 		}
 	};
 
-	$.fn.collapsibleSearch = function(options) {
-		new CollapsibleSearch(this, options);
+	var old = $.fn.collapsibleSearch;
+
+	var Plugin = function(options) {
+		return this.each(
+			function() {
+				var $this = $(this);
+
+				var data = $this.data('lexicon.collapsible-search')
+
+				if (!data) {
+					data = new CollapsibleSearch($this, typeof options === 'object' ? options : null);
+
+					$this.data('lexicon.collapsible-search', data);
+				}
+
+				if (typeof options === 'string') {
+					data[options].call($this);
+				}
+			}
+		);
+	};
+
+	Plugin.noConflict = function() {
+		$.fn.collapsibleSearch = old;
 
 		return this;
 	};
 
-	$.fn.collapsibleSearch.defaults = {
-
+	Plugin.defaults = {
+		breakpoint: 768
 	};
 
-	$.fn.collapsibleSearch.Constructor = CollapsibleSearch;
+	Plugin.Constructor = CollapsibleSearch;
 
+	$.fn.collapsibleSearch = Plugin;
 }(jQuery);
