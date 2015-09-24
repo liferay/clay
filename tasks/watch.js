5cde90757be84fbdf0d88f19a9a0a0b3e8cc6c3e
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2015-09-24 06:28:59 -0700

    Make watch also build when started

--- tasks/watch.js
+++ tasks/watch.js
@@ -3,6 +3,7 @@ module.exports = function(gulp, plugins, _, config) {
 
 	gulp.task(
 		'build:watch',
+		['build'],
 		function(done) {
 			gulp.watch(
 				[config.SRC_GLOB, '!src/scss/bootstrap/_variables.scss', 'templates/**/*'],
