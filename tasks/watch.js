fb70ad016f70ba36261abfbe7e4845174d578445
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2015-10-30 13:01:05 -0700

    Adding changelog to site

--- tasks/watch.js
+++ tasks/watch.js
@@ -6,7 +6,7 @@ module.exports = function(gulp, plugins, _, config) {
 		['build'],
 		function(done) {
 			gulp.watch(
-				[config.SRC_GLOB, '!src/scss/bootstrap/_variables.scss', 'templates/**/*'],
+				['./CHANGELOG.md', config.SRC_GLOB, '!src/scss/bootstrap/_variables.scss', 'templates/**/*'],
 				['build']
 			);
 		}
