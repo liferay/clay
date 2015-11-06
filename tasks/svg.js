2ebc9492e9f246e486c6eab8e7c990997c02d7b9
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2015-11-06 14:32:58 -0800

    Fixing the svg build task so it works with watch

--- tasks/svg.js
+++ tasks/svg.js
@@ -3,5 +3,7 @@ var _s = require('underscore.string');
 module.exports = function(gulp, plugins, _, config) {
 	var svgstore = require('../lib/svgstore');
 
-	gulp.task('build:svg', svgstore(gulp, plugins, _, {}));
+	gulp.task('build:svg', function() {
+		return svgstore(gulp, plugins, _, {});
+	});
 };
