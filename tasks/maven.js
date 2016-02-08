7caad56677eeb15c1e2f219a1d9099cc3baa11c7
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2016-02-08 09:56:29 -0800

    Formatting the gulp file to follow our normal conventions

--- tasks/maven.js
+++ tasks/maven.js
@@ -1,57 +1,55 @@
-'use strict';
+module.exports = function(gulp, plugins, _, config) {
+	var runSequence = require('run-sequence');
 
-var gulp = require('gulp');
-var maven = require('gulp-maven-deploy');
-var runSequence = require('run-sequence');
+	var version = require('../package.json').version;
 
-// Maven -----------------------------------------------------------------------
+	gulp.task('prepare-maven-snapshot', function() {
+		return gulp.src(['release/**/*'])
+			.pipe(gulp.dest('maven-dist/META-INF/resources/webjars/lexicon/' + version + '/release'));
+	});
 
-gulp.task('prepare-maven-snapshot', function() {
-	return gulp.src(['release/*'])
-		.pipe(gulp.dest('maven-dist/META-INF/resources/webjars/lexicon/0.2.1-SNAPSHOT/release'));
-});
+	gulp.task('install-maven-snapshot', function() {
+		return gulp.src('.')
+			.pipe(plugins.mavenDeploy.install({
+				'config': {
+					'artifactId': 'com.liferay.webjars.lexicon',
+					'buildDir': 'maven-dist',
+					'finalName': '{name}-{version}',
+					'groupId': 'com.liferay.webjars',
+					'type': 'jar'
+				}
+			}));
+	});
 
-gulp.task('install-maven-snapshot', function() {
-	return gulp.src('.')
-		.pipe(maven.install({
-			'config': {
-				'artifactId': 'lexicon',
-				'buildDir': 'maven-dist',
-				'finalName': '{name}-{version}',
-				'groupId': 'com.liferay.webjars',
-				'type': 'jar'
-			}
-		}));
-});
+	gulp.task('prepare-maven-artifact', function() {
+		return gulp.src(['release/**/*'])
+			.pipe(gulp.dest('maven-dist/META-INF/resources/webjars/lexicon/' + version + '/release'));
+	});
 
-gulp.task('prepare-maven-artifact', function() {
-	return gulp.src(['release/*'])
-		.pipe(gulp.dest('maven-dist/META-INF/resources/webjars/lexicon/0.2.1/release'));
-});
+	gulp.task('publish-maven-artifact', function() {
+		return gulp.src('.')
+			.pipe(plugins.mavenDeploy.deploy({
+				'config': {
+					'artifactId': 'com.liferay.webjars.lexicon',
+					'buildDir': 'maven-dist',
+					'finalName': '{name}-{version}',
+					'groupId': 'com.liferay.webjars',
+					'type': 'jar',
+					'repositories': [
+						{
+							'id': 'liferay-nexus-ce',
+							'url': 'https://repository.liferay.com/nexus/content/repositories/liferay-releases-ce/'
+						}
+					]
+				}
+			}));
+	});
 
-gulp.task('publish-maven-artifact', function() {
-	return gulp.src('.')
-		.pipe(maven.deploy({
-			'config': {
-				'artifactId': 'lexicon',
-				'buildDir': 'maven-dist',
-				'finalName': '{name}-{version}',
-				'groupId': 'com.liferay.webjars',
-				'type': 'jar',
-				'repositories': [
-					{
-						'id': 'liferay-nexus-ce',
-						'url': 'https://repository.liferay.com/nexus/content/repositories/liferay-releases-ce/'
-					}
-				]
-			}
-		}));
-});
+	gulp.task('maven-install', function(done) {
+		runSequence('prepare-maven-snapshot', 'install-maven-snapshot', done);
+	});
 
-gulp.task('maven-install', function(done) {
-	runSequence('prepare-maven-snapshot', 'install-maven-snapshot', done);
-});
-
-gulp.task('maven-publish', function(done) {
-	runSequence('prepare-maven-artifact', 'publish-maven-artifact', done);
-});
+	gulp.task('maven-publish', function(done) {
+		runSequence('prepare-maven-artifact', 'publish-maven-artifact', done);
+	});
+};
