686bc0bb605b2c00b4183067add0e075352ac312
Patrick Yeo <patrick.yeo@liferay.com>
2016-12-20 15:51:46 -0800

    Misc: Patch bootstrap.js with tether.js

--- tasks/patch.js
+++ tasks/patch.js
@@ -1,6 +1,9 @@
 module.exports = function(gulp, plugins, _, config) {
+	var fs = require('fs');
+
 	var BS4_VAR_OVERWRITES = '@import "../lexicon-base/variables/1bs4-variable-overwrites";\n\n';
 	var LEXICON_IMPORT = '\n\n@import "../lexicon-base/variables";';
+	var TETHER_JS = fs.readFileSync(config.TETHER_JS_FILE, 'utf8') + '\n\n';
 
 	var patchBootstrap = function(text, regex) {
 		return function(content) {
@@ -22,16 +25,28 @@ module.exports = function(gulp, plugins, _, config) {
 	};
 
 	gulp.task('build:patch-bootstrap', function(cb) {
-		return gulp.src(config.BOOTSTRAP_VAR_FILE)
+		var bootstrapVarFile = gulp.src(config.BOOTSTRAP_VAR_FILE)
 			.pipe(plugins.change(patchBootstrap(LEXICON_IMPORT)))
 			.pipe(plugins.change(patchBootstrap(BS4_VAR_OVERWRITES, '// Variables\n')))
 			.pipe(gulp.dest(config.BOOTSTRAP_VAR_DIR));
+
+		var bootstrapJsFile = gulp.src(config.BOOTSTRAP_JS_FILE)
+			.pipe(plugins.change(patchBootstrap(TETHER_JS, '/*!\n')))
+			.pipe(gulp.dest(config.BOOTSTRAP_JS_DIR));
+
+		return plugins.mergeStream(bootstrapVarFile, bootstrapJsFile);
 	});
 
 	gulp.task('build:clean-bootstrap-patch', function(cb) {
-		return gulp.src(config.BOOTSTRAP_VAR_FILE)
+		var bootstrapVarFile = gulp.src(config.BOOTSTRAP_VAR_FILE)
 			.pipe(plugins.change(cleanBootstrapPatch(BS4_VAR_OVERWRITES)))
 			.pipe(plugins.change(cleanBootstrapPatch(LEXICON_IMPORT)))
 			.pipe(gulp.dest(config.BOOTSTRAP_VAR_DIR));
+
+		var bootstrapJsFile = gulp.src(config.BOOTSTRAP_JS_FILE)
+			.pipe(plugins.change(cleanBootstrapPatch(TETHER_JS)))
+			.pipe(gulp.dest(config.BOOTSTRAP_JS_DIR));
+
+		return plugins.mergeStream(bootstrapVarFile, bootstrapJsFile);
 	});
 };
