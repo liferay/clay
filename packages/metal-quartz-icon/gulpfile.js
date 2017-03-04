5740685ea91e97a93645d1eb5af92f2fa9f48c89
Patrick Yeo <patrick.yeo@liferay.com>
2017-03-03 17:20:06 -0800

    metal-quartz-icon initial commit

---
+++ packages/metal-quartz-icon/gulpfile.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var metal = require('gulp-metal');
+
+metal.registerTasks({
+	bundleCssFileName: 'metal-quartz-icon.css',
+	bundleFileName: 'metal-quartz-icon.js',
+	moduleName: 'metal-quartz-icon'
+});
