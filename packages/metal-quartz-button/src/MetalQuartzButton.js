5c1df786bf6ccccdb13cf90ede0da4658a3f18fc
Fernando Souza <fernando.souza@liferay.com>
2017-03-13 12:10:19 -0300

    Button component initial commit

---
+++ packages/metal-quartz-button/src/MetalQuartzButton.js
@@ -0,0 +1,11 @@
+'use strict';
+
+import templates from './MetalQuartzButton.soy.js';
+import Component from 'metal-component';
+import Soy from 'metal-soy';
+
+class MetalQuartzButton extends Component {
+}
+Soy.register(MetalQuartzButton, templates);
+
+export default MetalQuartzButton;
---
+++ packages/metal-quartz-button/test/Button.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import Button from '../src/Button';
+
+describe('Button', function() {
+	it('should be tested', function() {
+		assert.fail('No tests for this module yet.');
+	});
+});
