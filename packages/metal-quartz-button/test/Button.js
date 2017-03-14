5789dd57bde1cba337b995737266257cc782b037
Fernando Souza <fernando.souza@liferay.com>
2017-03-14 17:31:41 -0300

    Adds test cases

--- packages/metal-quartz-button/test/Button.js
+++ packages/metal-quartz-button/test/Button.js
@@ -2,6 +2,14 @@
 
 import MetalQuartzButton from '../src/MetalQuartzButton';
 
+const FIXTURE_DIR = 'test/fixture/';
+
+const DEFAULT_BUTTON = __html__[FIXTURE_DIR + 'testDefaultMetalQuartzButton.html'].trim();
+const DEFAULT_BUTTON_WITH_LABEL = __html__[FIXTURE_DIR + 'testMetalQuartzButtonWithLabel.html'].trim();
+const DISABLED_BUTTON = __html__[FIXTURE_DIR + 'testDisabledMetalQuartzButton.html'].trim();
+const SUBMIT_BUTTON = __html__[FIXTURE_DIR + 'testSubmitMetalQuartzButton.html'].trim();
+const SUCCESS_BUTTON = __html__[FIXTURE_DIR + 'testSuccessMetalQuartzButton.html'].trim();
+
 let sample;
 
 describe('MetalQuartzButton', function() {
@@ -11,13 +19,41 @@ describe('MetalQuartzButton', function() {
 		}
 	});
 
-	it('should generate the content markup', function() {
-		sample = new MetalQuartzButton(
-			{
-				content: 'foo'
-			}
-		);
+	it('should generate the default markup', function() {
+		sample = new MetalQuartzButton();
 
-		assert.strictEqual(sample.element.outerHTML, __html__['test/fixture/testDefaultMetalQuartzButton.html'].trim());
+		assert.strictEqual(sample.element.outerHTML, DEFAULT_BUTTON);
+	});
+
+	it('should render a submit button', function() {
+		sample = new MetalQuartzButton({
+				type: 'submit'
+			});
+
+		assert.strictEqual(sample.element.outerHTML, SUBMIT_BUTTON);
+	});
+	
+	it('should render a disabled button', function() {
+		sample = new MetalQuartzButton({
+				disabled: true
+			});
+
+		assert.strictEqual(sample.element.outerHTML, DISABLED_BUTTON);
+	});
+	
+	it('should render a success button', function() {
+		sample = new MetalQuartzButton({
+				style: 'success'
+			});
+
+		assert.strictEqual(sample.element.outerHTML, SUCCESS_BUTTON);
+	});
+	
+	it('should render a button with label', function() {
+		sample = new MetalQuartzButton({
+				label: 'Label'
+			});
+
+		assert.strictEqual(sample.element.outerHTML, DEFAULT_BUTTON_WITH_LABEL);
 	});
 });
