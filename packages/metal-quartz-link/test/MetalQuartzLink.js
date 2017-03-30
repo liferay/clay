f4a69665758c5875430b0710b1efe011c95b6fe4
Patrick Yeo <patrick.yeo@liferay.com>
2017-03-30 11:19:22 -0700

    Metal Quartz Link: Tests

--- packages/metal-quartz-link/test/MetalQuartzLink.js
+++ packages/metal-quartz-link/test/MetalQuartzLink.js
@@ -1,5 +1,6 @@
 'use strict';
 
+import dom from 'metal-dom';
 import MetalQuartzLink from '../src/MetalQuartzLink';
 
 let component;
@@ -9,5 +10,75 @@ describe('MetalQuartzLink', function() {
 		if (component) {
 			component.dispose();
 		}
+	});
+
+	it('should apply classes to the anchor tag', () => {
+		component = new MetalQuartzLink({
+			elementClasses: 'my-test-class'
+		});
+
+		assert.ok(dom.hasClass(component.element, 'my-test-class'));
+	});
+
+	it('should apply the data-toggle attribute to the anchor tag', () => {
+		component = new MetalQuartzLink({
+			dataToggle: 'collapse'
+		});
+
+		assert.strictEqual(component.element.getAttribute('data-toggle'), 'collapse');
+	});
+
+	it('should apply an id to the anchor tag', () => {
+		component = new MetalQuartzLink({
+			id: 'myUniqueId'
+		});
+
+		assert.strictEqual(component.element.getAttribute('id'), 'myUniqueId');
+	});
+
+	it('should apply an href to the anchor tag', () => {
+		component = new MetalQuartzLink({
+			href: 'http://www.example.com'
+		});
+
+		assert.strictEqual(component.element.getAttribute('href'), 'http://www.example.com');
+	});
+
+	it('should should be able to render straight html', () => {
+		component = new MetalQuartzLink({
+			html: '<a aria-label="my-aria-label" href="#1" target="_blank">Html</a>'
+		});
+
+		assert.strictEqual(component.element.outerHTML, __html__['test/fixture/testDefaultMetalQuartzLink.html'].trim());
+	});
+
+	it('should render the correct markup with icon on the right', () => {
+		component = new MetalQuartzLink({
+			dataToggle: 'toggle',
+			href: '#1',
+			icon: {
+				alignment: 'right',
+				spritemap: '../node_modules/lexicon-ux/build/images/icons/icons.svg',
+				symbol: 'add-cell'
+			},
+			label: '<span>My Other Link Label</span>'
+		});
+
+		assert.strictEqual(component.element.outerHTML, __html__['test/fixture/testDefaultMetalQuartzLinkIconRight.html'].trim());
+	});
+
+	it('should render the correct markup with icon on the left', () => {
+		component = new MetalQuartzLink({
+			dataToggle: 'toggle',
+			href: '#1',
+			icon: {
+				alignment: 'left',
+				spritemap: '../node_modules/lexicon-ux/build/images/icons/icons.svg',
+				symbol: 'add-cell'
+			},
+			label: 'My Link Label'
+		});
+
+		assert.strictEqual(component.element.outerHTML, __html__['test/fixture/testDefaultMetalQuartzLinkIconLeft.html'].trim());
 	});
 });
