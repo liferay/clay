1eaf488bfdff58e6ed4a64321f94b608136e6b28
Patrick Yeo <patrick.yeo@liferay.com>
2017-03-30 11:19:45 -0700

    Metal Quartz Link: #18 Create Component

--- packages/metal-quartz-link/src/MetalQuartzLink.js
+++ packages/metal-quartz-link/src/MetalQuartzLink.js
@@ -1,8 +1,10 @@
 'use strict';
 
 import Component from 'metal-component';
-import core from 'metal';
+import MetalQuartzIcon from 'metal-quartz-icon';
 import Soy from 'metal-soy';
+import { core, object } from 'metal';
+import { validators } from 'metal-state';
 
 import templates from './MetalQuartzLink.soy.js';
 
@@ -10,6 +12,12 @@ import templates from './MetalQuartzLink.soy.js';
  * Implementation of the Metal Quartz Link.
  */
 class MetalQuartzLink extends Component {
+	/**
+	 * @inheritDoc
+	 */
+	created() {
+		MetalQuartzLink.instances.push(this);
+	}
 }
 
 /**
@@ -18,8 +26,78 @@ class MetalQuartzLink extends Component {
  * @type {!Object}
  */
 MetalQuartzLink.STATE = {
+	/**
+	 * Sets the data-toggle attribute on the anchor tag.
+	 * @instance
+	 * @memberof MetalQuartzLink
+	 * @type {?string}
+	 * default undefined
+	 */
+	dataToggle: {
+		validator: validators.string
+	},
 
+	/**
+	 * Sets the HTML attribute href on the anchor tag.
+	 * @instance
+	 * @memberof MetalQuartzLink
+	 * @type {?string}
+	 * @default undefined
+	 */
+	href: {
+		validator: validators.string
+	},
+
+	/**
+	 * Render custom HTML instead of using the template.
+	 * @instance
+	 * @memberof MetalQuartzLink
+	 * @type {?string}
+	 * @default undefined
+	 */
+	html: {
+		isHtml: true
+	},
+
+	/**
+	 * Render MetalQuartzIcon in the link, available options are `alignment`,
+	 * `elementClasses`, `spritemap`, `symbol`.
+	 * @instance
+	 * @memberof MetalQuartzLink
+	 * @type {?Object}
+	 * @default undefined
+	 */
+	icon: {
+		validator: validators.object
+	},
+
+	/**
+	 * Id to be applied to the anchor tag.
+	 * @instance
+	 * @memberof MetalQuartzLink
+	 * @type {?string}
+	 * default undefined
+	 */
+	id: {
+		validator: validators.string
+	},
+
+	/**
+	 * Sets the text or HTML to be rendered inside the anchor tag.
+	 * @instance
+	 * @memberof MetalQuartzLink
+	 * @type {?string}
+	 * @default undefined
+	 */
+	label: {
+		isHtml: true
+	}
 };
+
+/**
+ * An array of all MetalQuartzLink's that are created on the page.
+ */
+MetalQuartzLink.instances = [];
 
 Soy.register(MetalQuartzLink, templates);
 
