6aa5247ae7066348d2a41f4eac7031ae6adc7d7b
Fernando Souza <nandosouzafilho@gmail.com>
2017-06-16 11:37:23 -0700

    Web pack configuration and dependencies

---
+++ packages/metal-clay-button/webpack.config.js
@@ -0,0 +1,27 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/MetalClayButton.js',
+  module: {
+    rules: [{
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                compact: false,
+                presets: ['babel-preset-es2015'],
+                plugins: ['babel-plugin-transform-node-env-inline']
+            }
+        }
+    }]
+  },
+  output: {
+      library: 'metal',
+      libraryTarget: 'this',
+      filename: './build/globals/button.js',
+  },
+  plugins: [
+    new webpack.optimize.DedupePlugin()
+  ]
+};
