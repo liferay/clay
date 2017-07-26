2531595c07133cc797ee7102e7a60c817c6bbdfc
Carlos Lancha <carlos.lancha@liferay.com>
2017-07-26 12:44:37 +0200

    SF

--- packages/clay-link/src/ClayLink.js
+++ packages/clay-link/src/ClayLink.js
@@ -1,7 +1,7 @@
 'use strict';
 
-import Component from 'metal-component';
 import 'clay-icon';
+import Component from 'metal-component';
 import Soy from 'metal-soy';
 import { Config } from 'metal-state';
 
