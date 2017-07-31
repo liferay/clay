6113f405b1e76a406bf2d0d790d4339fa5c2424c
Carlos Lancha <carlos.lancha@liferay.com>
2017-07-31 11:41:04 +0200

    LEXI-47 Create ClayProgressBar

---
+++ packages/clay-progress-bar/src/__tests__/ClayProgressBar.js
@@ -0,0 +1,75 @@
+'use strict';
+
+import ClayProgressBar from '../ClayProgressBar';
+
+let progressBar;
+
+const spritemap = '../node_modules/lexicon-ux/build/images/icons/icons.svg';
+
+describe('ClayProgressBar', function() {
+	afterEach(() => {
+		if (progressBar) {
+			progressBar.dispose();
+		}
+	});
+
+	it('should generate an in progress bar', function() {
+		progressBar = new ClayProgressBar({
+			minValue: 0,
+			maxValue: 100,
+			spritemap: spritemap,
+			value: 40,
+		});
+
+		expect(progressBar).toMatchSnapshot();
+	});
+
+	it('should generate a progress bar at 0% of progress', function() {
+		progressBar = new ClayProgressBar({
+			minValue: 0,
+			maxValue: 100,
+			spritemap: spritemap,
+			value: 0,
+		});
+
+		expect(progressBar).toMatchSnapshot();
+	});
+
+	it('should generate a progress bar at 0% of progress', function() {
+		progressBar = new ClayProgressBar({});
+
+		expect(progressBar).toMatchSnapshot();
+	});
+
+	it('should generate a progress bar with warning', function() {
+		progressBar = new ClayProgressBar({
+			minValue: 0,
+			maxValue: 100,
+			spritemap: spritemap,
+			status: 'warning',
+			value: 40,
+		});
+
+		expect(progressBar).toMatchSnapshot();
+	});
+
+	it('should generate a completed progress bar', function() {
+		progressBar = new ClayProgressBar({
+			minValue: 0,
+			maxValue: 100,
+			spritemap: spritemap,
+			value: 100,
+		});
+
+		expect(progressBar).toMatchSnapshot();
+	});
+
+	it('should generate a completed progress bar', function() {
+		progressBar = new ClayProgressBar({
+			spritemap: spritemap,
+			status: 'complete',
+		});
+
+		expect(progressBar).toMatchSnapshot();
+	});
+});
