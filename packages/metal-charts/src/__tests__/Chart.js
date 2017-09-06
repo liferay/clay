eca7aed0aa7a5bdcfdef9c8ba1febc0c7e73bca0
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2017-09-06 12:31:18 -0700

    Add tests to ensure re-rendering is triggered when passing new config

--- packages/metal-charts/src/__tests__/Chart.js
+++ packages/metal-charts/src/__tests__/Chart.js
@@ -68,4 +68,77 @@ describe('Chart', function() {
 
 		expect(config).toMatchSnapshot();
 	});
+
+	it('should transform chart new type is passed', () => {
+		const transformMock = jest.fn();
+
+		bb.generate.mockReturnValue({
+			transform: transformMock
+		});
+
+		const chart = new Chart({
+			columns: [],
+			type: 'line'
+		});
+
+		chart.type = 'spline';
+
+		expect(transformMock.mock.calls[0][0]).toBe('spline');
+	});
+
+	it('should rerender chart when new columns are passed', () => {
+		const loadMock = jest.fn();
+
+		bb.generate.mockReturnValue({
+			load: loadMock
+		});
+
+		const chart = new Chart({
+			columns: [
+				{
+					id: 'data1',
+					data: [1, 2, 3]
+				}
+			]
+		});
+
+		chart.columns = [
+			{
+				id: 'data1',
+				data: [1, 2, 3, 4]
+			}
+		];
+
+		const config = JSON.stringify(loadMock.mock.calls[0][0]);
+
+		expect(config).toMatchSnapshot();
+	});
+
+	it('should unload removed columns when new columns are passed', () => {
+		const loadMock = jest.fn();
+
+		bb.generate.mockReturnValue({
+			load: loadMock
+		});
+
+		const chart = new Chart({
+			columns: [
+				{
+					id: 'data1',
+					data: [1, 2, 3]
+				}
+			]
+		});
+
+		chart.columns = [
+			{
+				id: 'data2',
+				data: [1, 2, 3]
+			}
+		];
+
+		const config = JSON.stringify(loadMock.mock.calls[0][0]);
+
+		expect(config).toMatchSnapshot();
+	});
 });
