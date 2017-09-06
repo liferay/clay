fbf28cb2001165911bbf7e96d45c6bcdc7e32a35
Matuzalém S. Teles <matuzalemteles@gmail.com>
2017-09-06 10:50:11 -0300

    LEXI-88 Add suffixes in label

--- packages/clay-badge/src/ClayBadge.js
+++ packages/clay-badge/src/ClayBadge.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import Component from 'metal-component';
+import core from 'metal';
 import Soy from 'metal-soy';
 import { Config } from 'metal-state';
 
@@ -9,7 +10,49 @@ import templates from './ClayBadge.soy.js';
 /**
  * Metal Clay Badge component.
  */
-class ClayBadge extends Component {}
+class ClayBadge extends Component {
+	/**
+	 * @inheritDoc
+	 */
+	created() {
+		this.suffixes = ['K', 'M', 'B', 'T'];
+	}
+
+	/**
+	 * Check if the value it is a number.
+	 * @param {number} value
+	 * @return {string|number}
+	 * @protected
+	 */
+	hasNumberLabel_(value) {
+		if (core.isNumber(value)) {
+			value = this.abbreviationNumbers_(value);
+		}
+
+		return value;
+	}
+
+	/**
+	 * Make abbreviation of numbers and return with suffixes if necessary.
+	 * @param {number} value
+	 * @return {string|number} return the value with the suffix.
+	 * @protected
+	 */
+	abbreviationNumbers_(value) {
+		for (let i = this.suffixes.length; i >= 0; i--) {
+			let size = Math.pow(10, (i + 1) * 3);
+
+			if (size <= value) {
+				value = Math.floor(Math.round(value * 10 / size) / 10);
+				value += this.suffixes[i];
+
+				break;
+			}
+		}
+
+		return value;
+	}
+}
 
 /**
  * State definition.
@@ -33,7 +76,7 @@ ClayBadge.STATE = {
 	 * @type {number}
 	 * default undefined
 	 */
-	label: Config.number().required(),
+	label: Config.number().setter('hasNumberLabel_').required(),
 
 	/**
 	 * Badge style color.
