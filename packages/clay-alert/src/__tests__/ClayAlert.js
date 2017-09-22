3b7e04977bb98a33b38e80a5670f69e74997b5f6
Carlos Lancha <carlos.lancha@liferay.com>
2017-09-22 15:28:07 +0200

    LEXI-153 Update demo and tests

--- packages/clay-alert/src/__tests__/ClayAlert.js
+++ packages/clay-alert/src/__tests__/ClayAlert.js
@@ -2,6 +2,8 @@ import ClayAlert from '../ClayAlert';
 
 let alert;
 
+const spritemap = '../node_modules/lexicon-ux/build/images/icons/icons.svg';
+
 describe('ClayAlert', function() {
   afterEach(() => {
     if (alert) {
@@ -9,18 +11,52 @@ describe('ClayAlert', function() {
     }
   });
 
-  it('should render a alert style "info"', function() {
+  it('should render default markup', function() {
     alert = new ClayAlert({
       message: 'message',
+      spritemap: spritemap,
       title: 'info',
     });
 
     expect(alert).toMatchSnapshot();
   });
 
-  it('should render a alert style "danger"', function() {
+  it('should render a notification alert', function() {
     alert = new ClayAlert({
       message: 'message',
+      spritemap: spritemap,
+      title: 'info',
+      type: 'notification',
+    });
+
+    expect(alert).toMatchSnapshot();
+  });
+
+  it('should render a fluid alert', function() {
+    alert = new ClayAlert({
+      message: 'message',
+      spritemap: spritemap,
+      title: 'info',
+      type: 'fluid',
+    });
+
+    expect(alert).toMatchSnapshot();
+  });
+
+  it('should render an alert style "info"', function() {
+    alert = new ClayAlert({
+      message: 'message',
+      spritemap: spritemap,
+      title: 'info',
+    });
+
+    expect(alert).toMatchSnapshot();
+  });
+
+  it('should render an alert style "danger"', function() {
+    alert = new ClayAlert({
+      message: 'message',
+      spritemap: spritemap,
       style: 'danger',
       title: 'danger',
     });
@@ -28,9 +64,10 @@ describe('ClayAlert', function() {
     expect(alert).toMatchSnapshot();
   });
 
-  it('should render a alert style "warning"', function() {
+  it('should render an alert style "warning"', function() {
     alert = new ClayAlert({
       message: 'message',
+      spritemap: spritemap,
       style: 'warning',
       title: 'warning',
     });
@@ -38,9 +75,10 @@ describe('ClayAlert', function() {
     expect(alert).toMatchSnapshot();
   });
 
-  it('should render a alert style "success"', function() {
+  it('should render an alert style "success"', function() {
     alert = new ClayAlert({
       message: 'message',
+      spritemap: spritemap,
       style: 'success',
       title: 'success',
     });
@@ -48,10 +86,49 @@ describe('ClayAlert', function() {
     expect(alert).toMatchSnapshot();
   });
 
+  it('should render a closeable alert', function() {
+    alert = new ClayAlert({
+      closeable: true,
+      message: 'message',
+      spritemap: spritemap,
+      title: 'success',
+    });
+
+    expect(alert).toMatchSnapshot();
+  });
+
+  it('should close an alert', function() {
+    alert = new ClayAlert({
+      closeable: true,
+      message: 'message',
+      spritemap: spritemap,
+      title: 'success',
+    });
+
+    alert.element.querySelector('button').click();
+
+    expect(alert._visible).toBeFalsy();
+  });
+
+  it('should close and destroy alert', function() {
+    alert = new ClayAlert({
+      closeable: true,
+      destroyOnHide: true,
+      message: 'message',
+      spritemap: spritemap,
+      title: 'success',
+    });
+
+    alert.element.querySelector('button').click();
+
+    expect(alert.disposed_).toBeTruthy();
+  });
+
   it('should fail when no title is passed', function() {
     expect(() => {
       alert = new ClayAlert({
         message: 'message',
+        spritemap: spritemap,
       });
     }).toThrow();
   });
@@ -59,6 +136,16 @@ describe('ClayAlert', function() {
   it('should fail when no message is passed', function() {
     expect(() => {
       alert = new ClayAlert({
+        spritemap: spritemap,
+        title: 'title',
+      });
+    }).toThrow();
+  });
+
+  it('should fail when no spritemap is passed', function() {
+    expect(() => {
+      alert = new ClayAlert({
+        message: 'message',
         title: 'title',
       });
     }).toThrow();
