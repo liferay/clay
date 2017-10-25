dbe9da3fdd3a6bc78982ed05ecfa455fec7de7cd
Carlos Lancha <carlos.lancha@liferay.com>
2017-10-25 13:56:11 +0200

    LEXI-64 Update tests

--- packages/clay-modal/src/__tests__/ClayModal.js
+++ packages/clay-modal/src/__tests__/ClayModal.js
@@ -1,9 +1,7 @@
-import dom from 'metal-dom';
 import ClayModal from '../ClayModal';
 
 let clayModal;
 let body = '<h4>Hello world!</h4>';
-let iframe = '<iframe title="Clay" src=""></iframe>';
 let spritemap = 'icons.svg';
 
 describe('ClayModal', function() {
@@ -19,19 +17,16 @@ describe('ClayModal', function() {
     expect(clayModal).toMatchSnapshot();
   });
 
-  it('should render a modal complete', () => {
+  it('should render a modal with title', () => {
     clayModal = new ClayModal({
-      body: body,
-      footer: [
-        {
-          label: 'Cancel',
-          type: 'close',
-        },
-        {
-          label: 'Save',
-          type: 'save',
-        },
-      ],
+      title: 'My title',
+    });
+
+    expect(clayModal).toMatchSnapshot();
+  });
+
+  it('should render a modal with title and header closing button', () => {
+    clayModal = new ClayModal({
       spritemap: spritemap,
       title: 'My title',
     });
@@ -39,301 +34,144 @@ describe('ClayModal', function() {
     expect(clayModal).toMatchSnapshot();
   });
 
-  it('should render a modal with header and body', () => {
+  it('should render a modal with body', () => {
     clayModal = new ClayModal({
-      body: body,
-      spritemap: spritemap,
-      title: 'My title',
+      body: 'My body',
     });
 
     expect(clayModal).toMatchSnapshot();
   });
 
-  it('should render a modal with body and footer', () => {
+  it('should render a modal with footer buttons', () => {
     clayModal = new ClayModal({
-      body: body,
-      footer: [
+      footerButtons: [
         {
           label: 'Cancel',
           type: 'close',
         },
         {
           label: 'Save',
-          type: 'save',
         },
       ],
-    });
-
-    expect(clayModal).toMatchSnapshot();
-  });
-
-  it('should render a modal with status `info`', () => {
-    clayModal = new ClayModal({
-      body: body,
-      footer: [
-        {
-          label: 'Cancel',
-          type: 'close',
-        },
-        {
-          label: 'Save',
-          type: 'save',
-        },
-      ],
-      status: 'info',
-      spritemap: spritemap,
-      title: 'My title',
-    });
-
-    expect(clayModal).toMatchSnapshot();
-  });
-
-  it('should render a modal with status `danger`', () => {
-    clayModal = new ClayModal({
-      body: body,
-      footer: [
-        {
-          label: 'Cancel',
-          type: 'close',
-        },
-        {
-          label: 'Save',
-          type: 'save',
-        },
-      ],
-      status: 'danger',
-      spritemap: spritemap,
-      title: 'My title',
-    });
-
-    expect(clayModal).toMatchSnapshot();
-  });
-
-  it('should render a modal with status `success`', () => {
-    clayModal = new ClayModal({
-      body: body,
-      footer: [
-        {
-          label: 'Cancel',
-          type: 'close',
-        },
-        {
-          label: 'Save',
-          type: 'save',
-        },
-      ],
-      status: 'success',
-      spritemap: spritemap,
-      title: 'My title',
-    });
-
-    expect(clayModal).toMatchSnapshot();
-  });
-
-  it('should render a modal with status `warning`', () => {
-    clayModal = new ClayModal({
-      body: body,
-      footer: [
-        {
-          label: 'Cancel',
-          type: 'close',
-        },
-        {
-          label: 'Save',
-          type: 'save',
-        },
-      ],
-      status: 'warning',
-      spritemap: spritemap,
-      title: 'My title',
-    });
-
-    expect(clayModal).toMatchSnapshot();
-  });
-
-  it('should render a small modal', () => {
-    clayModal = new ClayModal({
-      body: body,
-      footer: [
-        {
-          label: 'Cancel',
-          type: 'close',
-        },
-        {
-          label: 'Save',
-          type: 'save',
-        },
-      ],
-      size: 'sm',
-      spritemap: spritemap,
-      title: 'My title',
-    });
-
-    expect(clayModal).toMatchSnapshot();
-  });
-
-  it('should render a large modal', () => {
-    clayModal = new ClayModal({
-      body: body,
-      footer: [
-        {
-          label: 'Cancel',
-          type: 'close',
-        },
-        {
-          label: 'Save',
-          type: 'save',
-        },
-      ],
-      size: 'lg',
-      spritemap: spritemap,
-      title: 'My title',
-    });
-
-    expect(clayModal).toMatchSnapshot();
-  });
-
-  it('should render a full-screen modal', () => {
-    clayModal = new ClayModal({
-      body: body,
-      footer: [
-        {
-          label: 'Cancel',
-          type: 'close',
-        },
-        {
-          label: 'Save',
-          type: 'save',
-        },
-      ],
-      size: 'full-screen',
-      spritemap: spritemap,
-      title: 'My title',
-    });
-
-    expect(clayModal).toMatchSnapshot();
-  });
-
-  it('should render a modal with iframe', () => {
-    clayModal = new ClayModal({
-      body: iframe,
-      iframe: true,
-      footer: [
-        {
-          label: 'Cancel',
-          type: 'close',
-        },
-        {
-          label: 'Save',
-          type: 'save',
-        },
-      ],
-      spritemap: spritemap,
-      title: 'My title',
     });
 
     expect(clayModal).toMatchSnapshot();
   });
 
   // eslint-disable-next-line
-  it('should render a modal with buttons in position right and left in footer', () => {
+  it('should render a modal with footer buttons aligned to left and right', () => {
     clayModal = new ClayModal({
-      body: body,
-      footer: [
+      footerButtons: [
         {
           label: 'Cancel',
           type: 'close',
         },
         {
           label: 'Save',
-          type: 'save',
         },
         {
-          label: 'All',
+          alignment: 'left',
+          label: 'My button 1',
         },
         {
-          label: 'All',
-          style: 'secondary',
+          alignment: 'left',
+          label: 'My button 2',
         },
       ],
-      spritemap: spritemap,
-      title: 'My title',
     });
 
     expect(clayModal).toMatchSnapshot();
   });
 
-  it('should render a modal open', done => {
+  it('should render a modal with iframe as body', () => {
+    clayModal = new ClayModal({
+      url: 'http://www.claycss.com',
+    });
+
+    expect(clayModal).toMatchSnapshot();
+  });
+
+  it('should render a modal with status `danger` and title', () => {
+    clayModal = new ClayModal({
+      status: 'danger',
+      spritemap: spritemap,
+      title: 'My Title',
+    });
+
+    expect(clayModal).toMatchSnapshot();
+  });
+
+  it('should render a modal with status `info` and title', () => {
+    clayModal = new ClayModal({
+      status: 'info',
+      spritemap: spritemap,
+      title: 'My Title',
+    });
+
+    expect(clayModal).toMatchSnapshot();
+  });
+
+  it('should render a modal with status `success` and title', () => {
+    clayModal = new ClayModal({
+      status: 'success',
+      spritemap: spritemap,
+      title: 'My Title',
+    });
+
+    expect(clayModal).toMatchSnapshot();
+  });
+
+  it('should render a modal with status `warning` and title', () => {
+    clayModal = new ClayModal({
+      status: 'warning',
+      spritemap: spritemap,
+      title: 'My Title',
+    });
+
+    expect(clayModal).toMatchSnapshot();
+  });
+
+  it('should render a modal with size `small`', () => {
     clayModal = new ClayModal({
       body: body,
-      footer: [
-        {
-          label: 'Cancel',
-          type: 'close',
-        },
-        {
-          label: 'Save',
-          type: 'save',
-        },
-      ],
+      size: 'sm',
       spritemap: spritemap,
-      title: 'My title',
+      title: 'My Title',
     });
 
-    clayModal.show();
-
-    setTimeout(() => {
-      expect(dom.hasClass(document.body, 'modal-open')).toBeTruthy();
-      expect(clayModal._isTransitioning).toBeTruthy();
-      expect(clayModal.visible).toBeTruthy();
-      setTimeout(() => {
-        expect(clayModal).toMatchSnapshot();
-        done();
-      }, 100);
-    }, 10);
+    expect(clayModal).toMatchSnapshot();
   });
 
-  it('should close modal when press the escape key', done => {
-    clayModal = new ClayModal({});
-
-    clayModal.show();
-    dom.triggerEvent(document, 'keyup');
-
-    setTimeout(() => {
-      expect(clayModal.visible).toBeTruthy();
-      expect(clayModal._isTransitioning).toBeTruthy();
-
-      dom.triggerEvent(document, 'keyup', {
-        keyCode: 27,
-      });
-
-      setTimeout(() => {
-        expect(clayModal.visible).toBeFalsy();
-        expect(clayModal._isTransitioning).toBeFalsy();
-        done();
-      }, 100);
-    }, 10);
-  });
-
-  it('should close on clicking close icon', done => {
+  it('should render a modal with size `large`', () => {
     clayModal = new ClayModal({
+      body: body,
+      size: 'lg',
       spritemap: spritemap,
-      title: 'My title',
+      title: 'My Title',
     });
 
-    clayModal.show();
+    expect(clayModal).toMatchSnapshot();
+  });
 
-    setTimeout(() => {
-      expect(clayModal.visible).toBeTruthy();
-      expect(clayModal._isTransitioning).toBeTruthy();
+  it('should render a modal with size `full-screen`', () => {
+    clayModal = new ClayModal({
+      body: body,
+      size: 'full-screen',
+      spritemap: spritemap,
+      title: 'My Title',
+    });
 
-      dom.triggerEvent(clayModal.element.querySelector('.close'), 'click');
+    expect(clayModal).toMatchSnapshot();
+  });
 
-      setTimeout(() => {
-        expect(clayModal.visible).toBeFalsy();
-        expect(clayModal._isTransitioning).toBeFalsy();
-        done();
-      }, 100);
-    }, 10);
+  it('should render a modal opened', () => {
+    clayModal = new ClayModal({
+      body: body,
+      spritemap: spritemap,
+      title: 'My Title',
+      visible: true,
+    });
+
+    expect(clayModal).toMatchSnapshot();
   });
 });
