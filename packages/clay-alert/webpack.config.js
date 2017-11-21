11cc542c6bd0386b54f8e10a4440c8298133aa40
Carlos Lancha <carlos.lancha@liferay.com>
2017-11-21 12:55:52 +0100

    Export all alert types

--- packages/clay-alert/webpack.config.js
+++ packages/clay-alert/webpack.config.js
@@ -2,30 +2,30 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-  entry: './src/ClayAlert.js',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            compact: false,
-            presets: ['babel-preset-env'],
-            plugins: ['babel-plugin-transform-node-env-inline'],
-          },
-        },
-      },
-    ],
-  },
-  output: {
-    library: 'metal',
-    libraryTarget: 'this',
-    filename: './build/globals/clay-alert.js',
-  },
-  plugins: [new webpack.optimize.ModuleConcatenationPlugin()],
-  resolve: {
-    mainFields: ['esnext:main', 'main'],
-  },
+	entry: ['./src/all.js'],
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						compact: false,
+						presets: ['babel-preset-env'],
+						plugins: ['babel-plugin-transform-node-env-inline'],
+					},
+				},
+			},
+		],
+	},
+	output: {
+		library: 'metal',
+		libraryTarget: 'this',
+		filename: './build/globals/clay-alert.js',
+	},
+	plugins: [new webpack.optimize.ModuleConcatenationPlugin()],
+	resolve: {
+		mainFields: ['esnext:main', 'main'],
+	},
 };
