2b45daedb101462e4a692179eb91de3df67cc7d2
Carlos Lancha <carlos.lancha@liferay.com>
2017-11-21 17:29:55 +0100

    SF

--- packages/generator-metal-clay/__tests__/test-app.js
+++ packages/generator-metal-clay/__tests__/test-app.js
@@ -5,47 +5,53 @@ let assert = require('yeoman-generator').assert;
 let helpers = require('yeoman-generator').test;
 
 describe('clay-generator:app', function() {
-  beforeAll(function(done) {
-    helpers
-      .run(path.join(__dirname, '../app'))
-      .withOptions({skipInstall: true})
-      .withPrompts({
-        componentName: 'ClayComponent',
-        repoDescription: 'My awesome Clay componentc',
-      })
-      .on('end', done);
-  });
+	beforeAll(function(done) {
+		helpers
+			.run(path.join(__dirname, '../app'))
+			.withOptions({skipInstall: true})
+			.withPrompts({
+				componentName: 'ClayComponent',
+				repoDescription: 'My awesome Clay componentc',
+			})
+			.on('end', done);
+	});
 
-  it('creates files', function() {
-    assert.file([
-      'demos/index.html',
-      'src/ClayComponent.js',
-      'src/ClayComponent.soy',
-      'src/__tests__/ClayComponent.js',
-      'webpack.config.js',
-      'package.json',
-      'README.md',
-    ]);
-  });
+	it('creates files', function() {
+		assert.file([
+			'demos/index.html',
+			'src/ClayComponent.js',
+			'src/ClayComponent.soy',
+			'src/__tests__/ClayComponent.js',
+			'webpack.config.js',
+			'package.json',
+			'README.md',
+		]);
+	});
 
-  it('content of ClayComponent.js', function() {
-    assert.fileContent(
-      'src/ClayComponent.js',
-      /class ClayComponent extends Component/
-    );
-    assert.fileContent(
-      'src/ClayComponent.js',
-      /Soy\.register\(ClayComponent, templates\)/
-    );
-    assert.fileContent('src/ClayComponent.js', /export {ClayComponent}/);
-    assert.fileContent('src/ClayComponent.js', /export default ClayComponent/);
-  });
+	it('content of ClayComponent.js', function() {
+		assert.fileContent(
+			'src/ClayComponent.js',
+			/class ClayComponent extends Component/
+		);
+		assert.fileContent(
+			'src/ClayComponent.js',
+			/Soy\.register\(ClayComponent, templates\)/
+		);
+		assert.fileContent('src/ClayComponent.js', /export {ClayComponent}/);
+		assert.fileContent(
+			'src/ClayComponent.js',
+			/export default ClayComponent/
+		);
+	});
 
-  it('content of ClayComponent.soy', function() {
-    assert.fileContent('src/ClayComponent.soy', /{namespace ClayComponent}/);
-    assert.fileContent(
-      'src/ClayComponent.soy',
-      /<div class="clay-component">Hello World<\/div>/
-    );
-  });
+	it('content of ClayComponent.soy', function() {
+		assert.fileContent(
+			'src/ClayComponent.soy',
+			/{namespace ClayComponent}/
+		);
+		assert.fileContent(
+			'src/ClayComponent.soy',
+			/<div class="clay-component">Hello World<\/div>/
+		);
+	});
 });
