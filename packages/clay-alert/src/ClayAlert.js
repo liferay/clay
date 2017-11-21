cb210e984a8a19731097a912ba6aa3ebde78292e
Carlos Lancha <carlos.lancha@liferay.com>
2017-11-21 12:29:36 +0100

    Create ClayAlert

---
+++ packages/clay-alert/src/ClayAlert.js
@@ -0,0 +1,83 @@
+import './ClayAlertBase';
+import Component from 'metal-component';
+import defineWebComponent from 'metal-web-component';
+import Soy from 'metal-soy';
+import {Config} from 'metal-state';
+
+import templates from './ClayAlert.soy.js';
+
+/**
+ * Metal Clay Alert component.
+ */
+class ClayAlert extends Component {
+	handleHide_(event) {
+		this.emit('hide', event);
+	}
+}
+
+/**
+ * State definition.
+ * @static
+ * @type {!Object}
+ */
+ClayAlert.STATE = {
+	/**
+	 * Flag to indicate if the alert is closeable.
+	 * @instance
+	 * @memberof ClayAlert
+	 * @type {?bool}
+	 * @default false
+	 */
+	closeable: Config.bool().value(false),
+
+	/**
+	 * Flag to indicate if the alert should be destroyen when close.
+	 * @instance
+	 * @memberof ClayAlert
+	 * @type {?bool}
+	 * @default false
+	 */
+	destroyOnHide: Config.bool().value(false),
+
+	/**
+	 * The message of alert
+	 * @instance
+	 * @memberof ClayAlert
+	 * @type {!html|string}
+	 */
+	message: Config.any().required(),
+
+	/**
+	 * The path to the SVG spritemap file containing the icons.
+	 * @instance
+	 * @memberof ClayIcon
+	 * @type {!string}
+	 * @default undefined
+	 */
+	spritemap: Config.string().required(),
+
+	/**
+	 * The style of alert
+	 * @instance
+	 * @memberof ClayAlert
+	 * @type {?string}
+	 * @default info
+	 */
+	style: Config.oneOf(['danger', 'info', 'success', 'warning']).value('info'),
+
+	/**
+	 * The title of alert
+	 * @instance
+	 * @memberof ClayAlert
+	 * @type {!string}
+	 * @default undefined
+	 */
+	title: Config.string().required(),
+};
+
+defineWebComponent('clay-alert', ClayAlert);
+
+Soy.register(ClayAlert, templates);
+
+export {ClayAlert};
+export default ClayAlert;
