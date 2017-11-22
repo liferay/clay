945edab019c689b52a0dd54515ced340059cb92b
Carlos Lancha <carlos.lancha@liferay.com>
2017-11-22 10:26:14 +0100

    Follow same structure in metal.js also in dropdown

--- packages/clay-dropdown/src/all.js
+++
@@ -1,5 +0,0 @@
-import ClayActionsDropdown from './ClayActionsDropdown';
-import ClayDropdownBase from './ClayDropdownBase';
-import ClayDropdown from './ClayDropdown';
-
-export {ClayActionsDropdown, ClayDropdown, ClayDropdownBase};
---
+++ packages/clay-dropdown/src/all/dropdown.js
@@ -0,0 +1,5 @@
+import ClayActionsDropdown from '../ClayActionsDropdown';
+import ClayDropdownBase from '../ClayDropdownBase';
+import ClayDropdown from '../ClayDropdown';
+
+export {ClayActionsDropdown, ClayDropdown, ClayDropdownBase};
