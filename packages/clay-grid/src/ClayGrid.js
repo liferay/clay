d1227eb344e4f1c820e96f9285f54ba552354b24
Carlos Lancha <carlos.lancha@liferay.com>
2017-12-14 12:53:31 +0100

    Render grid with ClayFileCards

--- packages/clay-grid/src/ClayGrid.js
+++ packages/clay-grid/src/ClayGrid.js
@@ -1,3 +1,4 @@
+import 'clay-card';
 import Component from 'metal-component';
 import defineWebComponent from 'metal-web-component';
 import Soy from 'metal-soy';
@@ -8,7 +9,16 @@ import templates from './ClayGrid.soy.js';
 /**
  * Metal ClayGrid component.
  */
-class ClayGrid extends Component {}
+class ClayGrid extends Component {
+	/**
+	 * Continues the propagation of the checkbox changed event
+	 * @param {!Event} event
+	 * @private
+	 */
+	handleItemToggled_(event) {
+		this.emit('itemToggled', event);
+	}
+}
 
 /**
  * State definition.
@@ -33,6 +43,79 @@ ClayGrid.STATE = {
 	 * @default undefined
 	 */
 	id: Config.string(),
+
+	/**
+	 * List of items.
+	 * @instance
+	 * @memberof ClayGrid
+	 * @type {!array}
+	 * @default undefined
+	 */
+	items: Config.arrayOf(
+		Config.shapeOf({
+			actionItems: Config.arrayOf(
+				Config.shapeOf({
+					disabled: Config.bool().value(false),
+					href: Config.string().required(),
+					icon: Config.string(),
+					label: Config.string().required(),
+					quickAction: Config.bool(),
+					separator: Config.bool().value(false),
+				})
+			),
+			description: Config.string(),
+			disabled: Config.bool(),
+			fileType: Config.string(),
+			fileTypeStyle: Config.string(),
+			href: Config.string(),
+			icon: Config.string(),
+			iconShape: Config.oneOf(['circle', 'rounded']).value('rounded'),
+			labels: Config.arrayOf(
+				Config.shapeOf({
+					label: Config.string(),
+					style: Config.oneOf([
+						'danger',
+						'info',
+						'secondary',
+						'success',
+						'warning',
+					]).value('secondary'),
+				})
+			),
+			selected: Config.bool().value(false),
+			title: Config.string().required(),
+			type: Config.oneOf(['file', 'folder', 'image', 'user']).value(
+				'image'
+			),
+		})
+	).required(),
+
+	/**
+	 * Flag to indicate if the list group items are selectable.
+	 * @instance
+	 * @memberof ClayGrid
+	 * @type {?bool|undefined}
+	 * @default false
+	 */
+	selectable: Config.bool().value(false),
+
+	/**
+	 * The path to the SVG spritemap file containing the icons.
+	 * @instance
+	 * @memberof ClayGrid
+	 * @type {?string|undefined}
+	 * @default undefined
+	 */
+	spritemap: Config.string(),
+
+	/**
+	 * Header of the list group.
+	 * @instance
+	 * @memberof ClayGrid
+	 * @type {?string|undefined}
+	 * @default undefined
+	 */
+	title: Config.string(),
 };
 
 defineWebComponent('clay-grid', ClayGrid);
