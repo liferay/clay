0ff3750edde738b0ae427d4dd3a4df21b007d602
Carlos Lancha <carlos.lancha@liferay.com>
2017-12-20 12:40:56 +0100

    Add schema and table header to ClayTable

--- packages/clay-table/src/ClayTable.js
+++ packages/clay-table/src/ClayTable.js
@@ -1,3 +1,4 @@
+import 'clay-link';
 import Component from 'metal-component';
 import defineWebComponent from 'metal-web-component';
 import Soy from 'metal-soy';
@@ -8,7 +9,18 @@ import templates from './ClayTable.soy.js';
 /**
  * Metal ClayTable component.
  */
-class ClayTable extends Component {}
+class ClayTable extends Component {
+	/**
+	 * Continues the propagation of the column sorting button click event
+	 * @param {!Event} event
+	 * @private
+	 */
+	handleSortingClick_(event) {
+		event.preventDefault();
+
+		this.emit('sortingButtonClicked', event);
+	}
+}
 
 /**
  * State definition.
@@ -35,6 +47,22 @@ ClayTable.STATE = {
 	id: Config.string(),
 
 	/**
+	 * Schema of the table containing an element per column with label and the
+	 * name of the field with the value.
+	 * @instance
+	 * @memberof ClayTable
+	 * @type {!array}
+	 * @default undefined
+	 */
+	schema: Config.arrayOf(
+		Config.shapeOf({
+			fieldName: Config.string().required(),
+			label: Config.string(),
+			sortingOrder: Config.oneOf(['asc', 'desc']),
+		})
+	).required(),
+
+	/**
 	 * Table responsive sizes. Available `lg`, `md`, `sm` and `xl`.
 	 * @instance
 	 * @memberof ClayTable
@@ -42,6 +70,15 @@ ClayTable.STATE = {
 	 * @default undefined
 	 */
 	size: Config.oneOf(['lg', 'md', 'sm', 'xl']),
+
+	/**
+	 * The path to the SVG spritemap file containing the icons.
+	 * @instance
+	 * @memberof ClayTable
+	 * @type {?string|undefined}
+	 * @default undefined
+	 */
+	spritemap: Config.string(),
 };
 
 defineWebComponent('clay-table', ClayTable);
