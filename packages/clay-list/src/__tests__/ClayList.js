6b07a6b480c2efe0bac09348521d249206566cc1
Carlos Lancha <carlos.lancha@liferay.com>
2017-12-29 12:26:23 +0100

    Update ClayList test

--- packages/clay-list/src/__tests__/ClayList.js
+++ packages/clay-list/src/__tests__/ClayList.js
@@ -2,6 +2,146 @@ import ClayList from '../ClayList';
 
 let component;
 
+const actionItems = [
+	{
+		href: '#1',
+		icon: 'trash',
+		label: 'Option 1',
+	},
+	{
+		href: '#2',
+		icon: 'download',
+		label: 'Option 2',
+		separator: true,
+	},
+	{
+		href: '#3',
+		icon: 'info-circle-open',
+		label: 'Option 3',
+	},
+];
+
+const actionItemsWithQuickItems = [
+	{
+		href: '#1',
+		icon: 'trash',
+		label: 'Option 1',
+		quickAction: true,
+	},
+	{
+		href: '#2',
+		icon: 'download',
+		label: 'Option 2',
+		quickAction: true,
+		separator: true,
+	},
+	{
+		href: '#3',
+		icon: 'info-circle-open',
+		label: 'Option 3',
+		quickAction: true,
+	},
+];
+
+const sagaItems = [
+	{
+		director: 'George Lucas',
+		downloadHref: '#',
+		rating: 10,
+		releaseDate: 'May 19th 1999',
+		status: 'Error',
+		title: 'Episode I: The Phantom Menace',
+	},
+	{
+		director: 'George Lucas',
+		downloadHref: '#',
+		rating: 50,
+		releaseDate: 'May 12th 2002',
+		status: 'Watched',
+		title: 'Episode II: Attack of the Clones',
+	},
+	{
+		director: 'George Lucas',
+		downloadHref: '#',
+		rating: 60,
+		releaseDate: 'May 12th 2005',
+		status: 'Watched',
+		title: 'Episode III: Revenge of the Sith',
+	},
+	{
+		director: 'George Lucas',
+		downloadHref: '#',
+		rating: 90,
+		releaseDate: 'May 25th 1977',
+		status: 'Watched',
+		title: 'Episode IV: A New Hope',
+	},
+	{
+		director: 'Irvin Kershner',
+		downloadHref: '#',
+		rating: 100,
+		releaseDate: 'May 21th 1980',
+		status: 'Watched',
+		title: 'Episode V: The Empire Strikes Back',
+	},
+	{
+		director: 'Richard Marquand',
+		downloadHref: '#',
+		rating: 90,
+		releaseDate: 'May 25th 1983',
+		status: 'Watched',
+		title: 'Episode VI: Return of the Jedi',
+	},
+	{
+		director: 'J. J. Abrams',
+		downloadHref: '#',
+		rating: 70,
+		releaseDate: 'December 14th 2015',
+		status: 'Watched',
+		title: 'Episode VII: The Force Awakens',
+	},
+	{
+		director: 'Rian Johnson',
+		downloadHref: '#',
+		rating: 0,
+		releaseDate: 'December 9th 2017',
+		status: 'Pending',
+		title: 'Episode VIII: The Last Jedi',
+	},
+];
+
+const expandedItems = [
+	{
+		director: 'Gareth Edwards',
+		downloadHref: '#',
+		rating: 70,
+		releaseDate: 'December 10th 2016',
+		status: 'Watched',
+		title: 'Rogue One: A Star Wars Story',
+	},
+	{
+		director: 'Ron Howard',
+		downloadHref: '#',
+		rating: 0,
+		releaseDate: 'May 25th 2018',
+		status: 'Pending',
+		title: 'Solo: A Star Wars Story',
+	},
+];
+
+const groupedItems = [
+	{
+		items: sagaItems,
+		label: 'Saga',
+	},
+	{
+		items: expandedItems,
+		label: 'Expanded Universe',
+	},
+];
+
+const spritemap = '../node_modules/lexicon-ux/build/images/icons/icons.svg';
+
 describe('ClayList', function() {
 	afterEach(() => {
 		if (component) {
@@ -11,7 +151,7 @@ describe('ClayList', function() {
 
 	it('should render the default markup', () => {
 		component = new ClayList({
-			schema: {},
+			schema: [],
 		});
 
 		expect(component).toMatchSnapshot();
@@ -35,16 +175,103 @@ describe('ClayList', function() {
 		expect(component).toMatchSnapshot();
 	});
 
-	it('should render a selectable ClayList', () => {
+	it('should render a ClayList with simple items', () => {
 		component = new ClayList({
-			items: [
+			items: sagaItems,
+			schema: [
 				{
-					title: 'Star Wars',
+					contentRenderer: 'simple',
+					fieldsMap: {
+						title: 'title',
+					},
 				},
 			],
-			schema: {
-				title: 'title',
-			},
+		});
+
+		expect(component).toMatchSnapshot();
+	});
+
+	it('should render a ClayList with simple items with links', () => {
+		component = new ClayList({
+			items: sagaItems,
+			schema: [
+				{
+					contentRenderer: 'simple',
+					fieldsMap: {
+						href: 'downloadHref',
+						title: 'title',
+					},
+				},
+			],
+		});
+
+		expect(component).toMatchSnapshot();
+	});
+
+	it('should render a ClayList with items', () => {
+		component = new ClayList({
+			items: sagaItems,
+			schema: [
+				{
+					contentRenderer: 'content',
+					fieldsMap: {
+						title: 'title',
+					},
+				},
+			],
+		});
+
+		expect(component).toMatchSnapshot();
+	});
+
+	it('should render a ClayList with items with links', () => {
+		component = new ClayList({
+			items: sagaItems,
+			schema: [
+				{
+					contentRenderer: 'content',
+					fieldsMap: {
+						href: 'downloadHref',
+						title: 'title',
+					},
+				},
+			],
+		});
+
+		expect(component).toMatchSnapshot();
+	});
+
+	it('should render a ClayList with items with description', () => {
+		component = new ClayList({
+			items: sagaItems,
+			schema: [
+				{
+					contentRenderer: 'content',
+					fieldsMap: {
+						description: 'director',
+						href: 'downloadHref',
+						title: 'title',
+					},
+				},
+			],
+		});
+
+		expect(component).toMatchSnapshot();
+	});
+
+	it('should render a selectable ClayList', () => {
+		component = new ClayList({
+			items: sagaItems,
+			schema: [
+				{
+					contentRenderer: 'content',
+					fieldsMap: {
+						description: 'director',
+						href: 'downloadHref',
+						title: 'title',
+					},
+				},
+			],
 			selectable: true,
 		});
 
@@ -53,19 +280,218 @@ describe('ClayList', function() {
 
 	it('should render a selectable ClayList with groups', () => {
 		component = new ClayList({
-			items: [
+			items: groupedItems,
+			schema: [
 				{
-					items: [
-						{
-							title: 'Star Wars',
-						},
-					],
-					label: 'Movies',
+					contentRenderer: 'content',
+					fieldsMap: {
+						description: 'director',
+						href: 'downloadHref',
+						title: 'title',
+					},
 				},
 			],
-			schema: {
-				title: 'title',
-			},
+		});
+
+		expect(component).toMatchSnapshot();
+	});
+
+	it('should render a selectable ClayList with icons', () => {
+		component = new ClayList({
+			items: groupedItems,
+			schema: [
+				{
+					contentRenderer: 'icon',
+					fieldName: 'status',
+					iconsMap: {
+						'Watched': 'check-circle',
+						'*': 'exclamation-circle',
+					},
+				},
+				{
+					contentRenderer: 'content',
+					fieldsMap: {
+						description: 'director',
+						href: 'downloadHref',
+						title: 'title',
+					},
+				},
+			],
+			spritemap: spritemap,
+		});
+
+		expect(component).toMatchSnapshot();
+	});
+
+	it('should render a selectable ClayList with circle icons', () => {
+		component = new ClayList({
+			items: groupedItems,
+			schema: [
+				{
+					contentRenderer: 'icon',
+					fieldName: 'status',
+					iconsMap: {
+						'Watched': 'check-circle',
+						'*': 'exclamation-circle',
+					},
+					iconShapesMap: {
+						'Watched': 'circle',
+						'*': 'rounded',
+					},
+				},
+				{
+					contentRenderer: 'content',
+					fieldsMap: {
+						description: 'director',
+						href: 'downloadHref',
+						title: 'title',
+					},
+				},
+			],
+			spritemap: spritemap,
+		});
+
+		expect(component).toMatchSnapshot();
+	});
+
+	it('should render a selectable ClayList with label', () => {
+		component = new ClayList({
+			items: groupedItems,
+			schema: [
+				{
+					contentRenderer: 'icon',
+					fieldName: 'status',
+					iconsMap: {
+						'Watched': 'check-circle',
+						'*': 'exclamation-circle',
+					},
+					iconShapesMap: {
+						'Watched': 'circle',
+						'*': 'rounded',
+					},
+				},
+				{
+					contentRenderer: 'content',
+					fieldsMap: {
+						description: 'director',
+						href: 'downloadHref',
+						label: 'status',
+						title: 'title',
+					},
+					labelStylesMap: {
+						'Watched': 'success',
+						'Pending': 'warning',
+						'*': 'danger',
+					},
+				},
+			],
+			spritemap: spritemap,
+		});
+
+		expect(component).toMatchSnapshot();
+	});
+
+	it('should render a selectable ClayList with action menu', () => {
+		let itemsWithActionItems = [];
+
+		sagaItems.forEach(item => {
+			let itemWithActionItems = {};
+
+			for (let key in item) {
+				if (Object.prototype.hasOwnProperty.call(item, key)) {
+					itemWithActionItems[key] = item[key];
+				}
+			}
+
+			itemWithActionItems.actionItems = actionItems;
+
+			itemsWithActionItems.push(itemWithActionItems);
+		});
+
+		component = new ClayList({
+			items: itemsWithActionItems,
+			schema: [
+				{
+					contentRenderer: 'icon',
+					fieldName: 'status',
+					iconsMap: {
+						'Watched': 'check-circle',
+						'*': 'exclamation-circle',
+					},
+					iconShapesMap: {
+						'Watched': 'circle',
+						'*': 'rounded',
+					},
+				},
+				{
+					contentRenderer: 'content',
+					fieldsMap: {
+						description: 'director',
+						href: 'downloadHref',
+						label: 'status',
+						title: 'title',
+					},
+					labelStylesMap: {
+						'Watched': 'success',
+						'Pending': 'warning',
+						'*': 'danger',
+					},
+				},
+			],
+			spritemap: spritemap,
+		});
+
+		expect(component).toMatchSnapshot();
+	});
+
+	it('should render a selectable ClayList with quick action menu', () => {
+		let itemsWithActionItems = [];
+
+		sagaItems.forEach(item => {
+			let itemWithActionItems = {};
+
+			for (let key in item) {
+				if (Object.prototype.hasOwnProperty.call(item, key)) {
+					itemWithActionItems[key] = item[key];
+				}
+			}
+
+			itemWithActionItems.actionItems = actionItemsWithQuickItems;
+
+			itemsWithActionItems.push(itemWithActionItems);
+		});
+
+		component = new ClayList({
+			items: itemsWithActionItems,
+			schema: [
+				{
+					contentRenderer: 'icon',
+					fieldName: 'status',
+					iconsMap: {
+						'Watched': 'check-circle',
+						'*': 'exclamation-circle',
+					},
+					iconShapesMap: {
+						'Watched': 'circle',
+						'*': 'rounded',
+					},
+				},
+				{
+					contentRenderer: 'content',
+					fieldsMap: {
+						description: 'director',
+						href: 'downloadHref',
+						label: 'status',
+						title: 'title',
+					},
+					labelStylesMap: {
+						'Watched': 'success',
+						'Pending': 'warning',
+						'*': 'danger',
+					},
+				},
+			],
+			spritemap: spritemap,
 		});
 
 		expect(component).toMatchSnapshot();
@@ -76,14 +502,15 @@ describe('ClayList', function() {
 
 		component = new ClayList({
 			events: {itemToggled: spy},
-			items: [
+			items: groupedItems,
+			schema: [
 				{
-					title: 'Star Wars',
+					contentRenderer: 'simple',
+					fieldsMap: {
+						title: 'title',
+					},
 				},
 			],
-			schema: {
-				title: 'title',
-			},
 			selectable: true,
 		});
 
