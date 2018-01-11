6593d2014e8e67174baa21c04ae1bdaf1782060d
Carlos Lancha <carlos.lancha@liferay.com>
2018-01-11 12:36:11 +0100

    Filter dropdown separators in viewType clicked handler in ClayManagementToolbar

--- packages/clay-management-toolbar/src/ClayManagementToolbar.js
+++ packages/clay-management-toolbar/src/ClayManagementToolbar.js
@@ -155,7 +155,11 @@ class ClayManagementToolbar extends Component {
 	handleViewTypeClicked_(event) {
 		let element = event.delegateTarget;
 		let elementIndex = Array.prototype.indexOf.call(
-			element.parentElement.children,
+			Array.prototype.filter.call(
+				element.parentElement.children,
+				childrenElement =>
+					childrenElement.getAttribute('role') !== 'presentation'
+			),
 			element
 		);
 		let item = this.viewTypes[elementIndex];
