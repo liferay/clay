291f0d1cd04439eb14451a42a423ce5dbeed3b28
Carlos Lancha <carlos.lancha@liferay.com>
2018-01-13 16:19:59 +0100

    Render ClayPagination in ClayPaginationBar

--- packages/clay-pagination-bar/src/ClayPaginationBar.js
+++ packages/clay-pagination-bar/src/ClayPaginationBar.js
@@ -1,3 +1,4 @@
+import 'clay-pagination';
 import Component from 'metal-component';
 import defineWebComponent from 'metal-web-component';
 import Soy from 'metal-soy';
@@ -17,6 +18,24 @@ class ClayPaginationBar extends Component {}
  */
 ClayPaginationBar.STATE = {
 	/**
+	 * Href to create each pagination page complete href.
+	 * @instance
+	 * @memberof ClayPaginationBar
+	 * @type {?string|undefined}
+	 * @default undefined
+	 */
+	baseHref: Config.string(),
+
+	/**
+	 * Current pagination page number.
+	 * @instance
+	 * @memberof ClayPaginationBar
+	 * @type {!number}
+	 * @default undefined
+	 */
+	currentPage: Config.number().required(),
+
+	/**
 	 * CSS classes to be applied to the element.
 	 * @instance
 	 * @memberof ClayPaginationBar
@@ -33,6 +52,24 @@ ClayPaginationBar.STATE = {
 	 * @default undefined
 	 */
 	id: Config.string(),
+
+	/**
+	 * The path to the SVG spritemap file containing the icons.
+	 * @instance
+	 * @memberof ClayPaginationBar
+	 * @type {!string}
+	 * @default undefined
+	 */
+	spritemap: Config.string().required(),
+
+	/**
+	 * Total number of pages to show in the pagination.
+	 * @instance
+	 * @memberof ClayPaginationBar
+	 * @type {!number}
+	 * @default undefined
+	 */
+	totalPages: Config.number().required(),
 };
 
 defineWebComponent('clay-pagination-bar', ClayPaginationBar);
