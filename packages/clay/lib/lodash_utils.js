0382472d7d480ff64d0dbf8afe4887a1fb49ffb7
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2018-01-24 09:19:52 -0800

    Remove unused lodash utils

--- packages/clay/lib/lodash_utils.js
+++ packages/clay/lib/lodash_utils.js
@@ -4,22 +4,6 @@ var REGEX_SUB = /\{\s*([^|}]+?)\s*(?:\|([^}]*))?\s*\}/g;
 
 _.mixin(
 	{
-		bindKeyRight: function(context, key) {
-			var args = _.toArray(arguments).slice(2);
-
-			args.unshift(_.bindKey(context, key));
-
-			return _.partialRight.apply(_, args);
-		},
-
-		bindRight: function(fn, context) {
-			var args = _.toArray(arguments).slice(2);
-
-			args.unshift(_.bind(fn, context));
-
-			return _.partialRight.apply(_, args);
-		},
-
 		cached: function(fn) {
 			return _.memoize(
 				fn,
@@ -41,33 +25,6 @@ _.mixin(
 				}
 			) : string;
 		}
-	}
-);
-
-_.mixin(
-	{
-		namespace: function(obj, path) {
-			if (arguments.length === 1) {
-				path = obj;
-				obj = this;
-			}
-
-			if (_.isString(path)) {
-				path = path.split('.');
-			}
-
-			for (var i = 0; i < path.length; i++) {
-				var name = path[i];
-
-				obj[name] = obj[name] || {};
-				obj = obj[name];
-			}
-
-			return obj;
-		}
-	},
-	{
-		chain: false
 	}
 );
 
