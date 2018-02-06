9d637dd4ad29839580346d5a5dac809bef870228
Matuzalém S. Teles <matuzalemteles@gmail.com>
2018-02-06 19:18:12 -0300

    SF | Fixes #481

--- packages/clay-management-toolbar/src/__tests__/ClayManagementToolbar.js
+++ packages/clay-management-toolbar/src/__tests__/ClayManagementToolbar.js
@@ -608,6 +608,9 @@ describe('ClayManagementToolbar', function() {
 		managementToolbar.refs.infoButton.element.click();
 
 		expect(spy).toHaveBeenCalled();
-		expect(spy).toHaveBeenCalledWith('infoButtonClicked', expect.any(Object));
+		expect(spy).toHaveBeenCalledWith(
+			'infoButtonClicked',
+			expect.any(Object)
+		);
 	});
 });
