e1819e0a851039eedb5e946e73c9172bd3477362
Carlos Lancha <carlos.lancha@liferay.com>
2018-02-20 16:00:03 +0100

    SF | Fixes #571

--- packages/clay-dataset-display/src/__tests__/ClayDatasetDisplay.js
+++ packages/clay-dataset-display/src/__tests__/ClayDatasetDisplay.js
@@ -511,8 +511,7 @@ describe('ClayDatasetDisplay', function() {
 
 		const spy = jest.spyOn(component, 'emit');
 
-		let filtersDropdown =
-			component.refs.managementToolbar.refs.filters;
+		let filtersDropdown = component.refs.managementToolbar.refs.filters;
 		filtersDropdown.refs.portal.refs.dropdownButton.element.click();
 
 		jest.runAllTimers();
