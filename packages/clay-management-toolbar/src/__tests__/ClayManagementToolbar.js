a4cd638385527d988c68eb71754a2458ee68c56f
Matuzalém S. Teles <matuzalemteles@gmail.com>
2018-02-20 11:06:01 -0300

    Add tests | Fixes #572

--- packages/clay-management-toolbar/src/__tests__/ClayManagementToolbar.js
+++ packages/clay-management-toolbar/src/__tests__/ClayManagementToolbar.js
@@ -391,6 +391,29 @@ describe('ClayManagementToolbar', function() {
 		expect(managementToolbar).toMatchSnapshot();
 	});
 
+	it('should render a management toolbar with search results bar', () => {
+		managementToolbar = new ClayManagementToolbar({
+			id: 'myId',
+			searchValue: 'foo',
+			selectable: true,
+			spritemap: spritemap,
+			totalItems: 10,
+		});
+
+		expect(managementToolbar).toMatchSnapshot();
+	});
+
+	it('should render a management toolbar with search results bar only when totalItems is greater than zero', () => {
+		managementToolbar = new ClayManagementToolbar({
+			id: 'myId',
+			searchValue: 'foo',
+			selectable: true,
+			spritemap: spritemap,
+		});
+
+		expect(managementToolbar).toMatchSnapshot();
+	});
+
 	it('should render a selectable management toolbar and emit an event on checkbox click', () => {
 		managementToolbar = new ClayManagementToolbar({
 			selectable: true,
@@ -641,6 +664,25 @@ describe('ClayManagementToolbar', function() {
 		expect(spy).toHaveBeenCalled();
 		expect(spy).toHaveBeenCalledWith(
 			'infoButtonClicked',
+			expect.any(Object)
+		);
+	});
+
+	it('should render a management toolbar and emit an event on clear button click', () => {
+		managementToolbar = new ClayManagementToolbar({
+			searchValue: 'foo',
+			selectable: true,
+			spritemap: spritemap,
+			totalItems: 10,
+		});
+
+		const spy = jest.spyOn(managementToolbar, 'emit');
+
+		managementToolbar.refs.clearSearch.element.click();
+
+		expect(spy).toHaveBeenCalled();
+		expect(spy).toHaveBeenCalledWith(
+			'clearButtonClicked',
 			expect.any(Object)
 		);
 	});
