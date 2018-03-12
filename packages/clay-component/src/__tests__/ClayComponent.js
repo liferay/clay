bbcdf9c4787747e62a987276d3edd510b4de267d
Alejandro Tardín <alejandro.tardin@liferay.com>
2018-03-12 17:24:13 +0100

    SF

--- packages/clay-component/src/__tests__/ClayComponent.js
+++ packages/clay-component/src/__tests__/ClayComponent.js
@@ -55,6 +55,8 @@ describe('ClayComponent', function() {
 	it('should create a ClayComponent and element.getAttribute must return null in case no attribute exists', () => {
 		component = new MyComponent();
 
-		expect(component.element.getAttribute('non-existing-attribute')).toBe('');
+		expect(component.element.getAttribute('non-existing-attribute')).toBe(
+			''
+		);
 	});
 });
