ae698daedc1053f3e1199ee7340e8223a15d7c86
Patrick Yeo <patrick.yeo@liferay.com>
2018-04-18 13:42:32 -0700

    (Fixes #806) Clay Component Navigation Bar should remove `height` on `transitionend`

--- packages/clay-navigation-bar/src/ClayNavigationBar.js
+++ packages/clay-navigation-bar/src/ClayNavigationBar.js
@@ -23,20 +23,10 @@ class ClayNavigationBar extends Component {
 	 */
 	// eslint-disable-next-line
 	sync_isTransitioning() {
-		const elementCollapse = this.element.querySelector('.navbar-collapse');
-
 		if (this._isTransitioning && !this._visible) {
-			const heightCollapse = elementCollapse.querySelector(
-				'.container-fluid.container-fluid-max-xl'
-			).clientHeight;
-
-			elementCollapse.setAttribute(
-				'style',
-				`height: ${heightCollapse}px`
-			);
+			this._setCollapseHeight();
 		} else if (this._isTransitioning && this._visible) {
-			elementCollapse.setAttribute('style', 'height: 0');
-			elementCollapse.removeAttribute('style');
+			this._removeCollapseHeight();
 		}
 	}
 
@@ -45,6 +35,10 @@ class ClayNavigationBar extends Component {
 	 * @private
 	 */
 	_handleClickToggler() {
+		if (this._visible && !this._isTransitioning) {
+			this._setCollapseHeight();
+		}
+
 		if (!this._isTransitioning) {
 			this._isTransitioning = true;
 		}
@@ -64,10 +58,36 @@ class ClayNavigationBar extends Component {
 		) {
 			this._visible = true;
 			this._isTransitioning = false;
+			this._removeCollapseHeight();
 		} else if (element == event.target) {
 			this._visible = false;
 			this._isTransitioning = false;
 		}
+	}
+
+	/**
+	 * Removes height css property on `.navbar-collapse`
+	 * @private
+	 */
+	_removeCollapseHeight() {
+		const elementCollapse = this.element.querySelector('.navbar-collapse');
+		elementCollapse.style.removeProperty('height');
+	}
+
+	/**
+	 * Sets the height css property on `.navbar-collapse`
+	 * @private
+	 */
+	_setCollapseHeight() {
+		const elementCollapse = this.element.querySelector('.navbar-collapse');
+		const heightCollapse = elementCollapse.querySelector(
+			'.container-fluid'
+		).clientHeight;
+
+		elementCollapse.setAttribute(
+			'style',
+			`height: ${heightCollapse}px`
+		)
 	}
 }
 
