201e29fb69463b65c9e4070dc83f3b84d0af8130
Chema Balsas <jose.balsas@liferay.com>
2018-04-25 12:11:15 +0200

    Updates lock

--- packages/clay/src/clay.js
+++ packages/clay/src/clay.js
@@ -1,18 +1,39 @@
-import ClayAlert from 'clay-alert';
+import {
+	ClayAlert,
+	ClayAlertBase,
+	ClayStripe,
+	ClayToast,
+} from 'clay-alert';
+
 import ClayBadge from 'clay-badge';
 import ClayButton from 'clay-button';
-import ClayCard from 'clay-card';
+
+import {
+	ClayCardBase,
+	ClayFileCard,
+	ClayHorizontalCard,
+	ClayImageCard,
+	ClayUserCard,
+} from 'clay-card';
+
 import ClayCardGrid from 'clay-card-grid';
 import ClayCheckbox from 'clay-checkbox';
 import ClayCollapse from 'clay-collapse';
 import ClayComponent from 'clay-component';
 import ClayDatasetDisplay from 'clay-dataset-display';
-import ClayDropdown from 'clay-dropdown';
+
+import {
+	ClayActionsDropdown,
+	ClayCreationMenuDropdown,
+	ClayDropdown,
+	ClayDropdownBase,
+} from 'clay-dropdown';
+
 import ClayIcon from 'clay-icon';
 import ClayLabel from 'clay-label';
 import ClayLink from 'clay-link';
 import ClayList from 'clay-list';
-import ClayManagementToolbar from 'clay-management-toolbar';
+import {ClayManagementToolbar} from 'clay-management-toolbar';
 import ClayModal from 'clay-modal';
 import ClayNavigationBar from 'clay-navigation-bar';
 import ClayPagination from 'clay-pagination';
@@ -26,15 +47,25 @@ import ClayTable from 'clay-table';
 
 export {
 	ClayAlert,
+	ClayAlertBase,
+	ClayStripe,
+	ClayToast,
 	ClayBadge,
 	ClayButton,
-	ClayCard,
+	ClayCardBase,
+	ClayFileCard,
+	ClayHorizontalCard,
+	ClayImageCard,
+	ClayUserCard,
 	ClayCardGrid,
 	ClayCheckbox,
 	ClayCollapse,
 	ClayComponent,
 	ClayDatasetDisplay,
+	ClayActionsDropdown,
+	ClayCreationMenuDropdown,
 	ClayDropdown,
+	ClayDropdownBase,
 	ClayIcon,
 	ClayLabel,
 	ClayLink,
