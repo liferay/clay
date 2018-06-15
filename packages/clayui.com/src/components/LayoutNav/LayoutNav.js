ba189be236d26bbdae82c1268a462eb661c03486
Diego Nascimento <diego.nascimento@liferay.com>
2018-06-15 19:16:50 -0300

    Adds a LayoutNav component

---
+++ packages/clayui.com/src/components/LayoutNav/LayoutNav.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+
+class LayoutNav extends Component {
+    
+    constructor() {
+        super();
+        this.node = window;
+    }
+
+    getScrollTop() {
+        if (this.node == window) {
+            return this.node.pageYOffset;
+        }
+        if (this.node == document) {
+            return this.node.defaultView.pageYOffset;
+        }
+        return this.node.scrollTop;
+    }
+
+    addScroll() {
+        let reference = document.querySelector('.navbar-clay-site');
+
+        this.node.addEventListener('scroll', evt => {
+            if (this.getScrollTop() >= 50) {
+                reference.classList.add('scroll');
+            } else {
+                reference.classList.remove('scroll');
+            }
+        });
+    }
+    
+    componentDidMount() {
+        this.addScroll();
+    }
+
+    componentWillUnmount() {
+        this.node.removeEventListener('scroll');
+    }
+
+    render() {
+        return (
+            <nav className="navbar fixed-top navbar-clay-site navbar-expand-lg navbar-dark">
+                <div className="container-fluid container-fluid-max-lg">
+                    <div className="navbar-brand">
+                        <img className="logo mr-2" src="/images/home/clay_logo_w.svg" alt="" />
+                        <span className="title align-middle">Clay</span>
+                    </div>
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item">
+                            <a className="nav-link ml-3" href="/docs/getting_started/introduction.html">Getting Started</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link ml-3" href="/docs/components/alerts.html">
+                                Components Library
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="mx-3 mr-lg-0" href="https://github.com/liferay/clay" target="_blank">
+                                <img src="/images/home/GitHub-Mark-64px.svg" alt="" />
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        );
+    }
+};
+
+export default LayoutNav;
