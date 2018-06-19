c1cf17eb227fc9b7c2e045b251878eb7737747eb
Diego Nascimento <diego.nascimento@liferay.com>
2018-06-19 15:20:45 -0300

    Fix to build wowjs without window is defined

--- packages/clayui.com/gatsby-node.js
+++ packages/clayui.com/gatsby-node.js
@@ -1,2 +1,10 @@
 exports.createPages = require('./gatsby/createPages');
 exports.onCreateNode = require('./gatsby/onCreateNode');
+exports.modifyWebpackConfig = ({ config, stage }) => {
+    if (stage === "build-html") {
+        config.loader("null", {
+            test: /wowjs/,
+            loader: "null-loader",
+        });
+    }
+};
