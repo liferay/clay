85a6105c8f1f3b6a96e0054312c076f52ed8fb89
Matuzalém S. Teles <matuzalemteles@gmail.com>
2018-06-20 00:03:37 -0300

    Add Helmet to docs template

--- packages/clayui.com/src/templates/docs.js
+++ packages/clayui.com/src/templates/docs.js
@@ -1,14 +1,29 @@
 import React from "react";
+import Helmet from "react-helmet"
 
 import docsSidebar from "../data/sidebars/doc-links.yaml"
 import Sidebar from '../components/Sidebar';
 
 export default function Docs({data, location}) {
     const { markdownRemark } = data;
-    const { frontmatter, html } = markdownRemark;
+    const { frontmatter, html, excerpt, timeToRead } = markdownRemark;
+    const title = `${frontmatter.title} - Clay`;
 
     return (
         <div className="docs">
+            <Helmet>
+                <title>{title}</title>
+                <meta name="description" content={excerpt} />
+                <meta name="og:description" content={excerpt} />
+                <meta name="twitter:description" content={excerpt} />
+                <meta name="og:title" content={title} />
+                <meta name="og:type" content="article" />
+                <meta name="twitter.label1" content="Reading time" />
+                <meta
+                    name="twitter:data1"
+                    content={`${timeToRead} min read`}
+                />
+            </Helmet>
             <main className="content">
                 <Sidebar sectionsList={docsSidebar} location={location} />
                 <div className="sidebar-offset">
@@ -37,6 +52,8 @@ export const pageQuery = graphql`
     query TemplateDocsMarkdown($slug: String!) {
         markdownRemark(fields: { slug: { eq: $slug } }) {
             html
+            excerpt
+            timeToRead
             frontmatter {
                 title
             }
