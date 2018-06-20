a57b99678658c21496fc15a13de2be7f38ce51ad
Matuzalém S. Teles <matuzalemteles@gmail.com>
2018-06-20 16:33:13 -0300

    Add Algolia DocSearch to Search component

--- packages/clayui.com/src/components/Sidebar/Search.js
+++ packages/clayui.com/src/components/Sidebar/Search.js
@@ -1,25 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Link, { withPrefix } from "gatsby-link";
 
-const Search = ({placeholder}) => (
-    <div className="page-autocomplete">
-		<div className="form-group">
-			<div className="input-group">
-				<input
-					className="form-control"
-					name="q"
-					placeholder={placeholder}
-					required
-					type="text"
-				/>
+class Search extends Component {
+	state = {
+		enabled: true
+	};
 
-				<span className="input-group-addon">
-					<svg className="lexicon-icon">
-						<use xlinkHref="#search" />
-					</svg>
-				</span>
+	componentDidMount() {
+		if (window.docsearch) {
+			window.docsearch({
+				apiKey: 'bc205a621e5176b8720081c2a3de450c',
+				indexName: 'clay',
+				inputSelector: '#algolia-doc-search',
+			});
+		} else {
+			this.setState({enabled: false});
+		}
+	}
+
+	render() {
+		const { placeholder } = this.props;
+
+		return(
+			<div className="page-autocomplete">
+				<div className="form-group">
+					<div className="input-group">
+						<input
+							className="form-control"
+							id="algolia-doc-search"
+							name="q"
+							placeholder={placeholder}
+							required
+							type="text"
+						/>
+
+						<span className="input-group-addon">
+							<svg className="lexicon-icon">
+								<use xlinkHref={withPrefix('/images/icons/icons.svg#search')} />
+							</svg>
+						</span>
+					</div>
+				</div>
 			</div>
-		</div>
-	</div>
-);
+		);
+	}	
+}
 
 export default Search;
