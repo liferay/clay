b7fcb17d9fdd87db951be5e3dc44fd2240072d38
Matuzalém S. Teles <matuzalemteles@gmail.com>
2018-06-24 00:14:30 -0300

    Ignore ClayTooltip and MetalClipboard at compile time

--- packages/clayui.com/gatsby/modifyWebpackConfig.js
+++ packages/clayui.com/gatsby/modifyWebpackConfig.js
@@ -1,7 +1,7 @@
 module.exports = ({ config, stage }) => {
     if (stage === "build-html") {
         config.loader("null", {
-            test: /wowjs|clay-charts|clay-alert|clay-badge|clay-button|clay-dropdown/,
+            test: /wowjs|clay-charts|clay-alert|clay-badge|clay-button|clay-dropdown|clay-tooltip|metal-clipboard/,
             loader: "null-loader",
         });
     }
