f0a38a48211019e3442076db0b73237048f4b1bb
Carlos Lancha <carlos.lancha@liferay.com>
2018-08-27 21:39:06 +0200

    Update tests | Fixes #1112

---
+++ packages/clay-management-toolbar/src/__tests__/ClayResultsBar.js
@@ -0,0 +1,101 @@
+import ClayResultsBar from '../ClayResultsBar';
+
+let resultsBar;
+
+const filterLabels = [
+	{
+		label: 'Label 1',
+	},
+	{
+		label: 'Label 2',
+	},
+	{
+		label: 'Label 3',
+	},
+];
+
+const spritemap = '../node_modules/clay-css/lib/images/icons/icons.svg';
+
+describe('ClayResultsBar', function() {
+	afterEach(() => {
+		if (resultsBar) {
+			resultsBar.dispose();
+		}
+	});
+
+	it('should render the default markup', () => {
+		resultsBar = new ClayResultsBar({
+			spritemap: spritemap,
+			totalItems: 10,
+		});
+
+		expect(resultsBar).toMatchSnapshot();
+	});
+
+	it('should render a results bar with search value', () => {
+		resultsBar = new ClayResultsBar({
+			searchValue: 'my search',
+			spritemap: spritemap,
+			totalItems: 10,
+		});
+
+		expect(resultsBar).toMatchSnapshot();
+	});
+
+	it('should render a results bar with filter labels', () => {
+		resultsBar = new ClayResultsBar({
+			filterLabels: filterLabels,
+			spritemap: spritemap,
+			totalItems: 10,
+		});
+
+		expect(resultsBar).toMatchSnapshot();
+	});
+
+	it('should render a results bar with filter labels and search value', () => {
+		resultsBar = new ClayResultsBar({
+			filterLabels: filterLabels,
+			searchValue: 'my search',
+			spritemap: spritemap,
+			totalItems: 10,
+		});
+
+		expect(resultsBar).toMatchSnapshot();
+	});
+
+	it('should render a results bar and emit an event on clear button click', () => {
+		resultsBar = new ClayResultsBar({
+			searchValue: 'my search',
+			spritemap: spritemap,
+			totalItems: 10,
+		});
+
+		const spy = jest.spyOn(resultsBar, 'emit');
+
+		resultsBar.refs.clearSearch.element.click();
+
+		expect(spy).toHaveBeenCalled();
+		expect(spy).toHaveBeenCalledWith(
+			'clearButtonClicked',
+			expect.any(Object)
+		);
+	});
+
+	it('should render a results bar and emit an event on close label click', () => {
+		resultsBar = new ClayResultsBar({
+			filterLabels: filterLabels,
+			spritemap: spritemap,
+			totalItems: 10,
+		});
+
+		const spy = jest.spyOn(resultsBar, 'emit');
+
+		resultsBar.refs.label0.refs.closeButton.element.click();
+
+		expect(spy).toHaveBeenCalled();
+		expect(spy).toHaveBeenCalledWith(
+			'filterLabelCloseClicked',
+			expect.any(Object)
+		);
+	});
+});
