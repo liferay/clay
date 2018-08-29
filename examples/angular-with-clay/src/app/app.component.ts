8816702b00a937af68cd49801dcac450245fe1c7
Matuzalém Teles <matuzalemteles@gmail.com>
2018-08-29 17:16:13 -0300

    SF

--- examples/angular-with-clay/src/app/app.component.ts
+++ examples/angular-with-clay/src/app/app.component.ts
@@ -1,16 +1,16 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 import 'clay-button';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  count = 0;
+    count = 0;
 
-  add() {
-    this.count += 1;
-  }
+    add() {
+      this.count += 1;
+    }
 }
--- examples/react-with-clay/src/index.js
+++ examples/react-with-clay/src/index.js
@@ -1,6 +1,8 @@
+// eslint-disable-next-line
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+// eslint-disable-next-line
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
