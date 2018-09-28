10ed4876669cc9b79f63520c152301d8b0d0b87c
Matuzalém Teles <matuzalemteles@gmail.com>
2018-09-28 15:35:47 -0300

    Fixes #1205 - Adding main.scss to be loaded in gatsby-browser.js

---
+++ clayui.com/gatsby-browser.js
@@ -0,0 +1,7 @@
+'use strict';
+
+require('./src/styles/main.scss');
+
+// A stub function is needed because gatsby won't load this file otherwise
+// (https://github.com/gatsbyjs/gatsby/issues/6759)
+exports.onClientEntry = () => {};
