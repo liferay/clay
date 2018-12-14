c5f8191cc4c58c7cea0e2f11c6186179cf2318a4
Matuzalém Teles <matuzalemteles@gmail.com>
2018-12-14 15:53:12 -0300

    Fixes #1393 - Replace itemSelected in favor of itemClicked

--- packages/clay-dropdown/src/ClayActionsDropdown.js
+++ packages/clay-dropdown/src/ClayActionsDropdown.js
@@ -55,20 +55,6 @@ class ClayActionsDropdown extends ClayComponent {
 	}
 
 	/**
-	 * Continues the propagation of the item clicked event
-	 * @param {!Event} event
-	 * @protected
-	 * @return {Boolean} If the event has been prevented or not.
-	 */
-	_handleItemSelected(event) {
-		return !this.emit({
-			data: event.data,
-			name: 'itemSelected',
-			originalEvent: event,
-		});
-	}
-
-	/**
 	 * Continues the propagation of the toggle event.
 	 * @param {!Event} event
 	 * @return {Boolean} If the event has been prevented or not.
