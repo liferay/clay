a461503970f36cddc85f1c91eeaff267c1f4a9e4
Matuzalém Teles <matuzalemteles@gmail.com>
2019-02-08 23:57:04 -0300

    Rewrites the LayoutNav and uses the Search in LayoutNav and makes a hook component

--- clayui.com/src/components/LayoutNav/LayoutNav.js
+++ clayui.com/src/components/LayoutNav/LayoutNav.js
@@ -1,73 +1,54 @@
-import { window, document } from 'browser-monads';
-import React, { Component } from 'react';
-import { Link } from "gatsby";
-import classnames from 'classnames';
+/**
+ * © 2018 Liferay, Inc. <https://liferay.com>
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
 
-class LayoutNav extends Component {
-    constructor() {
-        super();
-        this._rootNode = window || document;
-        this._addScroll = this._addScroll.bind(this);
-    }
+import React from 'react';
+import {Link} from 'gatsby';
 
-    _getScrollTop() {
-        if (this._rootNode === window) {
-            return this._rootNode.pageYOffset;
-        }
-        if (this._rootNode === document) {
-            return this._rootNode.defaultView.pageYOffset;
-        }
-    }
+import Search from './Search';
 
-    _addScroll() {
-        if (this._getScrollTop() >= 50) {
-            this.refs.navElement.classList.add('scroll');
-        } else {
-            this.refs.navElement.classList.remove('scroll');
-        }
-    }
-
-    componentDidMount() {
-        this._rootNode.addEventListener('scroll', this._addScroll, false);
-    }
-
-    componentWillUnmount() {
-        this._rootNode.removeEventListener('scroll', this._addScroll, false);
-    }
-
-    render() {
-        const { fixed = true } = this.props;
-
-        const styles = classnames('navbar navbar-clay-site navbar-expand-lg navbar-dark', {
-            'fixed-top': fixed,
-        });
-
-        return (
-            <nav ref="navElement" className={styles}>
-                <div className="container-fluid container-fluid-max-lg">
-                    <Link to="/" className="navbar-brand">
-                        <img className="logo mr-2" src="/images/home/clay_logo_w.svg" alt="" />
-                        <span className="title align-middle">Clay</span>
-                    </Link>
-                    <ul className="navbar-nav ml-auto">
-                        <li className="nav-item">
-                            <Link className="nav-link ml-3" to="/docs/get-started/introduction.html">Get Started</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link ml-3" to="/docs/components/alerts.html">
-                                Components Library
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <a className="mx-3 mr-lg-0" href="https://github.com/liferay/clay" target="_blank">
-                                <img src="/images/home/GitHub-Mark-64px.svg" alt="" />
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        );
-    }
-};
-
-export default LayoutNav;
+export default () => (
+	<React.Fragment>
+		<div className="clay-site-alert alert alert-dark alert-fixed fixed-top alert-fluid">
+			<b>Clay V3 is currently in beta</b> for ready-to-use components, use <a href="https://clayui.com" target="_blank">Clay V2</a>.
+		</div>
+		<nav className="navbar navbar-clay-site navbar-expand-lg navbar-dark fixed-top">
+			<div className="container-fluid">
+				<Link className="navbar-brand" to="/">
+					<img className="logo mr-2" src="/images/clay_logo_w.png" alt="Clay" />
+					<span className="title align-middle">Clay</span>
+				</Link>
+				<ul className="navbar-nav ml-auto">
+					<li className="nav-item">
+						<Link className="nav-link ml-3" to="/docs/get-started/introduction.html">Get Started</Link>
+					</li>
+					<li className="nav-item">
+						<Link className="nav-link ml-3" to="/docs/css">
+							Style Guide
+						</Link>
+					</li>
+					<li className="nav-item">
+						<Link className="nav-link ml-3" to="/docs/components">
+							Components Library
+						</Link>
+					</li>
+					<li className="nav-item">
+						<Link className="nav-link ml-3" to="/blog">
+							Blog
+						</Link>
+					</li>
+					<li className="nav-item">
+						<Search placeholder="Search..." />
+					</li>
+					<li className="nav-item">
+						<a className="mx-3 mr-lg-0" href="https://github.com/liferay/clay" target="_blank">
+							<img src="/images/home/GitHub-Mark-64px.svg" alt="" />
+						</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	</React.Fragment>
+);
