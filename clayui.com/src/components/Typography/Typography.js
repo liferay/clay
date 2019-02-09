f44e808ae3f3764e6b58ee30f33e75b8535888c6
Matuzalém Teles <matuzalemteles@gmail.com>
2019-02-08 23:59:26 -0300

    Adds id to the Typography component for use with anchors

--- clayui.com/src/components/Typography/Typography.js
+++ clayui.com/src/components/Typography/Typography.js
@@ -1,19 +1,24 @@
-import React, { Children } from 'react';
+/**
+ * © 2018 Liferay, Inc. <https://liferay.com>
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+import React from 'react';
 
 export const H1 = props => (
-	<h1 className="clay-h1">{props.children}</h1>
+	<h1 className="clay-h1" id={props.children.toLowerCase().split(' ').join('-')}>{props.children}</h1>
 );
 
 export const H2 = props => (
-	<h2 className="clay-h2">{props.children}</h2>
+	<h2 className="clay-h2" id={props.children.toLowerCase().split(' ').join('-')}>{props.children}</h2>
 );
 
 export const H3 = props => (
-	<h3 className="clay-h3">{props.children}</h3>
+	<h3 className="clay-h3" id={props.children.toLowerCase().split(' ').join('-')}>{props.children}</h3>
 );
 
 export const H4 = props => (
-	<h4 className="clay-h4">{props.children}</h4>
+	<h4 className="clay-h4" id={props.children.toLowerCase().split(' ').join('-')}>{props.children}</h4>
 );
 
 export const P = props => (
