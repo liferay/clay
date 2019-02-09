6b660d2fa53819b0f62824b69b14e4ad21799f93
Matuzalém Teles <matuzalemteles@gmail.com>
2019-02-08 23:48:21 -0300

    Makes the HTML component a hook

--- clayui.com/src/html.js
+++ clayui.com/src/html.js
@@ -1,35 +1,34 @@
-import React, { Component } from 'react';
+/**
+ * © 2018 Liferay, Inc. <https://liferay.com>
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
 
-class HTML extends Component {
-    render() {
-        return(
-            <html lang="en">
-                <head>
-                    <meta charSet="utf-8" />
-                    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-                    <meta
-                        name="viewport"
-                        content="width=device-width, initial-scale=1.0"
-                    />
-                    <link rel="shortcut icon" type="image/png" href="/images/favicon-32x32.png" sizes="16x16 32x32"/>
-                    {this.props.headComponents}
+import React from 'react';
 
-                    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.css" />
-                    <script type="text/javascript" src="/js/jquery.min.js"></script>
-                    <script type="text/javascript" src="/js/popper.js"></script>
-                    <script type="text/javascript" src="/js/bootstrap.js"></script>
-                </head>
-                <body {...this.props.bodyAttributes}>
-                    <div
-                        id="___gatsby"
-                        dangerouslySetInnerHTML={{ __html: this.props.body }}
-                    />
-                    {this.props.postBodyComponents}
-                    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.js"></script>
-                </body>
-            </html>
-        );
-    }
-}
+export default (props) => {
+	return (
+		<html lang="en">
+			<head>
+				<meta charSet="utf-8" />
+				<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1.0"
+				/>
+				<link rel="shortcut icon" type="image/png" href="/images/favicon-32x32.png" sizes="16x16 32x32"/>
+				{props.headComponents}
 
-export default HTML;
+				<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.css" />
+			</head>
+			<body {...props.bodyAttributes}>
+				<div
+					id="___gatsby"
+					dangerouslySetInnerHTML={{__html: props.body}}
+				/>
+				{props.postBodyComponents}
+				<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.js"></script>
+			</body>
+		</html>
+	);
+};
