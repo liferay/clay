f795085b8766d57e8e4fe1ccfbf7c77487235721
Matuzalém Teles <matuzalemteles@gmail.com>
2019-02-26 12:58:09 -0300

    Fixes #1604 - Update test

--- packages/clay-multi-select/src/__tests__/ClayMultiSelect.js
+++ packages/clay-multi-select/src/__tests__/ClayMultiSelect.js
@@ -303,7 +303,7 @@ describe('ClayMultiSelect', function() {
 				expect.objectContaining({
 					data: {
 						values: [],
-						valueOut: 'foo',
+						invalidWord: 'foo',
 					},
 					name: 'inputChange',
 					originalEvent: expect.any(Object),
