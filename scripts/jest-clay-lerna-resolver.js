2e0db858c05bfe1315bb0cd7d36a55570e98585c
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2019-03-08 10:31:03 -0800

    Fixes #1605 - Run format

--- scripts/jest-clay-lerna-resolver.js
+++ scripts/jest-clay-lerna-resolver.js
@@ -14,7 +14,7 @@ const resolve = require('resolve');
 module.exports = function(file, data) {
 	if (file.indexOf('clay-') === 0) {
 		data.packageFilter = function(pkg) {
-			return {main: pkg['jsnext:main']};
+			return {main: pkg['jsnext:main'],};
 		};
 	}
 
