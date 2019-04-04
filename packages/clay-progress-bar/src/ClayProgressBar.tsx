813bd55b72ff07e598cc96c853d16bb31cf70316
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2019-04-04 14:08:00 -0700

    Fixes #1773 - Update jsdocs

--- packages/clay-progress-bar/src/ClayProgressBar.tsx
+++ packages/clay-progress-bar/src/ClayProgressBar.tsx
@@ -8,8 +8,19 @@ import * as React from 'react';
 import classNames from 'classnames';
 
 interface Props extends React.HTMLAttributes<HTMLDivElement> {
+	/**
+	 * Flag to indicate if `children` should be wrapped as `feedback`.
+	 */
 	feedback?: boolean;
+
+	/**
+	 * Determines what the color the progress bar is.
+	 */
 	status?: 'success' | 'warning';
+
+	/**
+	 * The current value of the progress bar. Should range from 0 to 100.
+	 */
 	value: number;
 }
 
