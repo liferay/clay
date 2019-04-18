9779525694325741251cc2b19c6e01de792e8d10
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2019-04-18 13:59:48 -0700

    Fixes #1819 - Create DropDown component

--- packages/clay-drop-down/src/__tests__/index.tsx
+++ packages/clay-drop-down/src/__tests__/index.tsx
@@ -7,13 +7,215 @@
 import * as React from 'react';
 import * as TestRenderer from 'react-test-renderer';
 import ClayDropDown from '..';
+import {cleanup, fireEvent, render} from 'react-testing-library';
 
 describe('ClayDropDown', () => {
 	it('renders', () => {
 		const testRenderer = TestRenderer.create(
-			<ClayDropDown />
+			<ClayDropDown
+				items={[
+					{href: '#one', label: 'one'},
+					{href: '#two', label: 'two'},
+					{href: '#three', label: 'three'},
+				]}
+				spritemap="/foo/bar"
+			>
+				{'Click Me'}
+			</ClayDropDown>
 		);
 
 		expect(testRenderer.toJSON()).toMatchSnapshot();
+	});
+
+	it('renders with search input', () => {
+		const testRenderer = TestRenderer.create(
+			<ClayDropDown
+				items={[
+					{href: '#one', label: 'one'},
+					{href: '#two', label: 'two'},
+					{href: '#three', label: 'three'},
+				]}
+				searchable
+				spritemap="/foo/bar"
+			>
+				{'Click Me'}
+			</ClayDropDown>
+		);
+
+		expect(testRenderer.toJSON()).toMatchSnapshot();
+	});
+
+	it('renders with icons', () => {
+		const testRenderer = TestRenderer.create(
+			<ClayDropDown
+				items={[
+					{
+						label: 'Left Icon',
+						leftSymbol: 'pencil',
+					},
+					{
+						label: 'Right Icon',
+						rightSymbol: 'check',
+					},
+					{
+						label: 'Both Icons',
+						leftSymbol: 'trash',
+						rightSymbol: 'check',
+					},
+				]}
+				spritemap="/foo/bar"
+			>
+				{'Click Me'}
+			</ClayDropDown>
+		);
+
+		expect(testRenderer.toJSON()).toMatchSnapshot();
+	});
+
+	it('renders with groups', () => {
+		const testRenderer = TestRenderer.create(
+			<ClayDropDown
+				items={[
+					{
+						items: [
+							{href: '#one', label: 'one'},
+							{href: '#two', label: 'two'},
+						],
+						label: 'Strings',
+						type: 'group',
+					},
+					{
+						type: 'divider',
+					},
+					{
+						label: 'hello',
+					},
+					{
+						type: 'divider',
+					},
+					{
+						items: [
+							{href: '#one', label: '1'},
+							{href: '#two', label: '2'},
+						],
+						label: 'Numbers',
+						type: 'group',
+					},
+				]}
+				spritemap="/foo/bar"
+			>
+				{'Click Me'}
+			</ClayDropDown>
+		);
+
+		expect(testRenderer.toJSON()).toMatchSnapshot();
+	});
+});
+
+describe('Interactions', () => {
+	afterEach(cleanup);
+
+	it('renders', () => {
+		const handleClick = jest.fn();
+
+		const {container, getByLabelText} = render(
+			<ClayDropDown
+				items={[
+					{label: 'one', onClick: handleClick},
+					{href: '#two', label: 'two'},
+					{href: '#three', label: 'three'},
+				]}
+				spritemap="/foo/bar"
+			>
+				{'Click Me'}
+			</ClayDropDown>
+		);
+
+		const toggleButton = container.querySelector('.dropdown-toggle');
+
+		fireEvent.click(toggleButton as HTMLButtonElement, {});
+
+		const firstItem = container.querySelector('span.dropdown-item');
+
+		fireEvent.click(firstItem as HTMLSpanElement, {});
+
+		expect(handleClick).toHaveBeenCalledTimes(1);
+	});
+
+	it('filters items by search query', () => {
+		const {container} = render(
+			<ClayDropDown
+				items={[
+					{href: '#two', label: 'two'},
+					{href: '#three', label: 'three'},
+				]}
+				searchable
+				spritemap="/foo/bar"
+			>
+				{'Click Me'}
+			</ClayDropDown>
+		);
+
+		const toggleButton = container.querySelector('.dropdown-toggle');
+
+		fireEvent.click(toggleButton as HTMLButtonElement, {});
+
+		const searchInput = container.querySelector('input');
+
+		fireEvent.change(searchInput as HTMLButtonElement, {
+			target: {value: 'two'},
+		});
+
+		const dropdownItem = container.querySelector('.dropdown-item');
+
+		expect((dropdownItem as HTMLElement).innerHTML).toBe('two');
+	});
+
+	it('calls onChange for form items', () => {
+		const checkboxFn = jest.fn();
+		const onChangeFn = jest.fn();
+
+		const {container, getByLabelText} = render(
+			<ClayDropDown
+				items={[
+					{
+						label: 'Toggle',
+						onChange: checkboxFn,
+						type: 'checkbox',
+					},
+					{
+						items: [
+							{label: 'First', value: 'first'},
+							{label: 'Last', value: 'last'},
+						],
+						label: 'Order By',
+						name: 'orderBy',
+						onChange: onChangeFn,
+						type: 'radio',
+						value: 'first',
+					},
+				]}
+				spritemap="/foo/bar"
+			>
+				{'Click Me'}
+			</ClayDropDown>
+		);
+
+		const toggleButton = container.querySelector('.dropdown-toggle');
+
+		fireEvent.click(toggleButton as HTMLButtonElement, {});
+
+		const checkbox = getByLabelText('Toggle');
+
+		fireEvent.click(checkbox as HTMLButtonElement, {});
+
+		expect(checkboxFn).toHaveBeenCalledTimes(1);
+
+		const radio = getByLabelText('Last');
+
+		fireEvent.click(radio as HTMLButtonElement, {});
+
+		expect(onChangeFn).toHaveBeenCalledTimes(1);
+		expect(onChangeFn).toHaveBeenCalledWith('last');
 	});
 });
--- packages/clay-drop-down/stories/index.tsx
+++ packages/clay-drop-down/stories/index.tsx
@@ -3,14 +3,136 @@
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
-import React from 'react';
-import {boolean, select, text} from '@storybook/addon-knobs';
-import {storiesOf} from '@storybook/react';
 import ClayDropDown from '../src';
+import React from 'react';
+import {boolean} from '@storybook/addon-knobs';
+import {storiesOf} from '@storybook/react';
 
 import 'clay-css/lib/css/atlas.css';
+const spritemap = require('clay-css/lib/images/icons/icons.svg');
 
 storiesOf('ClayDropDown', module)
 	.add('default', () => (
-		<ClayDropDown />
+		<ClayDropDown
+			items={[
+				{href: '#one', label: 'one'},
+				{href: '#two', label: 'two'},
+				{href: '#three', label: 'three'},
+			]}
+			searchable={boolean('Searchable', false)}
+			spritemap={spritemap}
+		>
+			{'Click Me'}
+		</ClayDropDown>
+	))
+	.add('groups', () => (
+		<ClayDropDown
+			items={[
+				{
+					items: [
+						{href: '#one', label: 'one'},
+						{href: '#two', label: 'two'},
+					],
+					label: 'Strings',
+					type: 'group',
+				},
+				{
+					type: 'divider',
+				},
+				{
+					label: 'hello',
+				},
+				{
+					type: 'divider',
+				},
+				{
+					items: [
+						{href: '#one', label: '1'},
+						{href: '#two', label: '2'},
+					],
+					label: 'Numbers',
+					type: 'group',
+				},
+			]}
+			searchable={boolean('Searchable', false)}
+			spritemap={spritemap}
+		>
+			{'Click Me'}
+		</ClayDropDown>
+	))
+	.add('checkbox', () => (
+		<ClayDropDown
+			items={[
+				{
+					label: 'Toggle?',
+					onChange: () => alert('clicked checkbox!'),
+					type: 'checkbox',
+				},
+			]}
+			searchable={boolean('Searchable', false)}
+			spritemap={spritemap}
+		>
+			{'Click Me'}
+		</ClayDropDown>
+	))
+	.add('radio', () => (
+		<ClayDropDown
+			items={[
+				{
+					items: [
+						{label: 'First', value: 'first'},
+						{label: 'Last', value: 'last'},
+					],
+					label: 'Order By',
+					name: 'orderBy',
+					onChange: val => alert(`Selected ${val}`),
+					type: 'radio',
+					value: 'first',
+				},
+			]}
+			spritemap={spritemap}
+		>
+			{'Click Me'}
+		</ClayDropDown>
+	))
+	.add('footer options', () => (
+		<ClayDropDown
+			actionButtonProps={{
+				children: 'See More',
+				onClick: () => alert('Clicked See More!'),
+			}}
+			captionText="4 of 6"
+			items={[
+				{
+					label: 'Toggle',
+				},
+			]}
+			searchable={boolean('Searchable', false)}
+			spritemap={spritemap}
+		>
+			{'Click Me'}
+		</ClayDropDown>
+	))
+	.add('items with icons', () => (
+		<ClayDropDown
+			items={[
+				{
+					label: 'Left Icon',
+					leftSymbol: 'pencil',
+				},
+				{
+					label: 'Right Icon',
+					rightSymbol: 'check',
+				},
+				{
+					label: 'Both Icons',
+					leftSymbol: 'trash',
+					rightSymbol: 'check',
+				},
+			]}
+			searchable={boolean('Searchable', false)}
+			spritemap={spritemap}
+		>
+			{'Click Me'}
+		</ClayDropDown>
 	));
