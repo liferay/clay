ad50b685b69ab44c064ffcc3158fd4dc83c756ee
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2019-04-22 11:14:45 -0700

    Update radio-group tests

--- packages/clay-radio-group/src/__tests__/index.tsx
+++ packages/clay-radio-group/src/__tests__/index.tsx
@@ -7,9 +7,10 @@
 import * as React from 'react';
 import * as TestRenderer from 'react-test-renderer';
 import ClayRadioGroup from '..';
+import {cleanup, fireEvent, render} from 'react-testing-library';
 
-describe('ClayRadioGroup', () => {
-	it('renders with options', () => {
+describe('Rendering', () => {
+	it('default', () => {
 		const testRenderer = TestRenderer.create(
 			<ClayRadioGroup
 				onSelectedValueChange={() => {}}
@@ -22,5 +23,31 @@ describe('ClayRadioGroup', () => {
 		);
 
 		expect(testRenderer.toJSON()).toMatchSnapshot();
+	});
+});
+
+describe('Interactions', () => {
+	afterEach(cleanup);
+
+	it('selecting a radio element should fire the onSelectedValueChange prop with the new value', () => {
+		const handleSelectedChange = jest.fn();
+
+		const {getByLabelText} = render(
+			<ClayRadioGroup
+				onSelectedValueChange={handleSelectedChange}
+				selectedValue={'one'}
+			>
+				<ClayRadioGroup.Radio label="One" value="one" />
+				<ClayRadioGroup.Radio label="Two" value="two" />
+				<ClayRadioGroup.Radio label="Three" value="three" />
+			</ClayRadioGroup>
+		);
+
+		const radioThree = getByLabelText('Three');
+
+		fireEvent.click(radioThree as HTMLButtonElement, {});
+
+		expect(handleSelectedChange).toHaveBeenCalledTimes(1);
+		expect(handleSelectedChange).toHaveBeenCalledWith('three');
 	});
 });
