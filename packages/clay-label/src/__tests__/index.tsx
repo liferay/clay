19ef70843e73fa20741a933f0db7c5da7015203b
Carlos Lancha <carlos.lancha@liferay.com>
2019-04-23 12:20:44 +0200

    Fixes #1891 - Update clay-label tests

--- packages/clay-label/src/__tests__/index.tsx
+++ packages/clay-label/src/__tests__/index.tsx
@@ -8,6 +8,8 @@ import * as React from 'react';
 import * as TestRenderer from 'react-test-renderer';
 import ClayLabel from '..';
 
+const spritemap = 'path/to/spritemap';
+
 describe('ClayLabel', () => {
 	it('renders', () => {
 		const testRenderer = TestRenderer.create(
@@ -28,6 +30,37 @@ describe('ClayLabel', () => {
 	it('renders as a link ', () => {
 		const testRenderer = TestRenderer.create(
 			<ClayLabel href="#/foo/bar">{'Label w/ link'}</ClayLabel>
+		);
+
+		expect(testRenderer.toJSON()).toMatchSnapshot();
+	});
+
+	it('renders as closable', () => {
+		const testRenderer = TestRenderer.create(
+			<ClayLabel 
+				closeButtonProps={{
+					onClick: () => {},
+				}}
+				spritemap={spritemap}
+			>
+				{'Label Closable'}
+			</ClayLabel>
+		);
+
+		expect(testRenderer.toJSON()).toMatchSnapshot();
+	});
+
+	it('renders as a link and closable ', () => {
+		const testRenderer = TestRenderer.create(
+			<ClayLabel 
+				closeButtonProps={{
+					onClick: () => {},
+				}}
+				href="#/foo/bar"
+				spritemap={spritemap}
+			>
+				{'Label Closable'}
+			</ClayLabel>
 		);
 
 		expect(testRenderer.toJSON()).toMatchSnapshot();
