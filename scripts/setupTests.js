420f37b4b94e1b4d381cd0a8ce36e17634159033
Diego Nascimento <diego.nascimento@liferay.com>
2019-04-30 17:59:31 -0300

    Fixes #1866 | Adds missing resizing capabilities to JSDOM

--- scripts/setupTests.js
+++ scripts/setupTests.js
@@ -7,3 +7,12 @@
 'use strict';
 
 global.fetch = require('jest-fetch-mock');
+
+if (typeof window !== 'undefined') {
+	global.window.resizeTo = (width, height) => {
+		global.window.innerWidth = width || global.window.innerWidth;
+		global.window.innerHeight = height || global.window.innerHeight;
+		global.window.dispatchEvent(new Event('resize'));
+	};
+	global.window.scrollTo = () => {};
+}
