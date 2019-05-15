5c7dc73f60547d98622787b37091d1800becfa61
Matuzalém Teles <matuzalemteles@gmail.com>
2019-05-15 17:10:15 -0300

    test: Add fetch-mock's types

---
+++ scripts/setupTests.ts
@@ -0,0 +1,11 @@
+/**
+ * © 2019 Liferay, Inc. <https://liferay.com>
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+import {GlobalWithFetchMock} from 'jest-fetch-mock';
+
+const customGlobal: GlobalWithFetchMock = global as GlobalWithFetchMock;
+customGlobal.fetch = require('jest-fetch-mock');
+customGlobal.fetchMock = customGlobal.fetch;
