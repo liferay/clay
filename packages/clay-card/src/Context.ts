d47b22a9b46bc875a77e2d8d614d009bd0b73ce5
Diego Nascimento <diego.nascimento@liferay.com>
2019-05-28 13:45:12 -0300

    Fixes #1940 | Adds docs for `horizontal` and `interactive` props

--- packages/clay-card/src/Context.ts
+++ packages/clay-card/src/Context.ts
@@ -6,8 +6,18 @@
 import {createContext} from 'react';
 
 export interface IContext {
+	/**
+	 * Flag to indicate whether to use card as a horizontal/folder card.
+	 * See the following documenation:
+	 * https://liferay.design/lexicon/core-components/cards/cards-variation/
+	 */
 	horizontal?: boolean;
 
+	/**
+	 * Flag to indicate whether to use card as a navigation card.
+	 * See the following documenation:
+	 * https://liferay.design/lexicon/core-components/cards/cards-navigation/
+	 */
 	interactive?: boolean;
 }
 
