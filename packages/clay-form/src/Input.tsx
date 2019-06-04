bc4281e2c11369b78ed4c1f1cd0f15e07dafe3aa
Matuzalém Teles <matuzalemteles@gmail.com>
2019-06-04 14:37:05 -0300

    refactor(ClayForm.Input): remove allowedCharacters API

--- packages/clay-form/src/Input.tsx
+++ packages/clay-form/src/Input.tsx
@@ -5,15 +5,9 @@
  */
 
 import classNames from 'classnames';
-import React, {useMemo} from 'react';
+import React from 'react';
 
 export interface IProps extends React.InputHTMLAttributes<HTMLInputElement> {
-	/**
-	 * Gets the accepted characters of the input
-	 * element values
-	 */
-	allowedCharacters?: RegExp;
-
 	/**
 	 * Input component to render. Can either be a string like 'input' or a component.
 	 */
@@ -23,38 +17,18 @@ export interface IProps extends React.InputHTMLAttributes<HTMLInputElement> {
 const Input = React.forwardRef<HTMLInputElement, IProps>(
 	(
 		{
-			allowedCharacters,
 			className,
 			component: Component = 'input',
-			value,
 			...otherProps
 		},
 		ref
-	) => {
-		const getCharactersAllowed = (value: string) => {
-			const regexp = new RegExp(allowedCharacters as RegExp);
-			const match = value.match(regexp);
-
-			return Array.isArray(match) ? match.join('') : '';
-		};
-
-		const memoValue = useMemo(
-			() =>
-				allowedCharacters
-					? getCharactersAllowed(value as string)
-					: value,
-			[allowedCharacters, value]
-		);
-
-		return (
-			<Component
-				{...otherProps}
-				className={classNames('form-control', className)}
-				ref={ref}
-				value={memoValue}
-			/>
-		);
-	}
+	) => (
+		<Component
+			{...otherProps}
+			className={classNames('form-control', className)}
+			ref={ref}
+		/>
+	)
 );
 
 export default Input;
