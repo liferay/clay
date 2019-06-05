27def20ceb48a39420fc02b1392ec5db4693eb36
Diego Nascimento <diego.nascimento@liferay.com>
2019-06-05 17:18:11 -0300

    Fixes #1900 | SF

--- packages/clay-card/src/index.tsx
+++ packages/clay-card/src/index.tsx
@@ -14,6 +14,7 @@ import {Detail} from './Detail';
 import {Group} from './Group';
 
 type CardDisplayType = 'file' | 'image' | 'user';
+
 export interface ICardProps {
 	displayType?: CardDisplayType;
 
