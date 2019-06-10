825a89e4221b4d3b4cca791afd321f6be3f485d8
Matuzalém Teles <matuzalemteles@gmail.com>
2019-06-10 10:34:00 -0300

    refactor(ClaySelect): refactor Select by following the low-level and high-level components

--- packages/clay-select/stories/index.tsx
+++ packages/clay-select/stories/index.tsx
@@ -3,30 +3,56 @@
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
-import ClaySelect from '../src';
+import ClaySelect, {ClaySelectWithOption} from '../src';
 import React from 'react';
 import {boolean} from '@storybook/addon-knobs';
 import {storiesOf} from '@storybook/react';
 
 import 'clay-css/lib/css/atlas.css';
 
-storiesOf('ClaySelect', module).add('default', () => (
-	<ClaySelect
-		disabled={boolean('Disabled', false) ? true : false}
-		id="mySelectId"
-		inline={boolean('Inline', false) ? true : false}
-		label="Select Label"
-		multiple={boolean('Multiple', false) ? true : false}
-		name="mySelectName"
-		options={[
-			{
-				label: 'Option 1',
-				value: '1',
-			},
-			{
-				label: 'Option 2',
-				value: '2',
-			},
-		]}
-	/>
-));
+const options = [
+	{
+		label: 'Option 1',
+		value: '1',
+	},
+	{
+		label: 'Option 2',
+		value: '2',
+	},
+];
+
+storiesOf('ClaySelect', module)
+	.add('with low-level APIs (composition)', () => (
+		<div className="sheet">
+			<div className="form-group">
+				<label htmlFor="mySelectId">Select Label</label>
+				<ClaySelect
+					aria-label="Select Label"
+					disabled={boolean('Disabled', false)}
+					id="mySelectId"
+					multiple={boolean('Multiple', false)}
+				>
+					{options.map(item => (
+						<ClaySelect.Option
+							label={item.label}
+							value={item.value}
+						/>
+					))}
+				</ClaySelect>
+			</div>
+		</div>
+	))
+	.add('with high-level', () => (
+		<div className="sheet">
+			<div className="form-group">
+				<label htmlFor="mySelectId">Select Label</label>
+				<ClaySelectWithOption
+					aria-label="Select Label"
+					disabled={boolean('Disabled', false)}
+					id="mySelectId"
+					multiple={boolean('Multiple', false)}
+					options={options}
+				/>
+			</div>
+		</div>
+	));
