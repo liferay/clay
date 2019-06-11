92c8b5151df540d36b2b597efabd9d95b27e61d0
Diego Nascimento <diego.nascimento@liferay.com>
2019-06-11 17:08:21 -0300

    docs(navigation-bar): Split `Item` variant to a file

---
+++ packages/clay-navigation-bar/src/Item.tsx
@@ -0,0 +1,42 @@
+/**
+ * © 2019 Liferay, Inc. <https://liferay.com>
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+import classNames from 'classnames';
+import React from 'react';
+
+export interface IItemProps extends React.HTMLAttributes<HTMLLIElement> {
+	/**
+	 * Determines the active state of an dropdown list item.
+	 */
+	active?: boolean;
+
+	/**
+	 * Children elements received from ClayNavigationBar.Item component.
+	 */
+	children: React.ReactElement;
+}
+
+export const Item: React.FunctionComponent<IItemProps> = ({
+	active = false,
+	children,
+	className,
+	...otherProps
+}: IItemProps) => {
+	return (
+		<li {...otherProps} className={classNames('nav-item', className)}>
+			{React.Children.map(
+				children,
+				(child: React.ReactElement<IItemProps>, index) =>
+					React.cloneElement(child, {
+						...child.props,
+						className: classNames(child.props.className, {
+							active,
+						}),
+						key: index,
+					})
+			)}
+		</li>
+	);
+};
