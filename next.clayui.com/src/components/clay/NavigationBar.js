595efdc4860ff666498bc8de9466afc6401553c8
Diego Nascimento <diego.nascimento@liferay.com>
2019-06-11 16:51:06 -0300

    docs(navigation-bar): Adds React documentation of NavigationBar

---
+++ next.clayui.com/src/components/clay/NavigationBar.js
@@ -0,0 +1,44 @@
+/**
+ * © 2019 Liferay, Inc. <https://liferay.com>
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+import ClayLink from '@clayui/link';
+import ClayNavigationBar from '@clayui/navigation-bar';
+import Editor from './Editor';
+import React, {useState} from 'react';
+
+const spritemap = '/images/icons/icons.svg';
+
+const NavigationBar = () => {
+	const scope = {ClayLink, ClayNavigationBar, spritemap, useState};
+	const code = `const Component = () => {
+
+	return (
+        <ClayNavigationBar triggerLabel="Item 1" spritemap={spritemap}>
+            <ClayNavigationBar.Item active>
+                <ClayLink className="nav-link" displayType="unstyled">
+                    Item 1
+                </ClayLink>
+            </ClayNavigationBar.Item>
+            <ClayNavigationBar.Item>
+                <ClayLink className="nav-link" displayType="unstyled">
+                    Item 2
+                </ClayLink>
+            </ClayNavigationBar.Item>
+            <ClayNavigationBar.Item>
+                <ClayLink className="nav-link" displayType="unstyled">
+                    Item 3
+                </ClayLink>
+            </ClayNavigationBar.Item>
+		</ClayNavigationBar>
+	);
+}
+
+render(<Component />)`;
+
+	return <Editor code={code} scope={scope} />;
+};
+
+export {NavigationBar};
