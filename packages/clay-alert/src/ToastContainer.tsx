a60a32de7d0420e6b3a55e0edbad22c29fe027dc
Diego Nascimento <diego.nascimento@liferay.com>
2019-06-20 16:24:31 -0300

    chore(alert): Split ToastContainer composition of ClayAlert from index to another file

---
+++ packages/clay-alert/src/ToastContainer.tsx
@@ -0,0 +1,35 @@
+/**
+ * © 2019 Liferay, Inc. <https://liferay.com>
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+import classNames from 'classnames';
+import React from 'react';
+import {IClayAlertProps} from './index';
+
+export interface IToastContainerProps
+	extends React.HTMLAttributes<HTMLDivElement> {
+	/**
+	 * Chilren of the ToastContainer must be a ClayAlert
+	 */
+	children?:
+		| React.ReactElement<IClayAlertProps>
+		| React.ReactElement<IClayAlertProps>[];
+}
+
+export const ToastContainer = ({
+	children,
+	className,
+	...otherProps
+}: IToastContainerProps) => {
+	return (
+		<div
+			{...otherProps}
+			className={classNames(className, 'alert-container container')}
+		>
+			<div className="alert-notifications alert-notifications-fixed">
+				{children}
+			</div>
+		</div>
+	);
+};
