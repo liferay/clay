93cebbd5fc4ca9b7b5c6212133d7e6138d74152c
Matuzalém Teles <matuzalemteles@gmail.com>
2019-06-20 11:43:33 -0300

    docs(pagination): remove some sections that describe the API

--- next.clayui.com/src/components/clay/Pagination.js
+++ next.clayui.com/src/components/clay/Pagination.js
@@ -10,53 +10,6 @@ import React, {useState} from 'react';
 
 const spritemap = '/images/icons/icons.svg';
 
-const PaginationCode = `const Component = () => {
-	const initialActivePage = 1;
-	const [active, setActive] = useState(initialActivePage);
-
-	return (
-		<ClayPagination
-			activePage={active}
-			ellipsisBuffer={2}
-			onPageChange={setActive}
-			spritemap={spritemap}
-			totalPages={8}
-	  	/>
-	);
-}
-render(<Component />)`;
-
-export const Pagination = () => {
-	const scope = {ClayPagination, spritemap, useState};
-	const code = PaginationCode;
-
-	return <Editor code={code} scope={scope} />;
-};
-
-const PaginationWithLinksCode = `const Component = () => {
-	const initialActivePage = 1;
-	const [active, setActive] = useState(initialActivePage);
-
-	return (
-		<ClayPagination
-			activePage={active}
-			ellipsisBuffer={2}
-			hrefConstructor={page => "/#" + page}
-			onPageChange={setActive}
-			spritemap={spritemap}
-			totalPages={8}
-	  	/>
-	);
-}
-render(<Component />)`;
-
-export const PaginationWithLinks = () => {
-	const scope = {ClayPagination, spritemap, useState};
-	const code = PaginationWithLinksCode;
-
-	return <Editor code={code} scope={scope} />;
-};
-
 const PaginationEllipsisBufferCode = `const Component = () => {
 	const initialActivePage = 1;
 	const [active, setActive] = useState(initialActivePage);
@@ -73,33 +26,11 @@ const PaginationEllipsisBufferCode = `const Component = () => {
 }
 render(<Component />)`;
 
-export const PaginationEllipsisBuffer = () => {
+const PaginationEllipsisBuffer = () => {
 	const scope = {ClayPagination, spritemap, useState};
 	const code = PaginationEllipsisBufferCode;
 
 	return <Editor code={code} scope={scope} />;
 };
 
-const PaginationDisabledPagesCode = `const Component = () => {
-	const initialActivePage = 1;
-	const [active, setActive] = useState(initialActivePage);
-
-	return (
-		<ClayPagination
-			activePage={active}
-			disabledPages={[2,3]}
-			ellipsisBuffer={1}
-			onPageChange={setActive}
-			spritemap={spritemap}
-			totalPages={8}
-	  	/>
-	);
-}
-render(<Component />)`;
-
-export const PaginationDisabledPages = () => {
-	const scope = {ClayPagination, spritemap, useState};
-	const code = PaginationDisabledPagesCode;
-
-	return <Editor code={code} scope={scope} />;
-};
+export {PaginationEllipsisBuffer};
