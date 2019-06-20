14b86f07eeb956b2474d591e000fc49e5417aeea
Matuzalém Teles <matuzalemteles@gmail.com>
2019-06-20 11:23:07 -0300

    docs(buttons): remove some sections that describe the API

--- next.clayui.com/src/components/clay/Button.js
+++ next.clayui.com/src/components/clay/Button.js
@@ -5,28 +5,10 @@
  */
 
 import ClayButton from '@clayui/button';
-import ClayIcon from '@clayui/icon';
 import Editor from './Editor';
 import React from 'react';
 
 const spritemap = '/images/icons/icons.svg';
-
-const ButtonBlockCode = `const Component = () => {
-	return (
-        <ClayButton displayType="primary" block>
-			Button Block
-		</ClayButton>
-	);
-}
-
-render(<Component />);`;
-
-export const ButtonBlock = () => {
-	const scope = {ClayButton, spritemap};
-	const code = ButtonBlockCode;
-
-	return <Editor code={code} scope={scope} />;
-};
 
 const ButtonDisplayTypesCode = `const Component = () => {
 	return (
@@ -49,7 +31,7 @@ const ButtonDisplayTypesCode = `const Component = () => {
 
 render(<Component />);`;
 
-export const ButtonDisplayTypes = () => {
+const ButtonDisplayTypes = () => {
 	const scope = {ClayButton, spritemap};
 	const code = ButtonDisplayTypesCode;
 
@@ -70,60 +52,11 @@ const ButtonGroupCode = `const Component = () => {
 
 render(<Component />);`;
 
-export const ButtonGroup = () => {
+const ButtonGroup = () => {
 	const scope = {ClayButton, spritemap};
 	const code = ButtonGroupCode;
 
 	return <Editor code={code} scope={scope} />;
 };
 
-const ButtonMonospacedCode = `const Component = () => {
-	return (
-		<ClayButton displayType="primary" monospaced>
-			<ClayIcon spritemap={spritemap} symbol="plus"></ClayIcon>
-		</ClayButton>
-	);
-}
-
-render(<Component />);`;
-
-export const ButtonMonospaced = () => {
-	const scope = {ClayButton, ClayIcon, spritemap};
-	const code = ButtonMonospacedCode;
-
-	return <Editor code={code} scope={scope} />;
-};
-
-const ButtonWithStarCode = `const Component = () => {
-	return (
-		<ClayButton displayType="primary">
-			<ClayIcon spritemap={spritemap} symbol="bookmarks"></ClayIcon> Bookmark
-		</ClayButton>
-	);
-}
-
-render(<Component />);`;
-
-export const ButtonWithStar = () => {
-	const scope = {ClayButton, ClayIcon, spritemap};
-	const code = ButtonWithStarCode;
-
-	return <Editor code={code} scope={scope} />;
-};
-
-const ButtonSmallCode = `const Component = () => {
-	return (
-		<ClayButton displayType="primary" small>
-			<ClayIcon spritemap={spritemap} symbol="plus"></ClayIcon>
-		</ClayButton>
-	);
-}
-
-render(<Component />);`;
-
-export const ButtonSmall = () => {
-	const scope = {ClayButton, ClayIcon, spritemap};
-	const code = ButtonSmallCode;
-
-	return <Editor code={code} scope={scope} />;
-};
+export {ButtonDisplayTypes, ButtonGroup};
