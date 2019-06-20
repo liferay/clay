2bf8b8983741ffab64693984bf6f9381bcfb9753
Matuzalém Teles <matuzalemteles@gmail.com>
2019-06-20 11:58:51 -0300

    docs(list): remove some sections that describe the API

--- next.clayui.com/src/components/clay/List.js
+++ next.clayui.com/src/components/clay/List.js
@@ -62,154 +62,9 @@ const ListCode = `const Component = () => {
 
 render(<Component />)`;
 
-export const List = () => {
+const List = () => {
 	const scope = {ClayList, spritemap};
 	const code = ListCode;
-
-	return <Editor code={code} scope={scope} />;
-};
-
-const ListHeaderCode = `const Component = () => {
-	
-	return (
-		<ClayList>
-			<ClayList.Header>This is a header</ClayList.Header>
-			<ClayList.Item flex>
-				<ClayList.ItemField>Item 1</ClayList.ItemField>
-			</ClayList.Item>
-		</ClayList>
-	);
-}
-
-render(<Component />)`;
-
-export const ListHeader = () => {
-	const scope = {ClayList};
-	const code = ListHeaderCode;
-
-	return <Editor code={code} scope={scope} />;
-};
-
-const ListItemCode = `const Component = () => {
-	
-	return (
-		<ClayList>
-			<ClayList.Item header>
-				<ClayList.ItemField>Header Item</ClayList.ItemField>
-			</ClayList.Item>
-			<ClayList.Item>
-				<ClayList.ItemField>Default Item</ClayList.ItemField>
-			</ClayList.Item>
-			<ClayList.Item active>
-				<ClayList.ItemField>Active Item</ClayList.ItemField>
-			</ClayList.Item>
-			<ClayList.Item disabled>
-				<ClayList.ItemField>Disabled Item</ClayList.ItemField>
-			</ClayList.Item>
-			<ClayList.Item header>
-				<ClayList.ItemField>Header Item</ClayList.ItemField>
-			</ClayList.Item>
-			<ClayList.Item action>
-				<ClayList.ItemField>Action Item</ClayList.ItemField>
-			</ClayList.Item>
-		</ClayList>
-	);
-}
-
-render(<Component />)`;
-
-export const ListItem = () => {
-	const scope = {ClayList};
-	const code = ListItemCode;
-
-	return <Editor code={code} scope={scope} />;
-};
-
-const ListItemFieldCode = `const Component = () => {
-	
-	return (
-		<ClayList>
-			<ClayList.Item flex>
-				<ClayList.ItemField>Item 1</ClayList.ItemField>
-
-				<ClayList.ItemField>ItemField</ClayList.ItemField>
-
-				<ClayList.ItemField expand>
-					<ClayList.ItemTitle>Item Title and expanded</ClayList.ItemTitle>
-					<ClayList.ItemText>Item Text</ClayList.ItemText>
-				</ClayList.ItemField>
-
-				<ClayList.ItemField>ItemField</ClayList.ItemField>
-			</ClayList.Item>
-		</ClayList>
-	);
-}
-
-render(<Component />)`;
-
-export const ListItemField = () => {
-	const scope = {ClayList};
-	const code = ListItemFieldCode;
-
-	return <Editor code={code} scope={scope} />;
-};
-
-const ListItemTextCode = `const Component = () => {
-	
-	return (
-		<ClayList>
-			<ClayList.Item flex>
-				<ClayList.ItemField>Item 1</ClayList.ItemField>
-
-				<ClayList.ItemField>ItemField</ClayList.ItemField>
-
-				<ClayList.ItemField expand>
-					<ClayList.ItemTitle>Item Title and expanded</ClayList.ItemTitle>
-					<ClayList.ItemText>Item Text</ClayList.ItemText>
-					<ClayList.ItemText subtext>Subtitle</ClayList.ItemText>
-				</ClayList.ItemField>
-
-				<ClayList.ItemField>ItemField</ClayList.ItemField>
-			</ClayList.Item>
-		</ClayList>
-	);
-}
-
-render(<Component />)`;
-
-export const ListItemText = () => {
-	const scope = {ClayList};
-	const code = ListItemTextCode;
-
-	return <Editor code={code} scope={scope} />;
-};
-
-const ListItemTitleCode = `const Component = () => {
-	
-	return (
-		<ClayList>
-			<ClayList.Item flex>
-				<ClayList.ItemField>Item 1</ClayList.ItemField>
-
-				<ClayList.ItemField>ItemField</ClayList.ItemField>
-
-				<ClayList.ItemField expand>
-					<ClayList.ItemTitle>Item Title and expanded</ClayList.ItemTitle>
-					<ClayList.ItemText>Item Text</ClayList.ItemText>
-					<ClayList.ItemText subtext>Subtitle</ClayList.ItemText>
-				</ClayList.ItemField>
-
-				<ClayList.ItemField>ItemField</ClayList.ItemField>
-			</ClayList.Item>
-		</ClayList>
-	);
-}
-
-render(<Component />)`;
-
-export const ListItemTitle = () => {
-	const scope = {ClayList};
-	const code = ListItemTitleCode;
 
 	return <Editor code={code} scope={scope} />;
 };
@@ -244,9 +99,11 @@ const ListQuickActionsMenuCode = `const Component = () => {
 
 render(<Component />)`;
 
-export const ListQuickActionsMenu = () => {
+const ListQuickActionsMenu = () => {
 	const scope = {ClayList, spritemap};
 	const code = ListQuickActionsMenuCode;
 
 	return <Editor code={code} scope={scope} />;
 };
+
+export {List, ListQuickActionsMenu};
