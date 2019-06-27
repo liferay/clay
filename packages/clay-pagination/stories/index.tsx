e2b2dab2b2bbf3709d6891ca0784de68a558b422
Diego Nascimento <diego.nascimento@liferay.com>
2019-06-27 15:01:33 -0300

    chore(pagination): Adds Pagination Bar story on pagination

--- packages/clay-pagination/stories/index.tsx
+++ packages/clay-pagination/stories/index.tsx
@@ -4,6 +4,9 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 import '@clayui/css/lib/css/atlas.css';
+import ClayButton from '@clayui/button';
+import ClayDropDown from '@clayui/drop-down';
+import ClayIcon from '@clayui/icon';
 import ClayPagination from '../src';
 import React, {useState} from 'react';
 import {number} from '@storybook/addon-knobs';
@@ -53,4 +56,79 @@ storiesOf('ClayPagination', module)
 				totalPages={totalPages}
 			/>
 		);
+	})
+	.add('w/ bar', () => {
+		const PaginationBar = ({numberOfItems}: any) => {
+			const [active, setActive] = React.useState(false);
+			const [activePage, setActivePage] = React.useState(1);
+			const [perPage, setPerPage] = React.useState(10);
+
+			const options = [
+				{
+					label: '5',
+					value: 5,
+				},
+				{
+					label: '10',
+					value: 10,
+				},
+				{
+					label: '20',
+					value: 20,
+				},
+				{
+					label: '30',
+					value: 30,
+				},
+				{
+					label: '50',
+					value: 50,
+				},
+			];
+
+			return (
+				<div className="pagination-bar">
+					<ClayDropDown
+						active={active}
+						className="pagination-items-per-page"
+						onActiveChange={newVal => setActive(newVal)}
+						trigger={
+							<ClayButton displayType="unstyled">
+								{`${perPage} entries`}
+								<ClayIcon
+									spritemap={spritemap}
+									symbol="caret-double-l"
+								/>
+							</ClayButton>
+						}
+					>
+						<ClayDropDown.ItemList>
+							{options.map((item, i) => (
+								<ClayDropDown.Item
+									key={i}
+									onClick={() => setPerPage(item.value)}
+								>
+									{`${item.label} items`}
+								</ClayDropDown.Item>
+							))}
+						</ClayDropDown.ItemList>
+					</ClayDropDown>
+
+					<div className="pagination-results">
+						{`Showing ${activePage} to ${Math.ceil(
+							numberOfItems / perPage
+						)} of ${numberOfItems}`}
+					</div>
+
+					<ClayPagination
+						activePage={activePage}
+						onPageChange={(page) => page && setActivePage(page)}
+						spritemap={spritemap}
+						totalPages={Math.ceil(numberOfItems / perPage)}
+					/>
+				</div>
+			);
+		};
+
+		return <PaginationBar numberOfItems={500} />;
 	});
