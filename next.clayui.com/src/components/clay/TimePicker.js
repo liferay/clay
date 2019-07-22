90553fa94e7f4eaf24634e62827f418c4806150e
Matuzalém Teles <matuzalemteles@gmail.com>
2019-07-22 16:30:48 -0300

    chore(next.clayui.com): add TimePicker document

---
+++ next.clayui.com/src/components/clay/TimePicker.js
@@ -0,0 +1,38 @@
+/**
+ * © 2019 Liferay, Inc. <https://liferay.com>
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+import ClayTimePicker from '@clayui/time-picker';
+import Editor from './Editor';
+import React, {useState} from 'react';
+
+const spritemap = '/images/icons/icons.svg';
+
+const timePickerCode = `const Component = () => {
+	const [state, setState] = useState({
+		hours: '--',
+		minutes: '--',
+	});
+
+	return (
+		<ClayTimePicker
+			spritemap={spritemap}
+			onInputChange={setState}
+			values={state}
+			timezone="GMT+01:00"
+			use12Hours
+		/>
+	);
+}
+
+render(<Component />)`;
+
+const TimePickerWithState = () => {
+	const scope = {ClayTimePicker, spritemap, useState};
+	return <Editor code={timePickerCode} scope={scope} />;
+};
+export {
+	TimePickerWithState,
+};
