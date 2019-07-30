76d19d07924c43ff8d3191b0f65b84bf9cd8793f
Matuzalém Teles <matuzalemteles@gmail.com>
2019-07-30 13:49:43 -0300

    refactor(@clayui/navigation): update BreadcrumbEllipsis to use FocusScope component and useFocusManagement hook

--- packages/clay-navigation/src/BreadcrumbEllipsis.tsx
+++ packages/clay-navigation/src/BreadcrumbEllipsis.tsx
@@ -7,6 +7,7 @@ import ClayButton from '@clayui/button';
 import ClayDropDown from '@clayui/drop-down';
 import ClayIcon from '@clayui/icon';
 import React, {useState} from 'react';
+import {FocusScope, useFocusManagement} from '@clayui/shared';
 import {IBreadcrumbItem} from './BreadcrumbItem';
 
 export interface IBreadcrumbEllipsisProps {
@@ -25,37 +26,44 @@ export const BreadcrumbEllipsis: React.FunctionComponent<
 	IBreadcrumbEllipsisProps
 > = ({items, spritemap, ...otherProps}) => {
 	const [active, setActive] = useState(false);
+	const focusManager = useFocusManagement();
 
 	return (
-		<ClayDropDown
-			active={active}
-			className="breadcrumb-item"
-			containerElement="li"
-			onActiveChange={newVal => setActive(newVal)}
-			trigger={
-				<ClayButton
-					className="breadcrumb-link"
-					data-testid="breadcrumbDropdownTrigger"
-					displayType="unstyled"
-				>
-					<ClayIcon spritemap={spritemap} symbol="ellipsis-h" />
-					<ClayIcon spritemap={spritemap} symbol="caret-bottom" />
-				</ClayButton>
-			}
-			{...otherProps}
-		>
-			<ClayDropDown.ItemList>
-				{items.map(({href, label, onClick}, i) => (
-					<ClayDropDown.Item
-						href={href}
-						key={`breadcrumbEllipsisItem${i}`}
-						onClick={onClick}
-						title={label}
+		<FocusScope focusManager={focusManager}>
+			<ClayDropDown
+				active={active}
+				className="breadcrumb-item"
+				containerElement="li"
+				onActiveChange={newVal => setActive(newVal)}
+				trigger={
+					<ClayButton
+						className="breadcrumb-link"
+						data-testid="breadcrumbDropdownTrigger"
+						displayType="unstyled"
+						ref={ref => focusManager.createScope(ref, 'input')}
 					>
-						{label}
-					</ClayDropDown.Item>
-				))}
-			</ClayDropDown.ItemList>
-		</ClayDropDown>
+						<ClayIcon spritemap={spritemap} symbol="ellipsis-h" />
+						<ClayIcon spritemap={spritemap} symbol="caret-bottom" />
+					</ClayButton>
+				}
+				{...otherProps}
+			>
+				<ClayDropDown.ItemList>
+					{items.map(({href, label, onClick}, i) => (
+						<ClayDropDown.Item
+							href={href}
+							innerRef={ref =>
+								focusManager.createScope(ref, `item${i}`, true)
+							}
+							key={`breadcrumbEllipsisItem${i}`}
+							onClick={onClick}
+							title={label}
+						>
+							{label}
+						</ClayDropDown.Item>
+					))}
+				</ClayDropDown.ItemList>
+			</ClayDropDown>
+		</FocusScope>
 	);
 };
