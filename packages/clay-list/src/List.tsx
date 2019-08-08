0ac5ed5cda7ee606dd798dd0885e664a46c7baba
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2019-08-08 14:15:09 -0700

    feat(list): create high-level ClayListWithItems component

---
+++ packages/clay-list/src/List.tsx
@@ -0,0 +1,56 @@
+/**
+ * © 2019 Liferay, Inc. <https://liferay.com>
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+import classNames from 'classnames';
+import Header from './Header';
+import Item from './Item';
+import ItemField from './ItemField';
+import ItemText from './ItemText';
+import ItemTitle from './ItemTitle';
+import QuickActionMenu from './QuickActionMenu';
+import React from 'react';
+
+interface IProps extends React.HTMLAttributes<HTMLUListElement> {
+	/**
+	 * Flag to indicate if action items should be shown on hover.
+	 * Defaults to `true`
+	 */
+	showQuickActionsOnHover?: boolean;
+}
+
+const ClayList: React.FunctionComponent<IProps> & {
+	Header: typeof Header;
+	Item: typeof Item;
+	ItemField: typeof ItemField;
+	ItemText: typeof ItemText;
+	ItemTitle: typeof ItemTitle;
+	QuickActionMenu: typeof QuickActionMenu;
+} = ({
+	children,
+	className,
+	showQuickActionsOnHover = true,
+	...otherProps
+}: IProps) => {
+	return (
+		<ul
+			{...otherProps}
+			className={classNames('list-group', className, {
+				'show-quick-actions-on-hover': showQuickActionsOnHover,
+			})}
+		>
+			{children}
+		</ul>
+	);
+};
+
+ClayList.Header = Header;
+ClayList.Item = Item;
+ClayList.ItemField = ItemField;
+ClayList.ItemText = ItemText;
+ClayList.ItemTitle = ItemTitle;
+ClayList.QuickActionMenu = QuickActionMenu;
+
+export default ClayList;
--- packages/clay-list/src/index.tsx
+++ packages/clay-list/src/index.tsx
@@ -4,53 +4,9 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-import classNames from 'classnames';
-import Header from './Header';
-import Item from './Item';
-import ItemField from './ItemField';
-import ItemText from './ItemText';
-import ItemTitle from './ItemTitle';
-import QuickActionMenu from './QuickActionMenu';
-import React from 'react';
+import ClayList from './List';
+import {ClayListWithItems} from './ListWithItems';
 
-interface IProps extends React.HTMLAttributes<HTMLUListElement> {
-	/**
-	 * Flag to indicate if action items should be shown on hover.
-	 * Defaults to `true`
-	 */
-	showQuickActionsOnHover?: boolean;
-}
-
-const ClayList: React.FunctionComponent<IProps> & {
-	Header: typeof Header;
-	Item: typeof Item;
-	ItemField: typeof ItemField;
-	ItemText: typeof ItemText;
-	ItemTitle: typeof ItemTitle;
-	QuickActionMenu: typeof QuickActionMenu;
-} = ({
-	children,
-	className,
-	showQuickActionsOnHover = true,
-	...otherProps
-}: IProps) => {
-	return (
-		<ul
-			{...otherProps}
-			className={classNames('list-group', className, {
-				'show-quick-actions-on-hover': showQuickActionsOnHover,
-			})}
-		>
-			{children}
-		</ul>
-	);
-};
-
-ClayList.Header = Header;
-ClayList.Item = Item;
-ClayList.ItemField = ItemField;
-ClayList.ItemText = ItemText;
-ClayList.ItemTitle = ItemTitle;
-ClayList.QuickActionMenu = QuickActionMenu;
+export {ClayListWithItems};
 
 export default ClayList;
