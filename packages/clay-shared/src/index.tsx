63371b0e685709eb0a00a143aed2c1a1df68ebb9
Matuzalém Teles <matuzalemteles@gmail.com>
2019-08-20 22:07:08 -0300

    feat(@clayui/shared): add LinkOrButton component for use internal

--- packages/clay-shared/src/index.tsx
+++ packages/clay-shared/src/index.tsx
@@ -6,10 +6,11 @@
 
 export const noop = () => {};
 export {ClayPortal} from './Portal';
+export {FocusScope} from './FocusScope';
 export {getEllipsisItems} from './getEllipsisItems';
+export {LinkOrButton} from './LinkOrButton';
+export {sub} from './sub';
 export {useDebounce} from './useDebounce';
+export {useFocusManagement} from './useFocusManagement';
 export {useKeyHandlerForList} from './useKeyHandlerForList';
 export {useTransitionHeight} from './useTransitionHeight';
-export {useFocusManagement} from './useFocusManagement';
-export {sub} from './sub';
-export {FocusScope} from './FocusScope';
