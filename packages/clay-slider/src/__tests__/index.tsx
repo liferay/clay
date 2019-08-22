e9d35ab470a2bff2ef4f869a98406b837224936a
Matuzalém Teles <matuzalemteles@gmail.com>
2019-08-21 23:58:08 -0300

    feat(@clayui/slider): add ClaySlider component

---
+++ packages/clay-slider/src/__tests__/index.tsx
@@ -0,0 +1,49 @@
+/**
+ * © 2019 Liferay, Inc. <https://liferay.com>
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+import ClaySlider from '..';
+import React from 'react';
+import {cleanup, render} from '@testing-library/react';
+
+describe('ClaySlider', () => {
+	afterEach(cleanup);
+
+	it('renders', () => {
+		const {container} = render(
+			<ClaySlider onValueChange={() => {}} value={10} />
+		);
+
+		expect(container).toMatchSnapshot();
+	});
+
+	it('renders a Slider disabled', () => {
+		const {container} = render(
+			<ClaySlider disabled onValueChange={() => {}} value={10} />
+		);
+
+		const input = container.querySelector(
+			'.form-control-range'
+		) as HTMLInputElement;
+		const range = container.querySelector('.clay-range') as HTMLDivElement;
+
+		expect(range.classList).toContain('disabled');
+		expect(input.attributes.getNamedItem('disabled')).toBeTruthy();
+	});
+
+	it('renders a Slider with the tooltip position bottom', () => {
+		const {container} = render(
+			<ClaySlider
+				onValueChange={() => {}}
+				tooltipPosition="bottom"
+				value={10}
+			/>
+		);
+
+		expect(
+			container.querySelector('.tooltip.clay-tooltip-bottom')
+		).toBeTruthy();
+	});
+});
---
+++ packages/clay-slider/src/index.tsx
@@ -0,0 +1,149 @@
+/**
+ * © 2019 Liferay, Inc. <https://liferay.com>
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+import classNames from 'classnames';
+import React, {useEffect, useRef, useState} from 'react';
+
+interface IProps extends React.InputHTMLAttributes<HTMLInputElement> {
+	/**
+	 * Flag that will disable component features.
+	 */
+	disabled?: boolean;
+
+	/**
+	 * Sets maximum permitted value.
+	 */
+	max?: number;
+
+	/**
+	 * Sets minimum permitted value
+	 */
+	min?: number;
+
+	/**
+	 * Callback will always be called when the slider value changes.
+	 */
+	onValueChange: (value: number) => void;
+
+	/**
+	 * Flag to show tooltip or not.
+	 */
+	showTooltip?: boolean;
+
+	/**
+	 * Sets stepping interval.
+	 */
+	step?: number;
+
+	/**
+	 * Set tooltip position.
+	 */
+	tooltipPosition?: 'top' | 'bottom';
+
+	/**
+	 * Set the current value of the slider.
+	 */
+	value: number;
+}
+
+const THUMB_WIDTH = 24;
+
+const calcProgressWidth = (
+	element: HTMLInputElement,
+	value: number,
+	min: number,
+	max: number,
+	step: number
+) => {
+	const currentStep = (value - min) / step;
+	const totalSteps = (max - min) / step;
+
+	const progressWidth = (currentStep / totalSteps) * 100;
+	const rangeWidth = element.clientWidth;
+	const ratio =
+		(((1 - progressWidth * 0.01) * (THUMB_WIDTH / 1.001)) / rangeWidth) *
+		100;
+	let offsetWidth = progressWidth;
+
+	if (progressWidth !== 50) {
+		offsetWidth =
+			progressWidth - (THUMB_WIDTH / 2 / rangeWidth) * 100 + ratio;
+	}
+
+	return offsetWidth;
+};
+
+const ClaySlider: React.FunctionComponent<IProps> = ({
+	className,
+	disabled,
+	max = 100,
+	min = 0,
+	onValueChange,
+	showTooltip = true,
+	step = 1,
+	tooltipPosition = 'top',
+	value,
+	...otherProps
+}) => {
+	const [offsetWidth, setOffsetWidth] = useState<number>(0);
+	const sliderRef = useRef<HTMLInputElement | null>(null);
+
+	const handleEvents = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const value = Number(event.target.value);
+
+		onValueChange(value);
+		setOffsetWidth(calcProgressWidth(event.target, value, min, max, step));
+	};
+
+	useEffect(() => {
+		if (sliderRef.current) {
+			setOffsetWidth(
+				calcProgressWidth(sliderRef.current, value, min, max, step)
+			);
+		}
+	}, []);
+
+	return (
+		<div className={classNames('clay-range', className, {disabled})}>
+			<div className="clay-range-input">
+				<input
+					{...otherProps}
+					className="form-control-range"
+					disabled={disabled}
+					max={max}
+					min={min}
+					onChange={handleEvents}
+					ref={sliderRef}
+					step={step}
+					type="range"
+					value={value}
+				/>
+				<div className="clay-range-track" />
+				<div
+					className="clay-range-progress"
+					style={{width: `${offsetWidth}%`}}
+				>
+					{showTooltip && (
+						<div
+							className={classNames(
+								'tooltip',
+								`clay-tooltip-${tooltipPosition}`
+							)}
+							role="tooltip"
+						>
+							<div className="tooltip-arrow" />
+							<div className="tooltip-inner">
+								<div className="clay-range-value">{value}</div>
+							</div>
+						</div>
+					)}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default ClaySlider;
