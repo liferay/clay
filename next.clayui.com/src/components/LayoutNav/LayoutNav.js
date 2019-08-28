c524a244d31788835007c9b7135b30e38f600e23
Matuzalém Teles <matuzalemteles@gmail.com>
2019-08-28 14:28:46 -0300

    refactor(next.clayui.com): add LayoutNav on docs pages

--- next.clayui.com/src/components/LayoutNav/LayoutNav.js
+++ next.clayui.com/src/components/LayoutNav/LayoutNav.js
@@ -10,53 +10,14 @@ import {Link} from 'gatsby';
 import Search from './Search';
 
 export default () => (
-	<React.Fragment>
-		<div className="clay-site-alert alert alert-dark alert-fixed alert-fluid">
-			<b>{'Clay V3 is currently in beta'}</b>
-			{' for ready-to-use components, use '}
-			<a
-				className="link-secondary"
-				href="https://clayui.com"
-				rel="noopener noreferrer"
-				target="_blank"
-			>
-				{'Clay V2'}
-			</a>
-			{'.'}
-		</div>
-		<nav className="navbar navbar-clay-site navbar-expand-lg navbar-dark">
-			<div className="container-fluid">
-				<Link className="navbar-brand" to="/">
-					<img
-						alt="Clay"
-						className="logo mr-2"
-						src="/images/clay_logo_w.png"
-					/>
-					<span className="title align-middle">{'Clay'}</span>
-				</Link>
-				<div className="navbar-nav-scroll">
+	<nav className="navbar navbar-clay-site navbar-expand-lg navbar-light">
+		<div className="container-fluid">
+			<div className="autofit-padded autofit-row">
+				<div className="autofit-col autofit-col-expand">
+					<Search placeholder="Search..." />
+				</div>
+				<div className="autofit-col">
 					<ul className="navbar-nav ml-auto">
-						<li className="nav-item">
-							<Link
-								className="nav-link ml-3"
-								to="/docs/get-started/introduction.html"
-							>
-								{'Get Started'}
-							</Link>
-						</li>
-						<li className="nav-item">
-							<Link className="nav-link ml-3" to="/docs/css">
-								{'Style Guide'}
-							</Link>
-						</li>
-						<li className="nav-item">
-							<Link
-								className="nav-link ml-3"
-								to="/docs/components"
-							>
-								{'Components Library'}
-							</Link>
-						</li>
 						<li className="nav-item">
 							<a
 								className="nav-link ml-3"
@@ -67,14 +28,11 @@ export default () => (
 								{'Storybook'}
 							</a>
 						</li>
-						<li className="nav-item">
+						{/* <li className="nav-item">
 							<Link className="nav-link ml-3" to="/blog">
 								{'Blog'}
 							</Link>
-						</li>
-						<li className="nav-item">
-							<Search placeholder="Search..." />
-						</li>
+						</li> */}
 						<li className="nav-item">
 							<a
 								className="mx-3 mr-lg-0"
@@ -83,14 +41,15 @@ export default () => (
 								target="_blank"
 							>
 								<img
-									alt=""
-									src="/images/home/GitHub-Mark-64px.svg"
+									alt="Github"
+									className="github-logo"
+									src="/images/home/github-logo.svg"
 								/>
 							</a>
 						</li>
 					</ul>
 				</div>
 			</div>
-		</nav>
-	</React.Fragment>
+		</div>
+	</nav>
 );
