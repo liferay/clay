4bc20618610181387c60f293db4c6932651ffbff
Diego Nascimento <diego.nascimento@liferay.com>
2019-08-28 16:59:35 -0300

    chore(@clayui/navigation-bar): Adds a state for handling `triggerLabel` property on Navigation Bar stories

--- packages/clay-navigation-bar/stories/index.tsx
+++ packages/clay-navigation-bar/stories/index.tsx
@@ -14,36 +14,50 @@ import {storiesOf} from '@storybook/react';
 
 const spritemap = require('@clayui/css/lib/images/icons/icons.svg');
 
-storiesOf('ClayNavigationBar', module).add('default', () => (
-	<ClayNavigationBar
-		inverted={boolean('Inverted: ', false)}
-		spritemap={spritemap}
-		triggerLabel={text('triggerLabel: ', 'Item 1')}
-	>
-		<ClayNavigationBar.Item active={boolean('Active 1: ', true)}>
-			<ClayLink className="nav-link" displayType="secondary" href="#1">
-				<span className="navbar-text-truncate">{`Item 1`}</span>
-			</ClayLink>
-		</ClayNavigationBar.Item>
+storiesOf('ClayNavigationBar', module).add('default', () => {
+	const [triggerName, setTriggerName] = React.useState<string>('Item 1');
+	return (
+		<ClayNavigationBar
+			inverted={boolean('Inverted: ', false)}
+			spritemap={spritemap}
+			triggerLabel={text('triggerLabel: ', triggerName)}
+		>
+			<ClayNavigationBar.Item active={boolean('Active 1: ', true)}>
+				<ClayLink
+					className="nav-link"
+					displayType="secondary"
+					href="#1"
+				>
+					<span className="navbar-text-truncate">{`Item 1`}</span>
+				</ClayLink>
+			</ClayNavigationBar.Item>
 
-		<ClayNavigationBar.Item active={boolean('Active 2: ', false)}>
-			<ClayButton block className="nav-link" displayType="unstyled" small>
-				<span className="navbar-text-truncate">{`Item 2`}</span>
-			</ClayButton>
-		</ClayNavigationBar.Item>
+			<ClayNavigationBar.Item active={boolean('Active 2: ', false)}>
+				<ClayButton
+					block
+					className="nav-link"
+					displayType="unstyled"
+					onClick={() => setTriggerName('Item 2')}
+					small
+				>
+					<span className="navbar-text-truncate">{`Item 2`}</span>
+				</ClayButton>
+			</ClayNavigationBar.Item>
 
-		<ClayNavigationBar.Item active={boolean('Active 3: ', false)}>
-			<a className="nav-link link-secondary" href="#1">
-				<span className="navbar-text-truncate">{`Item 3`}</span>
-			</a>
-		</ClayNavigationBar.Item>
-		<ClayNavigationBar.Item active={boolean('Active 4: ', false)}>
-			<button
-				className="nav-link btn btn-unstyled btn-block btn-sm"
-				type="button"
-			>
-				<span className="navbar-text-truncate">{`Item 4`}</span>
-			</button>
-		</ClayNavigationBar.Item>
-	</ClayNavigationBar>
-));
+			<ClayNavigationBar.Item active={boolean('Active 3: ', false)}>
+				<a className="nav-link link-secondary" href="#1">
+					<span className="navbar-text-truncate">{`Item 3`}</span>
+				</a>
+			</ClayNavigationBar.Item>
+			<ClayNavigationBar.Item active={boolean('Active 4: ', false)}>
+				<button
+					className="nav-link btn btn-unstyled btn-block btn-sm"
+					onClick={() => setTriggerName('Item 4')}
+					type="button"
+				>
+					<span className="navbar-text-truncate">{`Item 4`}</span>
+				</button>
+			</ClayNavigationBar.Item>
+		</ClayNavigationBar>
+	);
+});
