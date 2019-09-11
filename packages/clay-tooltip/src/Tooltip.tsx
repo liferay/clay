cb41eef3966c12562b49c45734aba250cb5e2812
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2019-09-11 10:56:08 -0700

    feat(tooltip): create Tooltip component

---
+++ packages/clay-tooltip/src/Tooltip.tsx
@@ -0,0 +1,64 @@
+/**
+ * © 2019 Liferay, Inc. <https://liferay.com>
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+import classNames from 'classnames';
+import React from 'react';
+
+export const ALIGN_POSITIONS = [
+	'top',
+	'top-left',
+	'top-right',
+	'bottom',
+	'bottom-left',
+	'bottom-right',
+	'left',
+	'right',
+] as const;
+
+interface IProps extends React.HTMLAttributes<HTMLDivElement> {
+	/**
+	 * Position in which the tooltip will be aligned to the element.
+	 */
+	alignPosition?: typeof ALIGN_POSITIONS[number];
+
+	/**
+	 * Flag to indicate if tooltip is displayed.
+	 */
+	show?: boolean;
+}
+
+const ClayTooltip = React.forwardRef<HTMLElement, IProps>(
+	(
+		{
+			alignPosition = 'bottom',
+			children,
+			className,
+			show,
+			...otherProps
+		}: IProps,
+		ref
+	) => {
+		return (
+			<div
+				className={classNames(
+					className,
+					'tooltip',
+					`clay-tooltip-${alignPosition}`,
+					{show}
+				)}
+				role="tooltip"
+				{...otherProps}
+				ref={ref as React.RefObject<HTMLDivElement>}
+			>
+				<div className="arrow" />
+
+				<div className="tooltip-inner">{children}</div>
+			</div>
+		);
+	}
+);
+
+export default ClayTooltip;
