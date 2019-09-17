20276426b1438bb24542fc606599f0cf51d5daae
Diego Nascimento <diego.nascimento@liferay.com>
2019-09-17 12:28:36 +0200

    chore(@clayui/table): Adds a new line after imports

--- packages/clay-table/src/Row.tsx
+++ packages/clay-table/src/Row.tsx
@@ -7,6 +7,7 @@
 import classNames from 'classnames';
 import React from 'react';
 import {TDelimiter} from './types';
+
 interface IRowProps extends React.HTMLAttributes<HTMLTableRowElement> {
 	/**
 	 * Forces the active state inside the row.
