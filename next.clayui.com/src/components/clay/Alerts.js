492a57602f38afa55346c6b18226004c66e10490
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2019-09-19 12:32:25 +0200

    docs(alert): update docs

--- next.clayui.com/src/components/clay/Alerts.js
+++ next.clayui.com/src/components/clay/Alerts.js
@@ -18,8 +18,8 @@ const AlertCode = `const Component = () => {
 			</ClayAlert>
 
 			<ClayAlert displayType="warning" spritemap={spritemap} title="Warning" variant="stripe">
-                This is a stripe alert
-            </ClayAlert>
+				This is a stripe alert
+			</ClayAlert>
 		</>
 	);
 }
@@ -29,6 +29,48 @@ render(<Component />);`;
 export const Alert = () => {
 	const scope = {ClayAlert, spritemap, useState};
 	const code = AlertCode;
+
+	return <Editor code={code} scope={scope} />;
+};
+
+const AlertToastContainerCode = `const Component = () => {
+	const [toastItems, setToastItems] = useState([]);
+	return (
+		<>
+			<div>
+				<button
+					onClick={() =>
+						setToastItems([...toastItems, Math.random() * 100])
+					}
+				>
+					{'Add Alert'}
+				</button>
+			</div>
+
+			<ClayAlert.ToastContainer>
+				{toastItems.map(value => (
+					<ClayAlert
+						autoClose={5000}
+						key={value}
+						onClose={() => {
+							setToastItems(prevItems =>
+								prevItems.filter(item => item !== value)
+							);
+						}}
+						spritemap={spritemap}
+						title={'Hola:'}
+					>{\`My value is \${value}\`}</ClayAlert>
+				))}
+			</ClayAlert.ToastContainer>
+		</>
+	);
+}
+
+render(<Component />);`;
+
+export const AlertToastContainer = () => {
+	const scope = {ClayAlert, spritemap, useState};
+	const code = AlertToastContainerCode;
 
 	return <Editor code={code} scope={scope} />;
 };
