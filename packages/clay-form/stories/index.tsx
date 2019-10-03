8533397731ad53dd7ce4fa133a4bb6db121995e9
Diego Nascimento <diego.nascimento@liferay.com>
2019-10-03 10:54:01 -0300

    docs(@clayui/form): Adds Stories for Form and Form Groups

--- packages/clay-form/stories/index.tsx
+++ packages/clay-form/stories/index.tsx
@@ -4,11 +4,14 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
+const spritemap = require('@clayui/css/lib/images/icons/icons.svg');
 import {storiesOf} from '@storybook/react';
 import React from 'react';
 
+import ClayForm from '../src/Form';
 import {
 	ClayCheckbox,
+	ClayInput,
 	ClayRadio,
 	ClayRadioGroup,
 	ClaySelect,
@@ -52,6 +55,90 @@ storiesOf('Components|ClayCheckbox', module)
 				<span className="badge-item badge-item-expand">{'10'}</span>
 			</span>
 		</ClayCheckbox>
+	));
+
+storiesOf('Components|ClayForm', module)
+	.add('default', () => (
+		<div className="sheet">
+			<h4 className="sheet-subtitle">{'Form Example'}</h4>
+			<ClayForm>
+				<ClayForm.Group className="form-group-sm">
+					<label htmlFor="basicInput">{'Name'}</label>
+					<ClayInput placeholder="Name" type="text" />
+				</ClayForm.Group>
+				<ClayForm.Group className="form-group-sm">
+					<label htmlFor="basicInput">{'Description'}</label>
+					<textarea
+						className="form-control"
+						placeholder="Description"
+					/>
+				</ClayForm.Group>
+			</ClayForm>
+		</div>
+	))
+	.add('validation', () => (
+		<div className="sheet">
+			<h4 className="sheet-subtitle">{'Form Validation Example'}</h4>
+			<ClayForm>
+				<ClayForm.Group className="has-success">
+					<label htmlFor="basicInput">{'Name'}</label>
+					<ClayInput placeholder="Name" type="text" />
+					<ClayForm.FeedbackGroup>
+						<ClayForm.FeedbackItem>
+							{'This is a form-feedback-item.'}
+						</ClayForm.FeedbackItem>
+						<ClayForm.FeedbackItem>
+							<ClayForm.FeedbackIndicator
+								spritemap={spritemap}
+								symbol="check-circle-full"
+							/>
+							{
+								'This is a form-feedback-item with a check feedback indicator.'
+							}
+						</ClayForm.FeedbackItem>
+					</ClayForm.FeedbackGroup>
+				</ClayForm.Group>
+				<ClayForm.Group className="has-warning">
+					<label htmlFor="basicInput">{'Description'}</label>
+					<textarea
+						className="form-control"
+						placeholder="Description"
+					/>
+					<ClayForm.FeedbackGroup>
+						<ClayForm.FeedbackItem>
+							{'This is a form-feedback-item.'}
+						</ClayForm.FeedbackItem>
+						<ClayForm.FeedbackItem>
+							<ClayForm.FeedbackIndicator
+								spritemap={spritemap}
+								symbol="warning-full"
+							/>
+							{
+								'This is a form-feedback-item with a warning feedback indicator.'
+							}
+						</ClayForm.FeedbackItem>
+					</ClayForm.FeedbackGroup>
+				</ClayForm.Group>
+				<ClayForm.Group className="has-error">
+					<label htmlFor="basicInput">{'Password'}</label>
+					<ClayInput placeholder="Name" type="password" />
+					<ClayForm.FeedbackGroup>
+						<ClayForm.FeedbackItem>
+							{'This is a form-feedback-item.'}
+						</ClayForm.FeedbackItem>
+						<ClayForm.FeedbackItem>
+							<ClayForm.FeedbackIndicator
+								spritemap={spritemap}
+								symbol="exclamation-full"
+							/>
+							{
+								'This is a form-feedback-item with a error feedback indicator.'
+							}
+						</ClayForm.FeedbackItem>
+					</ClayForm.FeedbackGroup>
+				</ClayForm.Group>
+			</ClayForm>
+		</div>
 	));
 
 const RadioGroupWithState = ({inline}: {inline?: boolean}) => {
