a24ce70675fa8e8cd5061ab3cc94951d6e787e72
Diego Nascimento <diego.nascimento@liferay.com>
2019-10-14 15:02:32 -0300

    fix(@clayui/multi-step-nav): Adds missing types for making APIDocs work

--- packages/clay-multi-step-nav/src/MultiStepNavWithBasicItems.tsx
+++ packages/clay-multi-step-nav/src/MultiStepNavWithBasicItems.tsx
@@ -71,7 +71,7 @@ export const ClayMultiStepNavWithBasicItems: React.FunctionComponent<
 	spritemap,
 	steps,
 	...otherProps
-}) => {
+}: IProps) => {
 	let dropdownItems;
 	let showSteps = steps;
 	const indexEnd = steps.length - 1;
