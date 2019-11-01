a5026c67691a7343c53f33db9bfa008f8572b689
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2019-11-01 12:15:57 -0700

    chore: refactor forwardRef components

--- packages/clay-autocomplete/src/Item.tsx
+++ packages/clay-autocomplete/src/Item.tsx
@@ -9,8 +9,6 @@ import fuzzy from 'fuzzy';
 import React from 'react';
 
 interface IProps extends React.ComponentProps<typeof ClayDropDown.Item> {
-	forwardRef?: React.Ref<HTMLLIElement>;
-
 	innerRef?: React.Ref<HTMLAnchorElement>;
 
 	/**
@@ -27,32 +25,24 @@ interface IProps extends React.ComponentProps<typeof ClayDropDown.Item> {
 
 const optionsFuzzy = {post: '</strong>', pre: '<strong>'};
 
-const ClayAutocompleteItem: React.FunctionComponent<IProps> = ({
-	forwardRef,
-	innerRef,
-	match = '',
-	value,
-	...otherProps
-}: IProps) => {
-	const fuzzyMatch = fuzzy.match(match, value, optionsFuzzy);
+const ClayAutocompleteItem = React.forwardRef<HTMLLIElement, IProps>(
+	({innerRef, match = '', value, ...otherProps}: IProps, ref) => {
+		const fuzzyMatch = fuzzy.match(match, value, optionsFuzzy);
 
-	return (
-		<ClayDropDown.Item {...otherProps} innerRef={innerRef} ref={forwardRef}>
-			{match && fuzzyMatch ? (
-				<div
-					dangerouslySetInnerHTML={{
-						__html: fuzzyMatch.rendered,
-					}}
-				/>
-			) : (
-				value
-			)}
-		</ClayDropDown.Item>
-	);
-};
-
-export default React.forwardRef(
-	(props: Omit<IProps, 'forwardRef'>, ref?: React.Ref<HTMLLIElement>) => (
-		<ClayAutocompleteItem {...props} forwardRef={ref} />
-	)
+		return (
+			<ClayDropDown.Item {...otherProps} innerRef={innerRef} ref={ref}>
+				{match && fuzzyMatch ? (
+					<div
+						dangerouslySetInnerHTML={{
+							__html: fuzzyMatch.rendered,
+						}}
+					/>
+				) : (
+					value
+				)}
+			</ClayDropDown.Item>
+		);
+	}
 );
+
+export default ClayAutocompleteItem;
