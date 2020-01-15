3fb35316de10a0f10640df1b930844647793ca07
Krešimir Čoko <kresimir.coko@liferay.com>
2020-01-15 11:02:09 +0100

    feat(clayui.com): Collapsible code in Markup/CSS tab

--- clayui.com/plugins/gatsby-remark-use-clipboard/index.js
+++ clayui.com/plugins/gatsby-remark-use-clipboard/index.js
@@ -12,9 +12,28 @@ module.exports = ({markdownAST}) => {
 			return;
 		}
 
-		node.value = `<div class="code-container">
-            <button class="btn btn-sm btn-copy" title="Copy"><svg class="lexicon-icon"><use xlink:href="/images/icons/icons.svg#paste"></use></svg></button>
-            ${node.value}
-        </div>`;
+		node.value = `
+			<div class="code-container">
+				<button class="btn btn-sm btn-copy btn-monospaced btn-unstyled" title="Copy">
+					<svg class="lexicon-icon">
+						<use xlink:href="/images/icons/icons.svg#paste"></use>
+					</svg>
+				</button>
+				
+				<button class="btn btn-sm btn-collapse btn-collapse--collapse btn-monospaced btn-unstyled" title="Collapse">
+					<svg class="lexicon-icon icon-collapse">
+						<use xlink:href="/images/icons/icons.svg#angle-down"></use>
+					</svg>
+				</button>
+				
+				<button class="btn btn-sm btn-collapse btn-collapse--expand btn-monospaced btn-unstyled hide" title="Expand">
+					<svg class="lexicon-icon icon-collapse">
+						<use xlink:href="/images/icons/icons.svg#angle-right"></use>
+					</svg>
+				</button>
+
+				${node.value}
+			</div>
+		`;
 	});
 };
