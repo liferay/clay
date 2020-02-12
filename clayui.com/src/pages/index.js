344091af1fce298001ed3537f1c6650292599fa3
plhnk <paul.hanaoka@gmail.com>
2020-02-12 11:35:21 -0800

    fix(clayui.com): make the footer more accurate --> "Clay Team" to "Clay Contributors"

--- clayui.com/src/pages/index.js
+++ clayui.com/src/pages/index.js
@@ -395,7 +395,7 @@ export default () => {
 									rel="noopener noreferrer"
 									target="_blank"
 								>
-									{'Clay Team'}
+									{'Clay Contributors'}
 								</a>
 							</div>
 
