9d700c060f657c0439370f8ea5dcca677c9c5e47
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2020-02-13 11:47:30 -0800

    fix(@clayui/popover): make 'show' prop actually usable

--- packages/clay-popover/stories/index.tsx
+++ packages/clay-popover/stories/index.tsx
@@ -5,7 +5,7 @@
  */
 
 import '@clayui/css/lib/css/atlas.css';
-import {ClayButtonWithIcon} from '@clayui/button';
+import ClayButton, {ClayButtonWithIcon} from '@clayui/button';
 const spritemap = require('@clayui/css/lib/images/icons/icons.svg');
 import {boolean, select} from '@storybook/addon-knobs';
 import {storiesOf} from '@storybook/react';
@@ -64,5 +64,41 @@ storiesOf('Components|ClayPopover', module)
 				spoon acerbic to go macchiato strong.`}
 				</ClayPopover>
 			</div>
+		);
+	})
+	.add('popover w/ manual show', () => {
+		const [show, setShow] = React.useState(false);
+
+		return (
+			<>
+				<ClayButton
+					onClick={() => setShow(!show)}
+					style={{marginRight: 40}}
+				>
+					{'additional trigger'}
+				</ClayButton>
+
+				<ClayPopover
+					header="Popover"
+					onShowChange={setShow}
+					show={show}
+					trigger={
+						<ClayButtonWithIcon
+							spritemap={spritemap}
+							symbol="info-circle-open"
+						/>
+					}
+				>
+					{`Viennese flavour cup eu, percolator froth ristretto mazagran
+				caffeine. White roast seasonal, mocha trifecta, dripper caffeine
+				spoon acerbic to go macchiato strong. Viennese flavour cup eu, percolator froth ristretto mazagran
+				caffeine. White roast seasonal, mocha trifecta, dripper caffeine
+				spoon acerbic to go macchiato strong. Viennese flavour cup eu, percolator froth ristretto mazagran
+				caffeine. White roast seasonal, mocha trifecta, dripper caffeine
+				spoon acerbic to go macchiato strong. Viennese flavour cup eu, percolator froth ristretto mazagran
+				caffeine. White roast seasonal, mocha trifecta, dripper caffeine
+				spoon acerbic to go macchiato strong.`}
+				</ClayPopover>
+			</>
 		);
 	});
