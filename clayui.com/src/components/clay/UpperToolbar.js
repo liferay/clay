e89f81eabb7d609d0db4ba4c170e31583946e68f
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2020-02-26 14:45:44 -0800

    feat(@clayui/upper-toolbar): Create basic component and docs

---
+++ clayui.com/src/components/clay/UpperToolbar.js
@@ -0,0 +1,105 @@
+/**
+ * SPDX-FileCopyrightText: © 2019 Liferay, Inc. <https://liferay.com>
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+import ClayButton, {ClayButtonWithIcon} from '@clayui/button';
+import {ClayDropDownWithItems} from '@clayui/drop-down';
+import ClayIcon from '@clayui/icon';
+import ClayUpperToolbar from '@clayui/upper-toolbar';
+import React from 'react';
+
+import Editor from './Editor';
+
+const spritemap = '/images/icons/icons.svg';
+
+const upperToolbarImportsCode = `import ClayButton, {ClayButtonWithIcon} from '@clayui/button';
+import ClayIcon from '@clayui/icon';
+import {ClayDropDownWithItems} from '@clayui/drop-down';
+import ClayUpperToolbar from '@clayui/upper-toolbar';
+import React from 'react';`;
+
+const UpperToolbarCode = `const Component = () => {
+	return (
+		<ClayUpperToolbar>
+			<ClayUpperToolbar.Item className="text-left" expand>
+				<label className="component-title">{'Foo Bar'}</label>
+
+				<ClayIcon spritemap={spritemap} symbol="lock" />
+			</ClayUpperToolbar.Item>
+
+			<ClayUpperToolbar.Input placeholder="Search..." />
+
+			<ClayUpperToolbar.Item>
+				<ClayButton.Group>
+					<ClayButtonWithIcon
+						displayType="secondary"
+						onClick={() => {}}
+						small
+						spritemap={spritemap}
+						symbol="angle-left"
+					/>
+
+					<ClayButtonWithIcon
+						displayType="secondary"
+						onClick={() => {}}
+						small
+						spritemap={spritemap}
+						symbol="angle-right"
+					/>
+				</ClayButton.Group>
+			</ClayUpperToolbar.Item>
+
+			<ClayUpperToolbar.Item>
+				<ClayButton displayType="secondary" onClick={() => {}}>
+					{'Delete'}
+				</ClayButton>
+
+				<ClayButton className="inline-item-after" onClick={() => {}}>
+					{'Edit'}
+				</ClayButton>
+			</ClayUpperToolbar.Item>
+
+			<ClayUpperToolbar.Item>
+				<ClayDropDownWithItems
+					items={[
+						{href: '#one', label: 'one'},
+						{href: '#two', label: 'two'},
+						{disabled: true, href: '#three', label: 'three'},
+						{href: '#four', label: 'four'},
+					]}
+					spritemap={spritemap}
+					trigger={
+						<ClayButtonWithIcon
+							displayType="unstyled"
+							small
+							spritemap={spritemap}
+							symbol="ellipsis-v"
+						/>
+					}
+				/>
+			</ClayUpperToolbar.Item>
+		</ClayUpperToolbar>
+	)
+};
+
+render(<Component />);`;
+
+export const UpperToolbarExample = () => {
+	const scope = {
+		ClayButton,
+		ClayButtonWithIcon,
+		ClayDropDownWithItems,
+		ClayIcon,
+		ClayUpperToolbar,
+		spritemap,
+	};
+
+	return (
+		<Editor
+			code={UpperToolbarCode}
+			imports={upperToolbarImportsCode}
+			scope={scope}
+		/>
+	);
+};
--- packages/clay-upper-toolbar/src/index.tsx
+++ packages/clay-upper-toolbar/src/index.tsx
@@ -1,21 +1,80 @@
 /**
- * © 2019 Liferay, Inc. <https://liferay.com>
- *
+ * SPDX-FileCopyrightText: © 2020 Liferay, Inc. <https://liferay.com>
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-import React from 'react';
+import {ClayInput} from '@clayui/form';
 import classNames from 'classnames';
+import React from 'react';
 
-interface Props extends React.HTMLAttributes<HTMLDivElement> {}
+export interface IInputProps extends React.ComponentProps<typeof ClayInput> {}
 
-const ClayUpperToolbar: React.FunctionComponent<Props> = ({
+const Input: React.FunctionComponent<IInputProps> = ({
 	className,
 	...otherProps
-}) => {
+}) => (
+	<Item expand>
+		<ClayInput.Group>
+			<ClayInput.GroupItem>
+				<ClayInput
+					className={classNames(className, 'form-control-inline')}
+					type="text"
+					{...otherProps}
+				/>
+			</ClayInput.GroupItem>
+		</ClayInput.Group>
+	</Item>
+);
+
+Input.displayName = 'Input';
+
+interface IItemProps extends React.HTMLAttributes<HTMLLIElement> {
+	/**
+	 * Flag to indicate if Item should auto expand to fill the remaining width.
+	 */
+	expand?: boolean;
+}
+
+const Item: React.FunctionComponent<IItemProps> = ({
+	children,
+	className,
+	expand,
+	...otherProps
+}: IItemProps) => {
 	return (
-		<div {...otherProps} className={classNames(className)}>{'ClayUpperToolbar'}</div>
+		<li
+			className={classNames(className, 'tbar-item', {
+				'tbar-item-expand': expand,
+			})}
+			{...otherProps}
+		>
+			<div className="tbar-section">{children}</div>
+		</li>
 	);
 };
 
-export default ClayUpperToolbar;
+Item.displayName = 'Item';
+
+const ClayUpperToolbar: React.FunctionComponent<
+	React.HTMLAttributes<HTMLElement>
+> = ({children, className, ...otherProps}) => {
+	return (
+		<nav
+			className={classNames(
+				className,
+				'component-tbar',
+				'subnav-tbar-light',
+				'tbar'
+			)}
+			{...otherProps}
+		>
+			<div className="container-fluid container-fluid-max-xl">
+				<ul className="tbar-nav">{children}</ul>
+			</div>
+		</nav>
+	);
+};
+
+ClayUpperToolbar.displayName = 'ClayUpperToolbar';
+
+export default Object.assign(ClayUpperToolbar, {Input, Item});
