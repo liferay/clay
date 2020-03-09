8a6c2d910e6014d989fad6802959927e7bc20d49
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2020-03-09 14:16:12 -0700

    chore(@clayui/empty-state): run format

--- packages/clay-empty-state/src/index.tsx
+++ packages/clay-empty-state/src/index.tsx
@@ -1,4 +1,9 @@
 /**
+ * SPDX-FileCopyrightText: © 2020 Liferay, Inc. <https://liferay.com>
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+/**
  * © 2019 Liferay, Inc. <https://liferay.com>
  *
  * SPDX-License-Identifier: BSD-3-Clause
