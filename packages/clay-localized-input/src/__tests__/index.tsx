3638657c19ef9e4ee2f1ec863411260c3e4d7ea1
Krešimir Čoko <kresimir.coko@liferay.com>
2020-03-11 14:56:02 +0100

    feat(@clayui/localized-input): create LocalizedInput component

---
+++ packages/clay-localized-input/src/__tests__/index.tsx
@@ -0,0 +1,112 @@
+/**
+ * SPDX-FileCopyrightText: © 2020 Liferay, Inc. <https://liferay.com>
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+/**
+ * © 2019 Liferay, Inc. <https://liferay.com>
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+import ClayLocalizedInput from '..';
+import {cleanup, fireEvent, render} from '@testing-library/react';
+import React from 'react';
+
+const locales = [
+	{
+		label: 'en-US',
+		symbol: 'en-us',
+	},
+	{
+		label: 'es-ES',
+		symbol: 'es-es',
+	},
+	{
+		label: 'fr-FR',
+		symbol: 'fr-fr',
+	},
+	{
+		label: 'hr-HR',
+		symbol: 'hr-hr',
+	},
+];
+
+describe('ClayLocalizedInput', () => {
+	afterEach(cleanup);
+
+	it('renders', () => {
+		const {container} = render(
+			<ClayLocalizedInput
+				id="locale1"
+				locales={locales}
+				onSelectedLocaleChange={() => {}}
+				onTranslationsChange={() => {}}
+				selectedLocale={locales[0]}
+				spritemap="/path/to/svg"
+				translations={{
+					'en-US': 'Apple',
+					'es-ES': 'Manzana',
+				}}
+			/>
+		);
+
+		expect(container).toMatchSnapshot();
+	});
+
+	it('renders with prepend content and result formatter', () => {
+		const prepend = '/home/';
+
+		const {container} = render(
+			<ClayLocalizedInput
+				id="locale1"
+				locales={locales}
+				onSelectedLocaleChange={() => {}}
+				onTranslationsChange={() => {}}
+				prependContent={prepend}
+				resultFormatter={val => `https://liferay.com${prepend}${val}`}
+				selectedLocale={locales[0]}
+				spritemap="/path/to/svg"
+				translations={{
+					'en-US': 'Apple',
+					'es-ES': 'Manzana',
+				}}
+			/>
+		);
+
+		expect(container).toMatchSnapshot();
+	});
+
+	it("displays empty if translation doesn't exist", () => {
+		const onSelectedChangeFn = jest.fn();
+		const {container} = render(
+			<ClayLocalizedInput
+				id="input1"
+				locales={locales}
+				onSelectedLocaleChange={onSelectedChangeFn}
+				onTranslationsChange={() => {}}
+				selectedLocale={locales[0]}
+				spritemap="/path/to/svg"
+				translations={{
+					'en-US': 'Apple',
+					'es-ES': 'Manzana',
+				}}
+			/>
+		);
+
+		fireEvent.click(
+			container.querySelector('.dropdown-toggle') as HTMLButtonElement,
+			{}
+		);
+
+		fireEvent.click(
+			document.querySelectorAll('.dropdown-item')[1] as HTMLButtonElement,
+			{}
+		);
+
+		expect(onSelectedChangeFn).toBeCalledWith({
+			label: 'es-ES',
+			symbol: 'es-es',
+		});
+	});
+});
