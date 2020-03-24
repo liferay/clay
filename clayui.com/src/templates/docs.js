bb82028ef4a643ac172bbc8ee6b0e296d37b8c53
Patrick Yeo <patrick.yeo@liferay.com>
2020-03-24 10:58:17 -0700

    docs(clayui.com): Sidebar Navigation should use c-inner to better focus interactions
    docs(clayui.com): Site Nav should use c-inner
    
    docs(clayui.com): Docs template use c-inner for tabs and clay-site-labels
    
    docs(clayui.com): Nav Toc use browser default focus
    
    docs(clayui.com): Code copy and collapse buttons should have focus state when tabbing to them
    
    fixes #3042

--- clayui.com/src/templates/docs.js
+++ clayui.com/src/templates/docs.js
@@ -125,9 +125,14 @@ export default props => {
 																	id="advancedTabs"
 																	role="tab"
 																>
-																	{
-																		'React Component'
-																	}
+																	<span
+																		className="c-inner"
+																		tabIndex="-1"
+																	>
+																		{
+																			'React Component'
+																		}
+																	</span>
 																</a>
 															</li>
 															<li className="nav-item">
@@ -139,9 +144,14 @@ export default props => {
 																	id="simpleTab"
 																	role="tab"
 																>
-																	{
-																		'CSS / Markup'
-																	}
+																	<span
+																		className="c-inner"
+																		tabIndex="-1"
+																	>
+																		{
+																			'CSS / Markup'
+																		}
+																	</span>
 																</a>
 															</li>
 														</ul>
@@ -175,37 +185,41 @@ export default props => {
 																{fields && (
 																	<>
 																		{fields.packageStatus && (
-																			<Link to="/docs/get-started/how-to-read-this-documentation.html">
+																			<Link
+																				className={`label label-lg label-${
+																					mapStatus[
+																						fields.packageStatus.toLowerCase()
+																					]
+																				}`}
+																				to="/docs/get-started/how-to-read-this-documentation.html"
+																			>
 																				<span
-																					className={`label label-lg label-${
-																						mapStatus[
-																							fields.packageStatus.toLowerCase()
-																						]
-																					}`}
+																					className="c-inner"
+																					tabIndex="-1"
 																				>
-																					<span className="label-item label-item-expand">
-																						{
-																							fields.packageStatus
-																						}
-																					</span>
+																					{
+																						fields.packageStatus
+																					}
 																				</span>
 																			</Link>
 																		)}
 
 																		{fields.packageVersion && (
 																			<a
+																				className="label label-lg label-secondary"
 																				href={`https://www.npmjs.com/package/${
 																					frontmatter.packageNpm
 																				}`}
 																				rel="noopener noreferrer"
 																				target="_blank"
 																			>
-																				<span className="label label-lg label-secondary">
-																					<span className="label-item label-item-expand">
-																						{
-																							fields.packageVersion
-																						}
-																					</span>
+																				<span
+																					className="c-inner"
+																					tabIndex="-1"
+																				>
+																					{
+																						fields.packageVersion
+																					}
 																				</span>
 																			</a>
 																		)}
@@ -215,6 +229,7 @@ export default props => {
 																{frontmatter.packageNpm && (
 																	<>
 																		<a
+																			className="label label-lg label-secondary"
 																			href={`https://github.com/liferay/clay/blob/master/packages/${frontmatter.packageNpm.replace(
 																				'@clayui/',
 																				'clay-'
@@ -222,16 +237,18 @@ export default props => {
 																			rel="noopener noreferrer"
 																			target="_blank"
 																		>
-																			<span className="label label-lg label-secondary">
-																				<span className="label-item label-item-expand">
-																					{
-																						'CHANGELOG'
-																					}
-																				</span>
+																			<span
+																				className="c-inner"
+																				tabIndex="-1"
+																			>
+																				{
+																					'CHANGELOG'
+																				}
 																			</span>
 																		</a>
 
 																		<a
+																			className="align-middle d-inline-block link-primary"
 																			href={`https://storybook.clayui.com/?path=/story/components-${frontmatter.packageNpm
 																				.replace(
 																					'@clayui/',
@@ -244,10 +261,16 @@ export default props => {
 																			rel="noopener noreferrer"
 																			target="_blank"
 																		>
-																			<img
-																				alt="storybook demos"
-																				src="/images/storybook_badge.svg"
-																			/>
+																			<span
+																				className="c-inner d-block"
+																				tabIndex="-1"
+																			>
+																				<img
+																					alt="storybook demos"
+																					className="d-block"
+																					src="/images/storybook_badge.svg"
+																				/>
+																			</span>
 																		</a>
 																	</>
 																)}
@@ -259,6 +282,8 @@ export default props => {
 																{markdownJsx ? (
 																	<MDXProvider
 																		components={{
+																			a:
+																				Typography.A,
 																			h1:
 																				Typography.H1,
 																			h2:
@@ -326,6 +351,8 @@ export default props => {
 																<CodeClipboard>
 																	<MDXProvider
 																		components={{
+																			a:
+																				Typography.A,
 																			h1:
 																				Typography.H1,
 																			h2:
@@ -391,15 +418,20 @@ export default props => {
 															rel="noopener noreferrer"
 															target="_blank"
 														>
-															<svg
-																className="lexicon-icon"
-																height="24"
-																name="github"
-																viewBox="12 12 24 24"
-																width="24"
+															<span
+																className="c-inner"
+																tabIndex="-1"
 															>
-																<path d="M24.06 12C17.426 12 12 17.395 12 23.988c0 5.275 3.497 9.83 8.2 11.389.603.12.845-.24.845-.6V32.74c-3.377.72-4.1-1.558-4.1-1.558-.483-1.439-1.327-1.799-1.327-1.799-1.085-.719.12-.719.12-.719 1.206.12 1.81 1.199 1.81 1.199 1.085 1.918 2.894 1.319 3.497 1.079.12-.72.361-1.319.723-1.558-2.532-.36-5.427-1.439-5.427-5.994 0-1.32.483-2.398 1.206-3.237-.12-.36-.482-1.559.12-3.237 0 0 .966-.36 3.257 1.199 1.085-.12 2.17-.36 3.135-.36.965 0 2.05.12 3.015.36 2.291-1.559 3.256-1.199 3.256-1.199.603 1.678.241 2.877.12 3.117a4.854 4.854 0 0 1 1.207 3.237c0 4.555-2.774 5.634-5.548 5.874.483.36.844 1.079.844 2.277v3.237c0 .36.242.72.845.6 4.823-1.559 8.2-6.114 8.2-11.389C36.118 17.395 30.692 12 24.059 12z" />
-															</svg>
+																<svg
+																	className="lexicon-icon"
+																	height="24"
+																	name="github"
+																	viewBox="12 12 24 24"
+																	width="24"
+																>
+																	<path d="M24.06 12C17.426 12 12 17.395 12 23.988c0 5.275 3.497 9.83 8.2 11.389.603.12.845-.24.845-.6V32.74c-3.377.72-4.1-1.558-4.1-1.558-.483-1.439-1.327-1.799-1.327-1.799-1.085-.719.12-.719.12-.719 1.206.12 1.81 1.199 1.81 1.199 1.085 1.918 2.894 1.319 3.497 1.079.12-.72.361-1.319.723-1.558-2.532-.36-5.427-1.439-5.427-5.994 0-1.32.483-2.398 1.206-3.237-.12-.36-.482-1.559.12-3.237 0 0 .966-.36 3.257 1.199 1.085-.12 2.17-.36 3.135-.36.965 0 2.05.12 3.015.36 2.291-1.559 3.256-1.199 3.256-1.199.603 1.678.241 2.877.12 3.117a4.854 4.854 0 0 1 1.207 3.237c0 4.555-2.774 5.634-5.548 5.874.483.36.844 1.079.844 2.277v3.237c0 .36.242.72.845.6 4.823-1.559 8.2-6.114 8.2-11.389C36.118 17.395 30.692 12 24.059 12z" />
+																</svg>
+															</span>
 														</a>
 													</li>
 												</ul>
