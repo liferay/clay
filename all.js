dcbf0978cbb0da443e5cc22ff05576b289e0ac98
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2020-04-28 15:14:09 -0700

    chore: use `size-limit` to set hard limits for total size

---
+++ all.js
@@ -0,0 +1,43 @@
+/**
+ * SPDX-FileCopyrightText: © 2020 Liferay, Inc. <https://liferay.com>
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+export * from '@clayui/alert';
+export * from '@clayui/autocomplete';
+export * from '@clayui/badge';
+export * from '@clayui/breadcrumb';
+export * from '@clayui/button';
+export * from '@clayui/card';
+// export * from '@clayui/charts'; This package alone practically doubles the build
+export * from '@clayui/color-picker';
+export * from '@clayui/data-provider';
+export * from '@clayui/date-picker';
+export * from '@clayui/drop-down';
+export * from '@clayui/empty-state';
+export * from '@clayui/form';
+export * from '@clayui/icon';
+export * from '@clayui/label';
+export * from '@clayui/link';
+export * from '@clayui/list';
+export * from '@clayui/loading-indicator';
+export * from '@clayui/localized-input';
+export * from '@clayui/management-toolbar';
+export * from '@clayui/modal';
+export * from '@clayui/multi-select';
+export * from '@clayui/multi-step-nav';
+export * from '@clayui/nav';
+export * from '@clayui/navigation-bar';
+export * from '@clayui/pagination';
+export * from '@clayui/pagination-bar';
+export * from '@clayui/panel';
+export * from '@clayui/popover';
+export * from '@clayui/progress-bar';
+export * from '@clayui/shared';
+export * from '@clayui/slider';
+export * from '@clayui/sticker';
+export * from '@clayui/table';
+export * from '@clayui/tabs';
+export * from '@clayui/time-picker';
+export * from '@clayui/tooltip';
+export * from '@clayui/upper-toolbar';
