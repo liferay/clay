4a45b295b18fb27983ad2f99e22b558c58e3306e
Krešimir Čoko <kresimir.coko@liferay.com>
2020-05-08 10:18:23 +0200

    feat(clayui.com): Custom 404 page Markup/CSS fixes as suggested by Patrick

--- clayui.com/src/pages/404.js
+++ clayui.com/src/pages/404.js
@@ -15,8 +15,8 @@ const spritemap = '/images/icons/icons.svg';
 const PageNotFound = () => {
 	return (
 		<div className="clay-404-page-container d-flex flex-column min-vh-100">
-			<header className="d-flex">
-				<div className="container-fluid-max-xl d-flex mb-0 ml-auto mr-auto mt-0 w-100">
+			<header className="bg-white d-flex">
+				<div className="container-fluid container-fluid-max-xl d-flex px-0">
 					<Link className="d-none d-sm-flex mx-3" to="/">
 						<img
 							alt="Clay"
@@ -29,61 +29,56 @@ const PageNotFound = () => {
 				</div>
 			</header>
 
-			<div className="align-items-center container-fluid-max-lg d-flex justify-content-center m-auto main-content row">
-				<h1 className="col-12 display-3">
-					{"These aren't the components you're looking for."}
+			<div className="container-fluid container-fluid-max-lg d-flex flex-column flex-grow-1 justify-content-center main-content">
+				<h1 className="display-3">
+					{"This isn't the page you're looking for."}
 				</h1>
 
-				<div className="col-12 container-fluid">
-					<p className="my-2 py-2">
-						{
-							"Hey there, we couldn't find the page that you were looking for."
-						}
-						<br />
-						{"Maybe it's one of these:"}
-					</p>
+				<p className="mb-0 py-3">
+					{
+						"Hey there, we couldn't find the page that you were looking for."
+					}
+					<br />
+					{"Maybe it's one of these:"}
+				</p>
 
-					<nav className="list-inline mt-3">
-						<Link
-							className="align-middle list-inline-item mb-3 mr-3 px-3 py-2 rounded shadow text-decoration-none"
-							to="/"
-						>
-							<ClayIcon spritemap={spritemap} symbol="home" />
+				<nav className="list-inline mt-2">
+					<Link
+						className="align-middle list-inline-item mb-3 mr-3 px-3 py-2 rounded shadow text-decoration-none"
+						to="/"
+					>
+						<ClayIcon spritemap={spritemap} symbol="home" />
 
-							<span className="pl-2">{'Home'}</span>
-						</Link>
+						<span className="pl-2">{'Home'}</span>
+					</Link>
 
-						<Link
-							className="align-middle list-inline-item mb-3 mr-3 px-3 py-2 rounded shadow text-decoration-none"
-							to="/docs/get-started/index.html"
-						>
-							<ClayIcon spritemap={spritemap} symbol="bolt" />
+					<Link
+						className="align-middle list-inline-item mb-3 mr-3 px-3 py-2 rounded shadow text-decoration-none"
+						to="/docs/get-started/index.html"
+					>
+						<ClayIcon spritemap={spritemap} symbol="bolt" />
 
-							<span className="pl-2">{'Get Started'}</span>
-						</Link>
+						<span className="pl-2">{'Get Started'}</span>
+					</Link>
 
-						<Link
-							className="align-middle list-inline-item mb-3 mr-3 px-3 py-2 rounded shadow text-decoration-none"
-							to="/docs/components/index.html"
-						>
-							<ClayIcon
-								spritemap={spritemap}
-								symbol="plus-squares"
-							/>
+					<Link
+						className="align-middle list-inline-item mb-3 mr-3 px-3 py-2 rounded shadow text-decoration-none"
+						to="/docs/components/index.html"
+					>
+						<ClayIcon spritemap={spritemap} symbol="plus-squares" />
 
-							<span className="pl-2">{'Components'}</span>
-						</Link>
+						<span className="pl-2">{'Components'}</span>
+					</Link>
 
-						<a
-							className="align-items-center align-middle d-inline-flex list-inline-item mb-3 px-3 py-2 rounded shadow text-decoration-none"
-							href="https://github.com/liferay/clay"
-						>
-							<div className="githubImage" />
+					<a
+						className="align-items-center align-middle d-inline-flex list-inline-item mb-3 px-3 py-2 rounded shadow text-decoration-none"
+						href="https://github.com/liferay/clay"
+					>
+						<div className="githubImage" />
 
-							<span className="pl-2">{'Clay on GitHub'}</span>
-						</a>
-					</nav>
-				</div>
+						<span className="pl-2">{'Clay on GitHub'}</span>
+					</a>
+				</nav>
 			</div>
 		</div>
 	);
