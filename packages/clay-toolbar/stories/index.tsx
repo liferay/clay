10503b16eb8bae0cd846f8d3305b90602b4c6ef5
Krešimir Čoko <kresimir.coko@liferay.com>
2020-06-05 11:54:34 +0200

    feat(@clayui/toolbar): Make stories match examples from markup

--- packages/clay-toolbar/stories/index.tsx
+++ packages/clay-toolbar/stories/index.tsx
@@ -4,87 +4,216 @@
  */
 
 import '@clayui/css/lib/css/atlas.css';
+import ClayButton from '@clayui/button';
+const spritemap = require('@clayui/css/lib/images/icons/icons.svg');
+import ClayLayout from '@clayui/layout';
 import {storiesOf} from '@storybook/react';
 import React from 'react';
 
 import ClayToolbar from '../src';
 
 storiesOf('Components|ClayToolbar', module)
-	.add('default', () => (
+	.add('Default', () => (
 		<ClayToolbar>
 			<ClayToolbar.Nav>
-				<ClayToolbar.Item expand>{'Item #1'}</ClayToolbar.Item>
-				<ClayToolbar.Item expand>{'Item #2'}</ClayToolbar.Item>
-				<ClayToolbar.Item expand>
-					<ClayToolbar.Label>{'Label #1'}</ClayToolbar.Label>
+				<ClayToolbar.Item>
+					<ClayToolbar.Action
+						disabled
+						href="#"
+						spritemap={spritemap}
+						symbol="angle-left"
+					/>
 				</ClayToolbar.Item>
+
 				<ClayToolbar.Item expand>
-					<ClayToolbar.Link>{'Link #1'}</ClayToolbar.Link>
+					<ClayToolbar.Section>
+						<span className="text-truncate-inline">
+							<span className="text-truncate">
+								{
+									'Item 1 of 3,138,732,873,467,182,321,341,231,234,342,559,827,334,424'
+								}
+							</span>
+						</span>
+					</ClayToolbar.Section>
 				</ClayToolbar.Item>
-				<ClayToolbar.Item expand>
-					<ClayToolbar.Section>{'Section #1'}</ClayToolbar.Section>
-					<ClayToolbar.Section>{'Section #2'}</ClayToolbar.Section>
-				</ClayToolbar.Item>
-			</ClayToolbar.Nav>
-		</ClayToolbar>
-	))
-	.add('subnav', () => (
-		<ClayToolbar subnav={{disabled: true, displayType: 'primary'}}>
-			<ClayToolbar.Nav>
-				<ClayToolbar.Item expand>{'Item #1'}</ClayToolbar.Item>
-				<ClayToolbar.Item expand>{'Item #2'}</ClayToolbar.Item>
-				<ClayToolbar.Item expand>
-					<ClayToolbar.Label>{'Label #1'}</ClayToolbar.Label>
-				</ClayToolbar.Item>
-				<ClayToolbar.Item expand>
-					<ClayToolbar.Link displayType="primary" href="#">
-						{'Link #1'}
+
+				<ClayToolbar.Item>
+					<ClayToolbar.Link href="#">
+						{'Component Link'}
 					</ClayToolbar.Link>
 				</ClayToolbar.Item>
-				<ClayToolbar.Item expand>
-					<ClayToolbar.Section>{'Section #1'}</ClayToolbar.Section>
-					<ClayToolbar.Section>{'Section #2'}</ClayToolbar.Section>
+
+				<ClayToolbar.Item>
+					<ClayToolbar.Action
+						disabled
+						href="#"
+						spritemap={spritemap}
+						symbol="angle-right"
+					/>
+				</ClayToolbar.Item>
+
+				<ClayToolbar.Item>
+					<ClayToolbar.Action
+						disabled
+						href="#"
+						spritemap={spritemap}
+						symbol="times"
+					/>
 				</ClayToolbar.Item>
 			</ClayToolbar.Nav>
 		</ClayToolbar>
 	))
-	.add('component toolbar', () => (
+	.add('Component Toolbar', () => (
 		<ClayToolbar component>
-			<ClayToolbar.Nav>
-				<ClayToolbar.Item expand>{'Item #1'}</ClayToolbar.Item>
-				<ClayToolbar.Item expand>{'Item #2'}</ClayToolbar.Item>
-				<ClayToolbar.Item expand>
-					<ClayToolbar.Label>{'Label #1'}</ClayToolbar.Label>
-				</ClayToolbar.Item>
-				<ClayToolbar.Item expand>
-					<ClayToolbar.Link displayType="primary" href="#">
-						{'Link #1'}
-					</ClayToolbar.Link>
-				</ClayToolbar.Item>
-				<ClayToolbar.Item expand>
-					<ClayToolbar.Section>{'Section #1'}</ClayToolbar.Section>
-					<ClayToolbar.Section>{'Section #2'}</ClayToolbar.Section>
-				</ClayToolbar.Item>
-			</ClayToolbar.Nav>
+			<ClayLayout.ContainerFluid>
+				<ClayToolbar.Nav>
+					<ClayToolbar.Item>
+						<ClayToolbar.Action
+							disabled
+							href="#"
+							spritemap={spritemap}
+							symbol="angle-left"
+						/>
+					</ClayToolbar.Item>
+
+					<ClayToolbar.Item expand>
+						{
+							'Item 1 of 3,138,732,873,467,182,321,341,231,234,342,559,827,334,424'
+						}
+					</ClayToolbar.Item>
+
+					<ClayToolbar.Item>
+						<ClayToolbar.Action
+							disabled
+							href="#"
+							spritemap={spritemap}
+							symbol="angle-right"
+						/>
+					</ClayToolbar.Item>
+
+					<ClayToolbar.Item>
+						<ClayToolbar.Action
+							disabled
+							href="#"
+							spritemap={spritemap}
+							symbol="times"
+						/>
+					</ClayToolbar.Item>
+				</ClayToolbar.Nav>
+			</ClayLayout.ContainerFluid>
 		</ClayToolbar>
 	))
-	.add('inlineBreakpoint', () => (
-		<ClayToolbar inlineBreakpoint="xl">
-			<ClayToolbar.Nav>
-				<ClayToolbar.Item expand>{'Item #1'}</ClayToolbar.Item>
-				<ClayToolbar.Item expand>{'Item #2'}</ClayToolbar.Item>
-				<ClayToolbar.Item expand>
-					<ClayToolbar.Label>{'Label #1'}</ClayToolbar.Label>
-				</ClayToolbar.Item>
-				<ClayToolbar.Item expand>
-					<ClayToolbar.Link displayType="primary" href="#">
-						{'Link #1'}
-					</ClayToolbar.Link>
-				</ClayToolbar.Item>
-				<ClayToolbar.Item expand>
-					<ClayToolbar.Section>{'Section #1'}</ClayToolbar.Section>
-					<ClayToolbar.Section>{'Section #2'}</ClayToolbar.Section>
-				</ClayToolbar.Item>
-			</ClayToolbar.Nav>
+	.add('Subnav', () => (
+		<ClayToolbar subnav>
+			<ClayLayout.ContainerFluid>
+				<ClayToolbar.Nav>
+					<ClayToolbar.Item expand>
+						<ClayToolbar.Section>
+							<span className="text-truncate-inline">
+								<span className="text-truncate">
+									{'25,392 results for '}
+
+									<strong>
+										{
+											'ReallySuperInsanelyJustIncrediblyLongAndTotallyNotPossibleWordButWeAreReallyTryingToCoverAllOurBasesHereJustInCaseSomeoneIsNutsAsPerUsual'
+										}
+									</strong>
+								</span>
+							</span>
+						</ClayToolbar.Section>
+					</ClayToolbar.Item>
+					<ClayToolbar.Item>
+						<ClayToolbar.Link componentLink href="#">
+							{'Clear'}
+						</ClayToolbar.Link>
+					</ClayToolbar.Item>
+					<ClayToolbar.Item>
+						<ClayButton
+							className="component-link tbar-link"
+							displayType="unstyled"
+						>
+							{'Clear'}
+						</ClayButton>
+					</ClayToolbar.Item>
+				</ClayToolbar.Nav>
+			</ClayLayout.ContainerFluid>
+		</ClayToolbar>
+	))
+	.add('Subnav Primary Disabled', () => (
+		<ClayToolbar
+			inlineBreakpoint="md"
+			subnav={{disabled: true, displayType: 'primary'}}
+		>
+			<ClayLayout.ContainerFluid>
+				<ClayToolbar.Nav wrap>
+					<ClayToolbar.Item>
+						<ClayToolbar.Section>
+							<span className="component-text">
+								{'25,392 results for '}
+
+								<strong>
+									{
+										'"ReallySuperInsanelyJustIncrediblyLongAndTotallyNotPossibleWordButWeAreReallyTryingToCoverAllOurBasesHereJustInCaseSomeoneIsNutsAsPerUsual"'
+									}
+								</strong>
+							</span>
+						</ClayToolbar.Section>
+					</ClayToolbar.Item>
+
+					<ClayToolbar.Item>
+						<ClayToolbar.Section>
+							<ClayToolbar.Label
+								closeButtonProps={{disabled: true}}
+								componentLabel
+								dismissible
+								displayType="unstyled"
+								spritemap={spritemap}
+							>
+								{'Category: Productivity'}
+							</ClayToolbar.Label>
+						</ClayToolbar.Section>
+					</ClayToolbar.Item>
+
+					<ClayToolbar.Item>
+						<ClayToolbar.Section>
+							<ClayToolbar.Label
+								closeButtonProps={{disabled: true}}
+								componentLabel
+								dismissible
+								displayType="unstyled"
+								spritemap={spritemap}
+							>
+								{'Status: Approved'}
+							</ClayToolbar.Label>
+						</ClayToolbar.Section>
+					</ClayToolbar.Item>
+
+					<ClayToolbar.Item expand>
+						<ClayToolbar.Section>
+							<ClayToolbar.Label
+								closeButtonProps={{disabled: true}}
+								componentLabel
+								dismissible
+								displayType="unstyled"
+								spritemap={spritemap}
+							>
+								{'Type: png'}
+							</ClayToolbar.Label>
+						</ClayToolbar.Section>
+					</ClayToolbar.Item>
+
+					<ClayToolbar.Item>
+						<ClayToolbar.Section>
+							<ClayButton
+								className="component-link tbar-link"
+								disabled
+								displayType="unstyled"
+							>
+								{'Clear All'}
+							</ClayButton>
+						</ClayToolbar.Section>
+					</ClayToolbar.Item>
+				</ClayToolbar.Nav>
+			</ClayLayout.ContainerFluid>
 		</ClayToolbar>
 	));
