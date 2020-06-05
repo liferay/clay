cbbe4350c180780296efac4347b2dc25924e6826
Krešimir Čoko <kresimir.coko@liferay.com>
2020-06-05 11:54:18 +0200

    feat(@clayui/toolbar): Add new Action low level component

---
+++ packages/clay-toolbar/src/Action.tsx
@@ -0,0 +1,41 @@
+/**
+ * SPDX-FileCopyrightText: © 2020 Liferay, Inc. <https://liferay.com>
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+import ClayIcon from '@clayui/icon';
+import classNames from 'classnames';
+import React from 'react';
+
+export interface IProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
+	/**
+	 * Flag that determines if the Action will have a `disabled` class, disabling interactions.
+	 */
+	disabled?: boolean;
+
+	/**
+	 * Path to clay icon spritemap
+	 */
+	spritemap: string;
+	symbol: string;
+}
+
+const Action: React.FunctionComponent<IProps> = ({
+	className,
+	disabled,
+	spritemap,
+	symbol,
+	...otherProps
+}: IProps) => (
+	<a
+		className={classNames(className, 'component-action', {disabled})}
+		role="button"
+		{...otherProps}
+	>
+		<ClayIcon spritemap={spritemap} symbol={symbol} />
+	</a>
+);
+
+Action.displayName = 'ClayToolbarAction';
+
+export default Action;
