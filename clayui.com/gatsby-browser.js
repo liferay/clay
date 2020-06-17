4cab03c0af2dcec46c8894d6e29c117d71f59356
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2020-06-17 11:51:54 -0700

    feat(clayui.com): make it easier to work with clay-css on clayui.com

--- clayui.com/gatsby-browser.js
+++ clayui.com/gatsby-browser.js
@@ -3,8 +3,28 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-require('./src/styles/main.scss');
-
 // A stub function is needed because gatsby won't load this file otherwise
 // (https://github.com/gatsbyjs/gatsby/issues/6759)
-exports.onClientEntry = () => {};
+export const onClientEntry = () => {
+	if (process.env.GATSBY_CLAY_NIGHTLY === 'true') {
+		const isNullOrTrue = (val) => val === 'true' || val === null;
+
+		const showAtlas = isNullOrTrue(localStorage.getItem('clay.showAtlas'));
+		const showSiteCss = isNullOrTrue(
+			localStorage.getItem('clay.showSiteCss')
+		);
+
+		if (showAtlas) {
+			require('@clayui/css/src/scss/atlas.scss');
+		} else {
+			require('@clayui/css/src/scss/base.scss');
+		}
+
+		if (showSiteCss) {
+			require('./src/styles/main.scss');
+		}
+	} else {
+		require('@clayui/css/src/scss/atlas.scss');
+		require('./src/styles/main.scss');
+	}
+};
