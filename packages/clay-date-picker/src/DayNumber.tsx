e098b1fbe61e11066ce8d3dcf2b2a1ee263d30f9
Diego Nascimento <diego.nascimento@liferay.com>
2020-07-01 18:34:00 -0300

    fix(@clayui/date-picker): When selecting a date using space key, removes properly the `:active` state of the element
    This issue can't be reproduced on IE/FF but it can be reproduced on Chrome/Safari. Apparently this is a WebKit issue

--- packages/clay-date-picker/src/DayNumber.tsx
+++ packages/clay-date-picker/src/DayNumber.tsx
@@ -13,13 +13,21 @@ interface IProps {
 	daySelected: Date;
 	disabled?: boolean;
 	onClick: (date: Date) => void;
+	onKeyUp?: (event: React.KeyboardEvent) => void;
+	onKeyDown?: (event: React.KeyboardEvent) => void;
 }
+
+const noop = () => {};
+
+const KEY_CODE_SPACE = 32;
 
 const ClayDatePickerDayNumber: React.FunctionComponent<IProps> = ({
 	day,
 	daySelected,
 	disabled,
-	onClick,
+	onClick = noop,
+	onKeyDown = noop,
+	onKeyUp = noop,
 }) => {
 	const classNames = classnames(
 		'date-picker-date date-picker-calendar-item',
@@ -43,6 +51,23 @@ const ClayDatePickerDayNumber: React.FunctionComponent<IProps> = ({
 			className={classNames}
 			disabled={day.outside}
 			onClick={() => onClick(day.date)}
+			onKeyDown={(event) => {
+				// When tabbing and selecting a DayNumber using
+				// SPACE key the active state it's not being removed.
+				// See https://github.com/liferay/clay/issues/3374 for more details.
+				if (event.keyCode === KEY_CODE_SPACE) {
+					event.preventDefault();
+				}
+
+				onKeyDown(event);
+			}}
+			onKeyUp={(event) => {
+				if (event.keyCode === KEY_CODE_SPACE) {
+					onClick(day.date);
+				}
+
+				onKeyUp(event);
+			}}
 			type="button"
 		>
 			{day.date.getDate()}
