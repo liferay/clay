85df05fd31b534b871346da2a36d99f1eb70147e
Patrick Yeo <patrick.yeo@liferay.com>
2020-07-23 11:44:58 -0700

    docs(clayui.com): CSS Sliders add JS to make it work and make JS requirement more visible
    docs(clayui.com): CSS Sliders group each example with code so it's easier to match code to example

--- clayui.com/static/js/docs-site.js
+++ clayui.com/static/js/docs-site.js
@@ -27,3 +27,63 @@ if (!Element.prototype.closest) {
 		}
 	});
 })();
+
+(function() {
+	function calcProgressWidth(el) {
+		var clayRange = el.closest('[data-toggle="clay-css-range"]');
+
+		var min = el.getAttribute('min') || 0;
+		var max = el.getAttribute('max') || 100;
+		var step = el.getAttribute('step') || 1;
+
+		var thumbWidth = parseInt(
+			clayRange.querySelector(
+				'.clay-range-progress .clay-range-thumb'
+			).offsetWidth
+		);
+		var rangeWidth = parseInt(el.offsetWidth);
+
+		var currentStep = (el.value - min) / step;
+		var totalSteps = (max - min) / step;
+		var progressWidth = (currentStep / totalSteps) * 100;
+
+		var offsetWidth = progressWidth;
+		var ratio =
+			(((1 - progressWidth * 0.01) * (thumbWidth / 1.001)) /
+				rangeWidth) *
+			100;
+
+		if (progressWidth !== 50) {
+			offsetWidth =
+				progressWidth - (thumbWidth / 2 / rangeWidth) * 100 + ratio;
+		}
+
+		return offsetWidth;
+	}
+
+	function clayRange(e) {
+		var clayRangeInput = e.target.closest('[data-toggle="clay-css-range"]');
+
+		if (clayRangeInput) {
+			offsetWidth = calcProgressWidth(e.target);
+
+			clayRangeInput.querySelector(
+				'.clay-range-progress'
+			).style.width = offsetWidth + '%';
+
+			var currentVal = e.target.value;
+
+			var title = clayRangeInput.querySelectorAll(
+				'.clay-range-value'
+			);
+
+			for (var i = 0; i < title.length; i++) {
+				title[i].innerHTML = currentVal;
+			}
+		}
+	}
+
+	document.addEventListener('input', clayRange);
+
+	document.addEventListener('change', clayRange);
+})();
