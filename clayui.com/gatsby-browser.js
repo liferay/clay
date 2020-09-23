e27599560cf0e323db6801255f936cfa43dab73e
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2020-09-23 13:53:07 -0700

    chore: run format

--- clayui.com/gatsby-browser.js
+++ clayui.com/gatsby-browser.js
@@ -1,5 +1,5 @@
 /**
- * SPDX-FileCopyrightText: © 2018 Liferay, Inc. <https://liferay.com>
+ * SPDX-FileCopyrightText: © 2020 Liferay, Inc. <https://liferay.com>
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
