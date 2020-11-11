57b5a8fed4c7bc383465961e2c4c2fe168501a39
Marko Čikoš <marko.cikos@liferay.com>
2020-11-11 15:47:28 +0100

    fix(@clayui/card): Use ClayButtonWithIcon instead of vanilla button in clay-card

--- packages/clay-card/src/CardWithInfo.tsx
+++ packages/clay-card/src/CardWithInfo.tsx
@@ -3,6 +3,7 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
+import {ClayButtonWithIcon} from '@clayui/button';
 import {ClayDropDownWithItems} from '@clayui/drop-down';
 import {ClayCheckbox} from '@clayui/form';
 import ClayIcon from '@clayui/icon';
@@ -212,16 +213,13 @@ export const ClayCardWithInfo: React.FunctionComponent<IProps> = ({
 								items={actions}
 								spritemap={spritemap}
 								trigger={
-									<button
+									<ClayButtonWithIcon
 										{...dropDownTriggerProps}
 										className="component-action"
 										disabled={disabled}
-									>
-										<ClayIcon
-											spritemap={spritemap}
-											symbol="ellipsis-v"
-										/>
-									</button>
+										spritemap={spritemap}
+										symbol="ellipsis-v"
+									/>
 								}
 							/>
 						</ClayLayout.ContentCol>
