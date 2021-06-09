7e64c6800cc087c594a6cf1118a39c193a2cab90
Keven Leone <keven.leone@liferay.com>
2021-06-09 14:28:14 -0300

    fix(@clayui/nav): Adjust condition, to avoid set Boolean values in className property

--- packages/clay-nav/src/Vertical.tsx
+++ packages/clay-nav/src/Vertical.tsx
@@ -132,7 +132,7 @@ function Item({
 
 			{subItems && (
 				<CSSTransition
-					className={!expanded && 'collapse'}
+					className={expanded ? '' : 'collapse'}
 					classNames={{
 						enter: 'collapsing',
 						enterActive: `show`,
