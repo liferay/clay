f4c5f4849be9fde84d745e5f88df3b222fb1cc0c
Patrick Yeo <patrick.yeo@liferay.com>
2021-09-16 14:28:03 -0700

    chore(clayui.com): Properly serve HTML files from the static directory

--- clayui.com/gatsby-node.js
+++ clayui.com/gatsby-node.js
@@ -3,6 +3,12 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
+const express = require('express');
+
 exports.createPages = require('./gatsby/createPages');
 exports.onCreateNode = require('./gatsby/onCreateNode');
 exports.onCreateWebpackConfig = require('./gatsby/onCreateWebpackConfig');
+
+exports.onCreateDevServer = ({app}) => {
+	app.use(express.static('static'));
+};
