2f37207645bca34ef5d6737f34aad35b176a110d
Krešimir <kresimir.coko@liferay.com>
2021-10-11 12:44:00 +0200

    chore: disable rules introduced (in eslint-plugin) after eslint-config@21.1.0

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,13 @@ module.exports = {
 	parser: '@typescript-eslint/parser',
 	plugins: ['@liferay', '@typescript-eslint'],
 	rules: {
+		'@liferay/empty-line-between-elements': 'off',
+		'@liferay/imports-first': 'off',
+		'@liferay/no-abbreviations': 'off',
+		'@liferay/no-dynamic-require': 'off',
+		'@liferay/no-length-jsx-expression': 'off',
+		'@liferay/sort-imports': 'off',
+		'@liferay/group-imports': 'off',
 		'@typescript-eslint/array-type': ['error', {default: 'generic'}],
 		'@typescript-eslint/naming-convention': [
 			'error',
@@ -25,6 +32,7 @@ module.exports = {
 			},
 		],
 		'@typescript-eslint/no-unused-vars': 'error',
+		'@typescript-eslint/no-use-before-define': 'off',
 		'lines-around-comment': 'off',
 		'no-for-of-loops/no-for-of-loops': 'off',
 		'no-unused-vars': 'off',
@@ -37,14 +45,18 @@ module.exports = {
 		'prefer-template': 'error',
 		'react/display-name': 'error',
 		'react/jsx-boolean-value': 'error',
+		'react/jsx-curly-brace-presence': 'off',
 		'react/jsx-no-bind': [
 			'error',
 			{
 				allowArrowFunctions: true,
 			},
 		],
-		'react/jsx-no-literals': 'error',
+		'react/jsx-no-literals': 'off',
+		'react/no-unescaped-entities': 'off',
 		'react-hooks/exhaustive-deps': 'off',
+		'sort-keys': 'off',
 		'sort-vars': 'error',
+		'spaced-comment': 'off',
 	},
 };
