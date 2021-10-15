c9e201b1914379a89f7305c680cecd6dddc88396
Matuzalém Teles <matuzalemteles@gmail.com>
2021-10-15 19:18:09 -0300

    chore: refactors the `buildIconsSvg` method to use a writable stream
    It refactors the implementation of creating the icons.svg file using a writable stream instead of keeping each file in memory and all data too, thus keeping fewer data in memory.
    
    It also closes the process with exit(1) to terminate the process saying that the build failed so that the CI can fail.

--- packages/clay-css/scripts/build.js
+++ packages/clay-css/scripts/build.js
@@ -1,6 +1,12 @@
+/**
+ * SPDX-FileCopyrightText: © 2021 Liferay, Inc. <https://liferay.com>
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+'use strict';
+
 const fs = require('fs');
 const path = require('path');
-
 const sass = require('sass');
 
 const OUTPUT_DIRECTORY = path.resolve('./lib');
@@ -18,10 +24,12 @@ function copyRecursiveSync(sourcePath, destinationPath) {
 	const exists = fs.existsSync(sourcePath);
 	const stats = exists && fs.statSync(sourcePath);
 	const isDirectory = exists && stats.isDirectory();
+
 	if (isDirectory) {
 		if (!fs.existsSync(destinationPath) || !fs.statSync(destinationPath).isDirectory()) {
 			fs.mkdirSync(destinationPath, {recursive: true});
 		}
+
 		fs.readdirSync(sourcePath).forEach((childItemName) => {
 			copyRecursiveSync(
 				path.join(sourcePath, childItemName),
@@ -49,21 +57,63 @@ function ensureDirectory(directoryPath) {
 					reject(error);
 				}
 			}
+
 			resolve(directoryPath);
 		});
 	});
 }
 
-function readFile(url) {
-	return new Promise((resolve, reject) => {
-		let buffer = Buffer.from('');
-		fs.createReadStream(url)
-			.on('data', (chunk) => {
-				buffer = Buffer.concat([buffer, chunk]);
-			})
-			.on('close', () => resolve(buffer))
-			.on('error', (error) => reject(error));
-	});
+/**
+ * This method will look at all svg icons and create a new svg file containing
+ * a graphical model object and so that it can be instantiated by a `<use>`
+ * element.
+ */
+async function buildIconsSvg() {
+	const files = await fs.promises.readdir(ICONS_DIRECTORY);
+
+	await ensureDirectory(path.dirname(ICONS_OUTPUT_FILEPATH));
+
+	const fileWritable = fs.createWriteStream(ICONS_OUTPUT_FILEPATH);
+
+	// Writes the file header in the stream.
+	fileWritable.write(
+		'<?xml version="1.0" encoding="UTF-8"?>'
+		+ '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">'
+		+ '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">'
+	);
+
+	await Promise.all(files.map(async (file) => {
+		if (path.extname(file) !== '.svg') {
+			return;
+		}
+
+		const fileName = path.basename(file, '.svg');
+
+		const data = await fs.promises.readFile(`${ICONS_DIRECTORY}${path.sep}${file}`, 'utf8');
+
+		fileWritable.write(
+			data
+				.replace(HEADER_REGEXP, '')
+				.replace(/<svg/gm, '<symbol')
+				.replace(
+					/xmlns="http:\/\/www\.w3\.org\/2000\/svg"/gm,
+					`id="${fileName}"`
+				)
+				.replace(/<\/svg/gm, '</symbol')
+				.replace(/\n/gm, '')
+				.replace(/\t/gm, '')
+		);
+	}));
+
+	// Close the writable stream by adding the chunk at the end and wait for
+	// the stream to terminate with error or success.
+	await new Promise((resolve, reject) => fileWritable.end('</svg>', (error) => {
+		if (error) {
+			reject(error);
+		} else {
+			resolve();
+		}
+	}));
 }
 
 async function build() {
@@ -108,72 +158,11 @@ async function build() {
 	await buildIconsSvg();
 }
 
-function transformSvgFiles(files) {
-	return new Promise(async (resolve) => {
-		const outputFileContents = [
-			'<?xml version="1.0" encoding="UTF-8"?>',
-			'<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">',
-			'<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">',
-		];
-
-		for (let i = 0; i < files.length; i++) {
-			const file = files[i];
-
-			if (path.extname(file) !== '.svg') {
-				continue;
-			}
-
-			const fileName = path.basename(file, '.svg');
-
-			const data = await readFile(`${ICONS_DIRECTORY}${path.sep}${file}`);
-
-			const fileOutput = data
-				.toString()
-				.replace(HEADER_REGEXP, '')
-				.replace(/<svg/gm, '<symbol')
-				.replace(
-					/xmlns="http:\/\/www\.w3\.org\/2000\/svg"/gm,
-					`id="${fileName}"`
-				)
-				.replace(/<\/svg/gm, '</symbol')
-				.replace(/\n/gm, '')
-				.replace(/\t/gm, '');
-
-			outputFileContents.push(fileOutput);
-		}
-
-		outputFileContents.push('</svg>');
-
-		resolve(outputFileContents.join('').replace(/\n/gm, ''));
-	});
-}
-
-function buildIconsSvg() {
-	return new Promise((resolve, reject) => {
-		fs.readdir(ICONS_DIRECTORY, async (error, files) => {
-			if (error) {
-				reject(error);
-			}
-
-			await ensureDirectory(path.dirname(ICONS_OUTPUT_FILEPATH));
-
-			const svgData = await transformSvgFiles(files);
-
-			try {
-				fs.writeFileSync(ICONS_OUTPUT_FILEPATH, svgData);
-			} catch (error) {
-				reject(error);
-			}
-
-			resolve(ICONS_OUTPUT_FILEPATH);
-		});
-	});
-}
-
 build()
 	.then(() => {
 		console.log('Build successful');
 	})
 	.catch((error) => {
 		console.log('Build error:\n', error);
+		process.exit(1);
 	});
