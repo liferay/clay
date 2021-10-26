9e40c0f75c41b9f6cd48e9b5e48f5abde75eda07
Matuzalém Teles <matuzalemteles@gmail.com>
2021-10-26 20:18:43 -0300

    fix(@clayui/shared): fixes error when considering an element with negative tabIndex

--- packages/clay-shared/src/useFocusManagement.ts
+++ packages/clay-shared/src/useFocusManagement.ts
@@ -41,6 +41,10 @@ export function isFocusable({
 
 	const minTabIndex = 0;
 
+	if (tabIndex != null && tabIndex < minTabIndex) {
+		return false;
+	}
+
 	if (
 		(tabIndex != null && tabIndex >= minTabIndex) ||
 		contentEditable === true ||
@@ -54,11 +58,7 @@ export function isFocusable({
 	}
 
 	if (tagName === 'input') {
-		return (
-			type !== 'file' &&
-			type !== 'hidden' &&
-			(tabIndex != null ? tabIndex >= minTabIndex : true)
-		);
+		return type !== 'file' && type !== 'hidden';
 	}
 
 	return (
