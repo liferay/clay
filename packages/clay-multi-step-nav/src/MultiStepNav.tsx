bb43ff8d538e96e9ac5d6b635ef937abc112657a
Matuzalém Teles <matuzalemteles@gmail.com>
2021-12-16 13:00:44 -0600

    feat(@clayui/multi-step-nav): add the new `indicatorLabel` API to indicate the position of the indicator label

--- packages/clay-multi-step-nav/src/MultiStepNav.tsx
+++ packages/clay-multi-step-nav/src/MultiStepNav.tsx
@@ -21,6 +21,11 @@ interface IProps extends React.HTMLAttributes<HTMLOListElement> {
 	 * Flag to indicate if nav should add `multi-step-item-fixed-width` class.
 	 */
 	fixedWidth?: boolean;
+
+	/**
+	 * Flag to indicate the position of the indicator label.
+	 */
+	indicatorLabel?: 'bottom' | 'top';
 }
 
 const ClayMultiStepNav: React.FunctionComponent<IProps> & {
@@ -33,18 +38,17 @@ const ClayMultiStepNav: React.FunctionComponent<IProps> & {
 	children,
 	className,
 	fixedWidth,
+	indicatorLabel = 'bottom',
 	...otherProps
 }: IProps) => {
 	return (
 		<ol
-			className={classNames(
-				'multi-step-nav multi-step-indicator-label-bottom',
-				className,
-				{
-					['multi-step-item-fixed-width']: fixedWidth,
-					['multi-step-nav-collapse-sm']: autoCollapse,
-				}
-			)}
+			className={classNames('multi-step-nav', className, {
+				'multi-step-item-fixed-width': fixedWidth,
+				'multi-step-nav-collapse-sm': autoCollapse,
+				[`multi-step-indicator-label-${indicatorLabel}`]:
+					indicatorLabel,
+			})}
 			{...otherProps}
 		>
 			{children}
