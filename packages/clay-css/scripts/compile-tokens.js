3d487129ea25ab4ef04b9698c7a7e7c1cfabadc1
Patrick Yeo <patrick.yeo@liferay.com>
2022-03-22 16:30:35 -0700

    feat(@clayui/css): Adds script to generate frontend-token-definition.json
        - scrapes `clay-css/lib/css/atlas.css` for all css custom properties and generate `frontend-token-definition.json`

---
+++ packages/clay-css/scripts/compile-tokens.js
@@ -0,0 +1,148 @@
+/**
+ * SPDX-FileCopyrightText: © 2021 Liferay, Inc. <https://liferay.com>
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+const tokenCategories = require('./token-categories');
+
+const atlascss = path.join('lib', 'css', 'atlas.css');
+
+const css = fs.readFileSync(atlascss).toString();
+
+const REGEX_CSSVAR_AND_VALUE = /\s\s--(.+?)(?=;\n)/g;
+const REGEX_CSSVAR_AND_VALUE_NAME = /--(.+?)(?=:)/g;
+const REGEX_CSSVAR_AND_VALUE_VALUE = /\s(?!-)(?!\s)(.+?)$/gm;
+
+const REGEX_CSSVAR_WITH_FALLBACK = /var\(--(.+?),\s*var\(--(.+?),\s*var\(--(.+?),\s*var\(--(.+?),\s*var\(--(.+?),\s*var\(--(\)|(.+?)\))(\)|(.+?)\))(\)|(.+?)\))(\)|(.+?)\))(\)|(.+?)\))(\)|(.+?)\))|var\(--(.+?),\s*var\(--(.+?),\s*var\(--(.+?),\s*var\(--(.+?),\s*var\(--(\)|(.+?)\))(\)|(.+?)\))(\)|(.+?)\))(\)|(.+?)\))(\)|(.+?)\))|var\(--(.+?),\s*var\(--(.+?),\s*var\(--(.+?),\s*var\(--(\)|(.+?)\))(\)|(.+?)\))(\)|(.+?)\))(\)|(.+?)\))|var\(--(.+?),\s*var\(--(.+?),\s*var\(--(\)|(.+?)\))(\)|(.+?)\))(\)|(.+?)\))|var\(--(.+?),\s*var\(--(\)|(.+?)\))(\)|(.+?)\))|var\(--(.+?)calc(.+?)(?=;)|var\(--(.+?)\)/gm;
+const REGEX_CSSVAR_WITH_FALLBACK_NAME = /^var\(--(.+?)((?=,)|(?=\)))/gm;
+const REGEX_CSSVAR_WITH_FALLBACK_VALUE = /,(.+?)$/gm;
+
+const cssvarAndValue = css.match(REGEX_CSSVAR_AND_VALUE);
+const cssvarWithFallback = css.match(REGEX_CSSVAR_WITH_FALLBACK);
+
+const propArray = [];
+const valArray = [];
+
+// Gets variables from :root{}
+
+for (let i = 0; i < cssvarAndValue.length; i++) {
+	const property = cssvarAndValue[i].match(REGEX_CSSVAR_AND_VALUE_NAME)[0];
+	const value = cssvarAndValue[i].match(REGEX_CSSVAR_AND_VALUE_VALUE)[0].trim();
+
+	propArray.push(property);
+	valArray.push(value);
+}
+
+// Gets variables from the component selectors
+
+for (let i = 0; i < cssvarWithFallback.length; i++) {
+	// Removes `var(` from property name
+
+	const property = [cssvarWithFallback[i].match(REGEX_CSSVAR_WITH_FALLBACK_NAME)[0].replace('var(', '')];
+
+	let value = '';
+
+	propArray.push(property[0]);
+
+	// There is a fallback value
+
+	if (cssvarWithFallback[i].match(REGEX_CSSVAR_WITH_FALLBACK_VALUE)) {
+		// Removes `, ` from fallback value
+
+		const stripCommaSpace = cssvarWithFallback[i].match(REGEX_CSSVAR_WITH_FALLBACK_VALUE)[0].replace(/^,\s/gm, '')
+
+		// one:   ^(.+?)(\(|(.+?)\()(\)|(.+?)\))
+		// two:   ^(.+?)(\(|(.+?)\()(\(|(.+?)\()(\)|(.+?)\))(\)|(.+?)\))
+		// three: ^(.+?)(\(|(.+?)\()(\(|(.+?)\()(\(|(.+?)\()(\)|(.+?)\))(\)|(.+?)\))(\)|(.+?)\))
+		// four: ^(.+?)(\(|(.+?)\()(\(|(.+?)\()(\(|(.+?)\()(\(|(.+?)\()(\)|(.+?)\))(\)|(.+?)\))(\)|(.+?)\))(\)|(.+?)\))
+		value = stripCommaSpace.match(/^(.+?)(\(|(.+?)\()(\(|(.+?)\()(\(|(.+?)\()(\(|(.+?)\()(\)|(.+?)\))(\)|(.+?)\))(\)|(.+?)\))(\)|(.+?)\))|^(.+?)(\(|(.+?)\()(\(|(.+?)\()(\(|(.+?)\()(\)|(.+?)\))(\)|(.+?)\))(\)|(.+?)\))|^(.+?)(\(|(.+?)\()(\(|(.+?)\()(\)|(.+?)\))(\)|(.+?)\))|^(.+?)(\(|(.+?)\()(\)|(.+?)\))|(.+\b)/gm);
+
+		// The fallback value is invalid e.g., `var(--the-variable, );` or ""
+
+		if (value === null) {
+			value = '';
+		}
+	}
+
+	valArray.push(value[0]);
+}
+
+let uniqueValArray = [];
+let uniquePropArray = propArray.filter(function(item, pos, self) {
+	if (self.indexOf(item) === pos) {
+		uniqueValArray.push(valArray[pos]);
+
+		return true;
+	}
+
+	return false;
+});
+
+
+let frontendToken = {
+	frontendTokenCategories: [],
+};
+
+let tokenCategoriesIndex = 0;
+
+for (const cat in tokenCategories) {
+	let feTokenCategoriesObj = {
+		label: '',
+		name: '',
+		frontendTokenSets: [],
+	};
+
+	feTokenCategoriesObj.label = cat;
+	feTokenCategoriesObj.name = cat;
+
+	for (let i = 0; i < uniquePropArray.length; i++) {
+		let feTokenSetsObj = {
+			frontendTokens: [
+				{
+					defaultValue: '',
+					editorType: '',
+					label: '',
+					mappings: [
+						{
+							type: "cssVariable",
+							value: '',
+						},
+					],
+					name: '',
+					type: 'String',
+				}
+			],
+		};
+
+		if (uniquePropArray[i].match(tokenCategories[cat].regex)) {
+			let property = uniquePropArray[i];
+			let value = uniqueValArray[i];
+
+			let frontendTokens = feTokenSetsObj.frontendTokens[0];
+
+			if (property.match(/--(?!(.+?)rgb)(primary|secondary|success|info|warning|danger|dark|light|white|gray|black|blue|indigo|purple|pink|red|orange|yellow|green|teal|cyan|(.+?)-color$)/gm)) {
+				// frontendTokens.editorType = 'ColorPicker';
+			}
+
+			const label = property.replace('--', '');
+
+			frontendTokens.name = label;
+			frontendTokens.defaultValue = value;
+			frontendTokens.label = label;
+			frontendTokens.mappings[0].value = label;
+
+			feTokenCategoriesObj.frontendTokenSets.push(feTokenSetsObj);
+		}
+	}
+
+	frontendToken.frontendTokenCategories.push(feTokenCategoriesObj);
+
+	tokenCategoriesIndex++;
+}
+
+fs.writeFileSync(path.join('frontend-token-definition.json'), JSON.stringify(frontendToken, null, 2));
---
+++ packages/clay-css/scripts/token-categories.js
@@ -0,0 +1,101 @@
+module.exports = {
+	'grays': {
+		regex: /--((white)|(black)|(gray))/gm,
+	},
+	'colors': {
+		regex: /--((blue)|(indigo)|(purple)|(pink)|(red)|(orange)|(yellow)|(green)|(teal)|(cyan))/gm,
+	},
+	'globals': {
+		regex: /--((border-radius)|(rounded)|(component)|(font-family)|(font-weight))/gm,
+	},
+	'layout': {
+		regex: /--((spacer)|(border-radius)|(rounded)|(breakpoint))/gm,
+	},
+	'primary-colors': {
+		regex: /--primary/gm,
+	},
+	'secondary-colors': {
+		regex: /--secondary/gm,
+	},
+	'success-colors': {
+		regex: /--success/gm,
+	},
+	'info-colors': {
+		regex: /--info/gm,
+	},
+	'warning-colors': {
+		regex: /--warning/gm,
+	},
+	'danger-colors': {
+		regex: /--danger/gm,
+	},
+	'light-colors': {
+		regex: /--light/gm,
+	},
+	'dark-colors': {
+		regex: /--dark/gm,
+	},
+	'link': {
+		regex: /--link/gm,
+	},
+	'btn': {
+		regex: /--(?!btn-primary|btn-secondary|btn-success|btn-info|btn-warning|btn-danger|btn-light|btn-dark|btn-link|btn-outline|btn-sm|btn-lg|btn-monospaced)btn/gm,
+	},
+	'btn-lg': {
+		regex: /--btn-lg/gm,
+	},
+	'btn-sm': {
+		regex: /--btn-sm/gm,
+	},
+	'btn-monospaced': {
+		regex: /--(?!btn-monospaced-lg|btn-monospaced-sm)btn-monospaced/gm,
+	},
+	'btn-monospaced-lg': {
+		regex: /--btn-monospaced-lg/gm,
+	},
+	'btn-monospaced-sm': {
+		regex: /--btn-monospaced-sm/gm,
+	},
+	'btn-primary': {
+		regex: /--btn-primary/gm,
+	},
+	'btn-secondary': {
+		regex: /--btn-secondary/gm,
+	},
+	'btn-success': {
+		regex: /--btn-success/gm,
+	},
+	'btn-info': {
+		regex: /--btn-info/gm,
+	},
+	'btn-warning': {
+		regex: /--btn-warning/gm,
+	},
+	'btn-danger': {
+		regex: /--btn-danger/gm,
+	},
+	'btn-light': {
+		regex: /--btn-light/gm,
+	},
+	'btn-dark': {
+		regex: /--btn-dark/gm,
+	},
+	'btn-link': {
+		regex: /--btn-link/gm,
+	},
+	'alert': {
+		regex: /--alert/gm,
+	},
+	'form-control': {
+		regex:/--input/gm,
+	},
+	'table': {
+		regex: /--(?!table-list|table-cell)table/gm,
+	},
+	'table-cell': {
+		regex: /--(?!table-list)table-cell/gm,
+	},
+	'table-list': {
+		regex: /--table-list/gm,
+	},
+};
