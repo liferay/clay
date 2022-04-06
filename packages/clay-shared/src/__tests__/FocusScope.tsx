c81a6c16d49f7164648e0db589a37406a2b0e30a
Sergio Jimenez del Coso <sergio.jdelcoso@liferay.com>
2022-04-06 12:33:24 +0200

    feat(@clayui/clay-shared): Add more test cases to FocusScope

--- packages/clay-shared/src/__tests__/FocusScope.tsx
+++ packages/clay-shared/src/__tests__/FocusScope.tsx
@@ -3,22 +3,48 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-import {cleanup, render} from '@testing-library/react';
+import {cleanup, fireEvent, render} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
 
 import '@testing-library/jest-dom/extend-expect';
 import {ClayPortal, FocusScope} from '..';
 
+import ClayDropDown from '../../../clay-drop-down/src';
+
+const DropDownWithState: React.FunctionComponent<any> = ({
+	children,
+	...others
+}) => {
+	const [active, setActive] = React.useState(false);
+
+	return (
+		<ClayDropDown
+			{...others}
+			active={active}
+			className="dropdown-test"
+			onActiveChange={(val) => setActive(val)}
+			renderMenuOnClick
+			trigger={<button>Click Me</button>}
+		>
+			{children}
+		</ClayDropDown>
+	);
+};
+
 describe('FocusScope', () => {
 	afterEach(() => {
 		jest.clearAllTimers();
-
+		document.body.innerHTML = '';
 		cleanup();
 	});
 
-	it('manages focus order when using portals', () => {
-		// Create non-react based html nodes
+	afterAll(() => {
+		document.body.innerHTML = '';
+		cleanup();
+	});
+
+	beforeEach(() => {
 		if (document) {
 			const buttonNode = document.createElement('button');
 			buttonNode.id = 'button1';
@@ -41,8 +67,18 @@ describe('FocusScope', () => {
 					<ClayPortal>
 						<ul>
 							<li>
-								<a href="#" id="linkInPortal">
+								<a href="#" id="linkInPortal1">
 									link 1
+								</a>
+							</li>
+							<li>
+								<a href="#" id="linkInPortal2">
+									link 2
+								</a>
+							</li>
+							<li>
+								<a href="#" id="linkInPortal3">
+									link 3
 								</a>
 							</li>
 						</ul>
@@ -51,7 +87,9 @@ describe('FocusScope', () => {
 			</FocusScope>,
 			{container: document.getElementById('reactRoot') as HTMLElement}
 		);
+	});
 
+	it('manages focus order when using portals', () => {
 		// Putting this snapshot inline to reference the structure of the DOM
 		expect(document.body).toMatchInlineSnapshot(`
 		<body>
@@ -79,9 +117,25 @@ describe('FocusScope', () => {
 		      <li>
 		        <a
 		          href="#"
-		          id="linkInPortal"
+		          id="linkInPortal1"
 		        >
 		          link 1
+		        </a>
+		      </li>
+		      <li>
+		        <a
+		          href="#"
+		          id="linkInPortal2"
+		        >
+		          link 2
+		        </a>
+		      </li>
+		      <li>
+		        <a
+		          href="#"
+		          id="linkInPortal3"
+		        >
+		          link 3
 		        </a>
 		      </li>
 		    </ul>
@@ -94,8 +148,14 @@ describe('FocusScope', () => {
 		const reactButton = document.getElementById(
 			'reactButton'
 		) as HTMLElement;
-		const linkInPortal = document.getElementById(
-			'linkInPortal'
+		const linkInPortalOne = document.getElementById(
+			'linkInPortal1'
+		) as HTMLElement;
+		const linkInPortalTwo = document.getElementById(
+			'linkInPortal2'
+		) as HTMLElement;
+		const linkInPortalThree = document.getElementById(
+			'linkInPortal3'
 		) as HTMLElement;
 
 		userEvent.tab();
@@ -108,10 +168,185 @@ describe('FocusScope', () => {
 
 		userEvent.tab();
 
-		expect(linkInPortal).toHaveFocus();
+		expect(linkInPortalOne).toHaveFocus();
+
+		userEvent.tab();
+
+		expect(linkInPortalTwo).toHaveFocus();
+
+		userEvent.tab();
+
+		expect(linkInPortalThree).toHaveFocus();
 
 		userEvent.tab();
 
 		expect(htmlButton2).toHaveFocus();
+	});
+
+	it('interacts with shift + tab', () => {
+		const htmlButton1 = document.getElementById('button1') as HTMLElement;
+		const htmlButton2 = document.getElementById('button2') as HTMLElement;
+
+		const reactButton = document.getElementById(
+			'reactButton'
+		) as HTMLElement;
+
+		htmlButton2.focus();
+
+		expect(htmlButton2).toHaveFocus();
+
+		userEvent.tab({shift: true});
+
+		expect(reactButton).toHaveFocus();
+
+		userEvent.tab({shift: true});
+
+		expect(htmlButton1).toHaveFocus();
+	});
+
+	it('interacts with down arrow key', () => {
+		const linkInPortalOne = document.getElementById(
+			'linkInPortal1'
+		) as HTMLElement;
+		const linkInPortalTwo = document.getElementById(
+			'linkInPortal2'
+		) as HTMLElement;
+		const linkInPortalThree = document.getElementById(
+			'linkInPortal3'
+		) as HTMLElement;
+
+		linkInPortalOne.focus();
+
+		expect(linkInPortalOne).toHaveFocus();
+
+		fireEvent.keyDown(linkInPortalOne, {key: 'ArrowDown'});
+
+		expect(linkInPortalTwo).toHaveFocus();
+
+		fireEvent.keyDown(linkInPortalTwo, {key: 'ArrowDown'});
+
+		expect(linkInPortalThree).toHaveFocus();
+
+		fireEvent.keyDown(linkInPortalThree, {key: 'ArrowDown'});
+	});
+
+	it('interacts with up arrow key', () => {
+		const linkInPortalOne = document.getElementById(
+			'linkInPortal1'
+		) as HTMLElement;
+		const linkInPortalTwo = document.getElementById(
+			'linkInPortal2'
+		) as HTMLElement;
+		const linkInPortalThree = document.getElementById(
+			'linkInPortal3'
+		) as HTMLElement;
+
+		linkInPortalThree.focus();
+
+		expect(linkInPortalThree).toHaveFocus();
+
+		fireEvent.keyDown(linkInPortalThree, {key: 'ArrowUp'});
+
+		expect(linkInPortalTwo).toHaveFocus();
+
+		fireEvent.keyDown(linkInPortalTwo, {key: 'ArrowUp'});
+
+		expect(linkInPortalOne).toHaveFocus();
+
+		fireEvent.keyDown(linkInPortalOne, {key: 'ArrowUp'});
+	});
+
+	describe('FocusScope outside the React Tree', () => {
+		beforeEach(() => {
+			jest.clearAllTimers();
+			document.body.innerHTML = '';
+			cleanup();
+		});
+
+		it('interacts with React.Portal', () => {
+			render(
+				<>
+					<ClayPortal>
+						<div id="content">
+							<button id="button1" />
+						</div>
+					</ClayPortal>
+
+					<DropDownWithState>
+						<ClayDropDown.ItemList>
+							{[
+								{href: '#one', label: 'one'},
+								{href: '#two', label: 'two'},
+								{href: '#three', label: 'three'},
+							].map((item, i) => (
+								<ClayDropDown.Item
+									href={item.href}
+									key={i}
+									spritemap="/foo/bar"
+								>
+									{item.label}
+								</ClayDropDown.Item>
+							))}
+						</ClayDropDown.ItemList>
+					</DropDownWithState>
+				</>
+			);
+			const dropdownButton = document.querySelector(
+				'.dropdown-toggle'
+			) as HTMLElement;
+			const htmlButton1 = document.getElementById(
+				'button1'
+			) as HTMLElement;
+
+			dropdownButton.click();
+
+			userEvent.tab();
+
+			expect(dropdownButton).toHaveFocus();
+
+			userEvent.tab();
+
+			expect(htmlButton1).toHaveFocus();
+		});
+
+		it('interacts without React.Portal', () => {
+			render(
+				<>
+					<div id="content">
+						<button id="button1" />
+					</div>
+
+					<DropDownWithState>
+						<ClayDropDown.ItemList>
+							{[
+								{href: '#one', label: 'one'},
+								{href: '#two', label: 'two'},
+							].map((item, i) => (
+								<ClayDropDown.Item
+									href={item.href}
+									key={i}
+									spritemap="/foo/bar"
+								>
+									{item.label}
+								</ClayDropDown.Item>
+							))}
+						</ClayDropDown.ItemList>
+					</DropDownWithState>
+				</>
+			);
+
+			const dropdownButton = document.querySelector(
+				'.dropdown-toggle'
+			) as HTMLElement;
+			const htmlButton1 = document.getElementById(
+				'button1'
+			) as HTMLElement;
+
+			dropdownButton.click();
+
+			userEvent.tab();
+
+			expect(htmlButton1).toHaveFocus();
+		});
 	});
 });
