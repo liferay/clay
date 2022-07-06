3ad788b5ae3a34ae1375097c371b58e629d3490c
Matuzalém Teles <matuzalemteles@gmail.com>
2022-07-06 10:55:39 -0500

    fix(clayui.com): fix clipboard error in examples

--- clayui.com/src/components/CodeClipboard/CodeClipboard.js
+++ clayui.com/src/components/CodeClipboard/CodeClipboard.js
@@ -8,51 +8,56 @@ import {useEffect} from 'react';
 
 export default (props) => {
 	useEffect(() => {
-		const clayClipboard = new Clipboard('.tab-pane .btn-copy', {
-			text: (delegateTarget) => {
-				const buttonContent = delegateTarget.innerHTML;
-				const gatsbyHighlightContainer =
-					delegateTarget.closest('.gatsby-highlight') ||
-					delegateTarget.closest('.code-container');
-				const jspTab = gatsbyHighlightContainer.querySelector(
-					'[aria-labelledby="tab-JSP"]'
-				);
-				const reactTab = gatsbyHighlightContainer.querySelector(
-					'[aria-labelledby="tab-React"]'
-				);
+		const clayClipboard = new Clipboard(
+			'.gatsby-highlight .btn-copy, .code-container .btn[title=Copy]',
+			{
+				text: (delegateTarget) => {
+					const buttonContent = delegateTarget.innerHTML;
+					const gatsbyHighlightContainer =
+						delegateTarget.closest('.gatsby-highlight') ||
+						delegateTarget.closest('.code-container');
+					const jspTab = gatsbyHighlightContainer.querySelector(
+						'[aria-labelledby="tab-JSP"]'
+					);
+					const reactTab = gatsbyHighlightContainer.querySelector(
+						'[aria-labelledby="tab-React"]'
+					);
 
-				delegateTarget.innerHTML = delegateTarget.innerHTML.replace(
-					/paste/g,
-					'check'
-				);
+					delegateTarget.innerHTML = delegateTarget.innerHTML.replace(
+						/paste/g,
+						'check'
+					);
 
-				delegateTarget.setAttribute('title', 'Copied');
-
-				gatsbyHighlightContainer
-					.querySelector('.copied-alert')
-					.classList.remove('d-none');
-
-				setTimeout(() => {
-					delegateTarget.setAttribute('title', 'Copy');
-
-					delegateTarget.innerHTML = buttonContent;
+					delegateTarget.setAttribute('title', 'Copied');
 
 					gatsbyHighlightContainer
 						.querySelector('.copied-alert')
-						.classList.add('d-none');
-				}, 2000);
+						.classList.remove('d-none');
 
-				let codeContent =
-					gatsbyHighlightContainer.querySelector('pre code') ||
-					(reactTab.classList.contains('active') ? reactTab : jspTab);
+					setTimeout(() => {
+						delegateTarget.setAttribute('title', 'Copy');
 
-				if (codeContent) {
-					codeContent = codeContent.innerText;
-				}
+						delegateTarget.innerHTML = buttonContent;
 
-				return codeContent;
-			},
-		});
+						gatsbyHighlightContainer
+							.querySelector('.copied-alert')
+							.classList.add('d-none');
+					}, 2000);
+
+					let codeContent =
+						gatsbyHighlightContainer.querySelector('pre code') ||
+						(reactTab.classList.contains('active')
+							? reactTab
+							: jspTab);
+
+					if (codeContent) {
+						codeContent = codeContent.innerText;
+					}
+
+					return codeContent;
+				},
+			}
+		);
 
 		return () => {
 			clayClipboard.destroy();
