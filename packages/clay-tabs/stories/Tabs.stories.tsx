d235191896eddb8cb644a6d3ff54d95933c5f06a
Matuzalém Teles <matuzalemteles@gmail.com>
2022-10-21 15:40:58 -0500

    chore(@clayui/tabs): update story examples

--- packages/clay-tabs/stories/Tabs.stories.tsx
+++ packages/clay-tabs/stories/Tabs.stories.tsx
@@ -3,31 +3,16 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-import ClayDropDown, {Align} from '@clayui/drop-down';
-import ClayIcon from '@clayui/icon';
 import React, {useState} from 'react';
 
 import ClayTabs from '../src';
 
-const DropDownWithState = ({children, trigger, ...others}: any) => {
-	const [active, setActive] = useState<boolean>(false);
-
-	return (
-		<ClayDropDown
-			active={active}
-			alignmentPosition={Align.BottomLeft}
-			hasRightSymbols
-			onActiveChange={(newVal: boolean) => setActive(newVal)}
-			trigger={trigger}
-			{...others}
-		>
-			{children}
-		</ClayDropDown>
-	);
-};
-
 export default {
 	argTypes: {
+		activation: {
+			control: {type: 'select'},
+			options: ['manual', 'automatic', null],
+		},
 		displayType: {
 			control: {type: 'select'},
 			options: ['basic', 'underline', null],
@@ -38,119 +23,53 @@ export default {
 };
 
 export const Default = (args: any) => {
-	const [activeIndex, setActiveIndex] = useState<number>(0);
-
-	const dropdownTabsItems = [
-		{
-			disabled: true,
-			label: 'Tab 6',
-			tabkey: 5,
-		},
-		{
-			label: 'Tab 7',
-			tabkey: 6,
-		},
-		{
-			label: 'Tab 8',
-			tabkey: 7,
-		},
-	];
+	const [active, setActive] = useState<number>(0);
 
 	return (
 		<>
 			<ClayTabs
+				activation={args.activation}
+				active={active}
 				displayType={args.displayType}
 				justified={args.justified}
 				modern={args.modern}
+				onActiveChange={setActive}
 			>
 				<ClayTabs.Item
-					active={activeIndex == 0}
 					disabled={args.disabledFirstTab}
-					href="#"
 					innerProps={{
 						'aria-controls': 'tabpanel1',
 					}}
-					onClick={() => setActiveIndex(0)}
 				>
 					Tab 1
 				</ClayTabs.Item>
 				<ClayTabs.Item
-					active={activeIndex == 1}
 					disabled={args.disabledSecondTab}
-					href="#"
 					innerProps={{
 						'aria-controls': 'tabpanel2',
 					}}
-					onClick={() => setActiveIndex(1)}
 				>
 					Tab 2
 				</ClayTabs.Item>
 				<ClayTabs.Item
-					active={activeIndex == 2}
 					disabled={args.disabledThirdTab}
-					href="#"
 					innerProps={{
 						'aria-controls': 'tabpanel3',
 					}}
-					onClick={() => setActiveIndex(2)}
 				>
 					Tab 3
 				</ClayTabs.Item>
 				<ClayTabs.Item
-					active={activeIndex == 3}
 					disabled={args.disabledFourthTab}
-					href="#"
 					innerProps={{
 						'aria-controls': 'tabpanel4',
 					}}
-					onClick={() => setActiveIndex(3)}
 				>
 					Tab 4
 				</ClayTabs.Item>
-
-				<DropDownWithState
-					trigger={
-						<ClayTabs.Item
-							active={[5, 6, 7].includes(activeIndex)}
-							disabled={args.disabledFourthTab}
-							innerProps={{
-								'aria-controls': 'tabpanel5',
-							}}
-							onClick={() => setActiveIndex(4)}
-						>
-							Tab 5
-							<ClayIcon symbol="caret-bottom" />
-						</ClayTabs.Item>
-					}
-				>
-					<ClayDropDown.ItemList>
-						{dropdownTabsItems.map(
-							({disabled = false, label, tabkey}, i) => {
-								return (
-									<ClayDropDown.Item
-										active={activeIndex === tabkey}
-										aria-controls={`tabpanel${tabkey}`}
-										aria-selected={activeIndex === tabkey}
-										disabled={disabled}
-										key={i}
-										onClick={() => setActiveIndex(tabkey)}
-										role="tab"
-										symbolRight={
-											activeIndex === tabkey
-												? 'check'
-												: undefined
-										}
-									>
-										{label}
-									</ClayDropDown.Item>
-								);
-							}
-						)}
-					</ClayDropDown.ItemList>
-				</DropDownWithState>
 			</ClayTabs>
-			<ClayTabs.Content activeIndex={activeIndex} fade={args.fade}>
-				<ClayTabs.TabPane id="tabpanel1" role="tabpanel">
+			<ClayTabs.Content activeIndex={active} fade={args.fade}>
+				<ClayTabs.TabPane id="tabpanel1">
 					{`1. Single origin, extra id beans, eu to go, skinny
 				americano ut aftertas te sugar. At americano, viennese
 				variety iced grounds, grinder froth and pumpkin spice
@@ -160,7 +79,7 @@ export const Default = (args: any) => {
 				plunger pot. Single shot variety pumpkin spice seasonal
 				skinny barista carajillo robust cream.`}
 				</ClayTabs.TabPane>
-				<ClayTabs.TabPane id="tabpanel2" role="tabpanel">
+				<ClayTabs.TabPane id="tabpanel2">
 					{`2. Single origin, extra id beans, eu to go, skinny
 				americano ut aftertaste sugar. At americano, viennese
 				variety iced grounds, grinder froth and pumpkin spice
@@ -170,7 +89,7 @@ export const Default = (args: any) => {
 				plunger pot. Single shot variety pumpkin spice seasonal
 				skinny barista carajillo robust cream.`}
 				</ClayTabs.TabPane>
-				<ClayTabs.TabPane id="tabpanel3" role="tabpanel">
+				<ClayTabs.TabPane id="tabpanel3">
 					{`3. Single origin, extra id beans, eu to go, skinny
 				americano ut aftertaste sugar. At americano, viennese
 				variety iced grounds, grinder froth and pumpkin spice
@@ -180,7 +99,7 @@ export const Default = (args: any) => {
 				plunger pot. Single shot variety pumpkin spice seasonal
 				skinny barista carajillo robust cream.`}
 				</ClayTabs.TabPane>
-				<ClayTabs.TabPane id="tabpanel4" role="tabpanel">
+				<ClayTabs.TabPane id="tabpanel4">
 					{`4. Single origin, extra id beans, eu to go, skinny
 				americano ut aftertaste sugar. At americano, viennese
 				variety iced grounds, grinder froth and pumpkin spice
@@ -190,38 +109,8 @@ export const Default = (args: any) => {
 				plunger pot. Single shot variety pumpkin spice seasonal
 				skinny barista carajillo robust cream.`}
 				</ClayTabs.TabPane>
-				<ClayTabs.TabPane id="tabpanel5" role="tabpanel">
+				<ClayTabs.TabPane id="tabpanel5">
 					{`4. Single origin, extra id beans, eu to go, skinny
-				americano ut aftertaste sugar. At americano, viennese
-				variety iced grounds, grinder froth and pumpkin spice
-				aromatic. Cultivar aged lungo, grounds café au lait,
-				skinny, blue mountain, in variety sugar shop roast.
-				Wings, blue mountain affogato organic cappuccino java
-				plunger pot. Single shot variety pumpkin spice seasonal
-				skinny barista carajillo robust cream.`}
-				</ClayTabs.TabPane>
-				<ClayTabs.TabPane id="tabpanel6" role="tabpanel">
-					{`6. Single origin, extra id beans, eu to go, skinny
-				americano ut aftertaste sugar. At americano, viennese
-				variety iced grounds, grinder froth and pumpkin spice
-				aromatic. Cultivar aged lungo, grounds café au lait,
-				skinny, blue mountain, in variety sugar shop roast.
-				Wings, blue mountain affogato organic cappuccino java
-				plunger pot. Single shot variety pumpkin spice seasonal
-				skinny barista carajillo robust cream.`}
-				</ClayTabs.TabPane>
-				<ClayTabs.TabPane id="tabpanel7" role="tabpanel">
-					{`7. Single origin, extra id beans, eu to go, skinny
-				americano ut aftertaste sugar. At americano, viennese
-				variety iced grounds, grinder froth and pumpkin spice
-				aromatic. Cultivar aged lungo, grounds café au lait,
-				skinny, blue mountain, in variety sugar shop roast.
-				Wings, blue mountain affogato organic cappuccino java
-				plunger pot. Single shot variety pumpkin spice seasonal
-				skinny barista carajillo robust cream.`}
-				</ClayTabs.TabPane>
-				<ClayTabs.TabPane id="tabpanel8" role="tabpanel">
-					{`8. Single origin, extra id beans, eu to go, skinny
 				americano ut aftertaste sugar. At americano, viennese
 				variety iced grounds, grinder froth and pumpkin spice
 				aromatic. Cultivar aged lungo, grounds café au lait,
@@ -236,6 +125,7 @@ export const Default = (args: any) => {
 };
 
 Default.args = {
+	activation: 'manual',
 	disabledFirstTab: false,
 	disabledFourthTab: false,
 	disabledSecondTab: true,
@@ -246,23 +136,17 @@ Default.args = {
 };
 
 export const WithState = () => {
-	const [activeIndex, setActiveIndex] = useState(0);
+	const [active, setActive] = useState(0);
 
 	return (
 		<div style={{padding: 50}}>
-			<ClayTabs modern={false}>
-				{['Tab 1', 'Tab 2', 'Tab 3'].map((item, index) => (
-					<ClayTabs.Item
-						active={activeIndex === index}
-						key={item}
-						onClick={() => setActiveIndex(index)}
-					>
-						{item}
-					</ClayTabs.Item>
+			<ClayTabs active={active} modern={false} onActiveChange={setActive}>
+				{['Tab 1', 'Tab 2', 'Tab 3'].map((item) => (
+					<ClayTabs.Item key={item}>{item}</ClayTabs.Item>
 				))}
 			</ClayTabs>
 
-			<ClayTabs.Content activeIndex={activeIndex} fade>
+			<ClayTabs.Content activeIndex={active} fade>
 				<ClayTabs.TabPane>
 					Single origin, extra id beans, eu to go, skinny americano ut
 					aftertaste sugar. At americano, viennese variety iced
