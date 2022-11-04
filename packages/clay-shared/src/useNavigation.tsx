87b13a6f57922f233fa738bcee87cd1a7e704eae
Matuzalém Teles <matuzalemteles@gmail.com>
2022-11-04 14:08:54 -0500

    feat(@clayui/shared): adds new hook to handle navigation using arrow keys

---
+++ packages/clay-shared/src/useNavigation.tsx
@@ -0,0 +1,87 @@
+/**
+ * SPDX-FileCopyrightText: © 2022 Liferay, Inc. <https://liferay.com>
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+import {useCallback} from 'react';
+
+import {Keys} from './Keys';
+import {FOCUSABLE_ELEMENTS} from './useFocusManagement';
+
+type Props<T> = {
+	/**
+	 * Flag to indicate the navigation behavior in the tab.
+	 *
+	 * - manual - it will just move the focus and tab activation is done just
+	 * by pressing space or enter.
+	 * - automatic - moves the focus to the tab and activates the tab.
+	 */
+	activation?: 'manual' | 'automatic';
+
+	/**
+	 * Reference of the parent element of the focusable elements.
+	 */
+	containeRef: React.MutableRefObject<T>;
+
+	/**
+	 * Indicates whether the element's orientation is horizontal or vertical.
+	 */
+	orientation: 'horizontal' | 'vertical';
+};
+
+const verticalKeys = [Keys.Up, Keys.Down, Keys.Home, Keys.End];
+const horizontalKeys = [Keys.Left, Keys.Right, Keys.Home, Keys.End];
+
+export function useNavigation<T extends HTMLElement | null>({
+	activation,
+	containeRef,
+	orientation = 'horizontal',
+}: Props<T>) {
+	const onKeyDown = useCallback((event: React.KeyboardEvent<HTMLElement>) => {
+		if (!containeRef.current) {
+			return;
+		}
+
+		const keys = orientation === 'vertical' ? verticalKeys : horizontalKeys;
+
+		if (keys.includes(event.key)) {
+			const tabs = Array.from<HTMLElement>(
+				containeRef.current.querySelectorAll(
+					FOCUSABLE_ELEMENTS.join(',')
+				)
+			);
+
+			let tab: HTMLElement | undefined;
+
+			switch (event.key) {
+				case Keys.Left:
+				case Keys.Right:
+				case Keys.Down:
+				case Keys.Up: {
+					const activeElement = document.activeElement as HTMLElement;
+
+					const position = tabs.indexOf(activeElement);
+					const key =
+						orientation === 'vertical' ? Keys.Up : Keys.Left;
+
+					tab = tabs[event.key === key ? position - 1 : position + 1];
+
+					break;
+				}
+				default:
+					tab = tabs[event.key === Keys.Home ? 0 : tabs.length - 1];
+					break;
+			}
+
+			if (tab) {
+				tab.focus();
+
+				if (activation === 'automatic') {
+					tab.click();
+				}
+			}
+		}
+	}, []);
+
+	return {onKeyDown};
+}
