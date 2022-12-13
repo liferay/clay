6b055ada959d7dfc9977a1ddcf69542b8a1e2c05
Matuzalém Teles <matuzalemteles@gmail.com>
2022-12-12 19:14:07 -0600

    feat(@clayui/shared): adds configuration for visual navigation and event scheduling

--- packages/clay-shared/src/useNavigation.tsx
+++ packages/clay-shared/src/useNavigation.tsx
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-import {useCallback, useEffect, useRef} from 'react';
+import React, {useCallback, useEffect, useRef} from 'react';
 
 import {Keys} from './Keys';
 import {FOCUSABLE_ELEMENTS, isFocusable} from './useFocusManagement';
@@ -19,6 +19,12 @@ type Props<T> = {
 	activation?: 'manual' | 'automatic';
 
 	/**
+	 * The id of the currently active item that is highlighted.
+	 * Typically used when navigation is not done via focus.
+	 */
+	active?: string;
+
+	/**
 	 * Reference of the parent element of the focusable elements.
 	 */
 	containeRef: React.MutableRefObject<T>;
@@ -27,6 +33,11 @@ type Props<T> = {
 	 * Flag to indicate if navigation should loop.
 	 */
 	loop?: boolean;
+
+	/**
+	 * Callback is called when the intent is to move to the element.
+	 */
+	onNavigate?: (element: HTMLElement, index: number) => void;
 
 	/**
 	 * Indicates whether the element's orientation is horizontal or vertical.
@@ -50,8 +61,10 @@ const horizontalKeys = [Keys.Left, Keys.Right, Keys.Home, Keys.End];
 
 export function useNavigation<T extends HTMLElement | null>({
 	activation = 'manual',
+	active,
 	containeRef,
 	loop = false,
+	onNavigate,
 	orientation = 'horizontal',
 	typeahead = false,
 	visible = false,
@@ -61,6 +74,12 @@ export function useNavigation<T extends HTMLElement | null>({
 	const prevIndexRef = useRef<number | null>(-1);
 	const matchIndexRef = useRef<number | null>(null);
 
+	// An event can be scheduled when the content is not visible in the DOM, it
+	// will be executed in sequence after the element is visible in the DOM.
+	const pendingEventStack = useRef<Array<React.KeyboardEvent<HTMLElement>>>(
+		[]
+	);
+
 	useEffect(() => {
 		if (visible) {
 			clearTimeout(timeoutIdRef.current);
@@ -69,140 +88,177 @@ export function useNavigation<T extends HTMLElement | null>({
 		}
 	}, [visible]);
 
-	const onKeyDown = useCallback((event: React.KeyboardEvent<HTMLElement>) => {
-		if (!containeRef.current) {
-			return;
-		}
+	const onKeyDown = useCallback(
+		(event: React.KeyboardEvent<HTMLElement>) => {
+			if (!containeRef.current) {
+				event.persist();
+				pendingEventStack.current.push(event);
 
-		const keys = orientation === 'vertical' ? verticalKeys : horizontalKeys;
-		const alternativeKeys =
-			orientation === 'vertical' ? horizontalKeys : verticalKeys;
+				return;
+			}
 
-		if (
-			keys.includes(event.key) ||
-			(typeahead && !alternativeKeys.includes(event.key))
-		) {
-			const tabs = Array.from<HTMLElement>(
-				containeRef.current.querySelectorAll(
-					FOCUSABLE_ELEMENTS.join(',')
-				)
-			).filter((element) =>
-				isFocusable({
-					contentEditable: element.contentEditable,
-					disabled: element.getAttribute('disabled') !== null,
-					offsetParent: element.offsetParent,
-					tabIndex: 0,
-					tagName: element.tagName,
-				})
-			);
+			const keys =
+				orientation === 'vertical' ? verticalKeys : horizontalKeys;
+			const alternativeKeys =
+				orientation === 'vertical' ? horizontalKeys : verticalKeys;
 
-			let tab: HTMLElement | undefined;
+			if (
+				keys.includes(event.key) ||
+				(typeahead && !alternativeKeys.includes(event.key))
+			) {
+				const tabs = Array.from<HTMLElement>(
+					containeRef.current.querySelectorAll(
+						FOCUSABLE_ELEMENTS.join(',')
+					)
+				).filter((element) =>
+					isFocusable({
+						contentEditable: element.contentEditable,
+						disabled: element.getAttribute('disabled') !== null,
+						offsetParent: element.offsetParent,
+						tabIndex: 0,
+						tagName: element.tagName,
+					})
+				);
 
-			switch (event.key) {
-				case Keys.Left:
-				case Keys.Right:
-				case Keys.Down:
-				case Keys.Up: {
-					const activeElement = document.activeElement as HTMLElement;
+				let tab: HTMLElement | undefined;
 
-					const position = tabs.indexOf(activeElement);
+				switch (event.key) {
+					case Keys.Left:
+					case Keys.Right:
+					case Keys.Down:
+					case Keys.Up: {
+						const activeElement =
+							document.activeElement as HTMLElement;
 
-					if (position === -1) {
+						let position = tabs.indexOf(activeElement);
+
+						if (typeof active === 'string') {
+							position = tabs.findIndex(
+								(element) =>
+									element.getAttribute('id') === active
+							);
+						}
+
+						if (position === -1) {
+							break;
+						}
+
+						const key =
+							orientation === 'vertical' ? Keys.Up : Keys.Left;
+
+						tab =
+							tabs[
+								event.key === key ? position - 1 : position + 1
+							];
+
+						if (loop && !tab) {
+							tab = tabs[event.key === key ? tabs.length - 1 : 0];
+						}
+
 						break;
 					}
+					case Keys.Home:
+					case Keys.End:
+						tab =
+							tabs[event.key === Keys.Home ? 0 : tabs.length - 1];
+						break;
+					default: {
+						const target = event.target as HTMLElement;
 
-					const key =
-						orientation === 'vertical' ? Keys.Up : Keys.Left;
-
-					tab = tabs[event.key === key ? position - 1 : position + 1];
-
-					if (loop && !tab) {
-						tab = tabs[event.key === key ? tabs.length - 1 : 0];
-					}
-
-					break;
-				}
-				case Keys.Home:
-				case Keys.End:
-					tab = tabs[event.key === Keys.Home ? 0 : tabs.length - 1];
-					break;
-				default: {
-					const target = event.target as HTMLElement;
-
-					if (!typeahead || target.tagName === 'INPUT') {
-						return;
-					}
-
-					if (!event.currentTarget.contains(target)) {
-						return;
-					}
-
-					if (
-						stringRef.current.length > 0 &&
-						stringRef.current[0] !== Keys.Spacebar
-					) {
-						if (event.key === Keys.Spacebar) {
-							event.preventDefault();
-							event.stopPropagation();
+						if (!typeahead || target.tagName === 'INPUT') {
+							return;
 						}
+
+						if (!event.currentTarget.contains(target)) {
+							return;
+						}
+
+						if (
+							stringRef.current.length > 0 &&
+							stringRef.current[0] !== Keys.Spacebar
+						) {
+							if (event.key === Keys.Spacebar) {
+								event.preventDefault();
+								event.stopPropagation();
+							}
+						}
+
+						if (
+							event.key.length !== 1 ||
+							event.ctrlKey ||
+							event.metaKey ||
+							event.altKey
+						) {
+							return;
+						}
+
+						if (stringRef.current === event.key) {
+							stringRef.current = '';
+							prevIndexRef.current = matchIndexRef.current;
+						}
+
+						stringRef.current += event.key;
+
+						clearTimeout(timeoutIdRef.current);
+
+						timeoutIdRef.current = setTimeout(() => {
+							stringRef.current = '';
+							prevIndexRef.current = matchIndexRef.current;
+						}, 1000);
+
+						const prevIndex = prevIndexRef.current;
+
+						const orderedList = [
+							...tabs.slice((prevIndex ?? 0) + 1),
+							...tabs.slice(0, (prevIndex ?? 0) + 1),
+						];
+
+						tab = orderedList.find(
+							(element) =>
+								element.innerText
+									?.toLowerCase()
+									.indexOf(
+										stringRef.current.toLocaleLowerCase()
+									) === 0
+						);
+
+						if (tab) {
+							matchIndexRef.current = tabs.indexOf(tab);
+						}
+						break;
+					}
+				}
+
+				if (tab) {
+					event.preventDefault();
+					if (onNavigate) {
+						onNavigate(tab, tabs.indexOf(tab));
+					} else {
+						tab.focus();
 					}
 
-					if (
-						event.key.length !== 1 ||
-						event.ctrlKey ||
-						event.metaKey ||
-						event.altKey
-					) {
-						return;
+					if (activation === 'automatic') {
+						tab.click();
 					}
-
-					if (stringRef.current === event.key) {
-						stringRef.current = '';
-						prevIndexRef.current = matchIndexRef.current;
-					}
-
-					stringRef.current += event.key;
-
-					clearTimeout(timeoutIdRef.current);
-
-					timeoutIdRef.current = setTimeout(() => {
-						stringRef.current = '';
-						prevIndexRef.current = matchIndexRef.current;
-					}, 1000);
-
-					const prevIndex = prevIndexRef.current;
-
-					const orderedList = [
-						...tabs.slice((prevIndex ?? 0) + 1),
-						...tabs.slice(0, (prevIndex ?? 0) + 1),
-					];
-
-					tab = orderedList.find(
-						(element) =>
-							element.innerText
-								?.toLowerCase()
-								.indexOf(
-									stringRef.current.toLocaleLowerCase()
-								) === 0
-					);
-
-					if (tab) {
-						matchIndexRef.current = tabs.indexOf(tab);
-					}
-					break;
 				}
 			}
+		},
+		[active]
+	);
 
-			if (tab) {
-				event.preventDefault();
-				tab.focus();
+	useEffect(() => {
+		if (visible && pendingEventStack.current.length !== 0) {
+			for (
+				let index = 0;
+				index < pendingEventStack.current.length;
+				index++
+			) {
+				const event = pendingEventStack.current.shift();
 
-				if (activation === 'automatic') {
-					tab.click();
-				}
+				onKeyDown(event!);
 			}
 		}
-	}, []);
+	}, [visible]);
 
 	return {onKeyDown};
 }
