99ab5811ead8419576f73c052d65c5463ab3acaf
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2023-03-27 17:16:38 +0200

    feat(@clayui/focus-trap): Add stories for FocusTrap component

---
+++ packages/clay-core/stories/FocusTrap.stories.tsx
@@ -0,0 +1,49 @@
+/**
+ * SPDX-FileCopyrightText: © 2022 Liferay, Inc. <https://liferay.com>
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+import ClayButton from '@clayui/button';
+import ClayCard from '@clayui/card';
+import React, {useRef, useState} from 'react';
+
+import {FocusTrap} from '../src/focus-trap';
+
+export default {
+	component: FocusTrap,
+	title: 'Design System/Components/FocusTrap',
+};
+
+export const Default = () => {
+	const [active, setActive] = useState(false);
+	const activateButtonRef = useRef<HTMLButtonElement>(null);
+
+	const onDeactivateFocusTrap = () => {
+		setActive(false);
+
+		if (activateButtonRef.current) {
+			activateButtonRef.current.focus();
+		}
+	};
+
+	return (
+		<>
+			<ClayButton onClick={() => setActive(true)} ref={activateButtonRef}>
+				Activate trap
+			</ClayButton>
+			{active && (
+				<FocusTrap active={active}>
+					<ClayCard className="mt-4 p-4">
+						<ClayButton displayType="link">Button 1</ClayButton>
+						<ClayButton displayType="link">Button 2</ClayButton>
+						<div className="mt-4">
+							<ClayButton onClick={onDeactivateFocusTrap}>
+								Leave trap
+							</ClayButton>
+						</div>
+					</ClayCard>
+				</FocusTrap>
+			)}
+		</>
+	);
+};
