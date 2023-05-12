52a4a91dbad8570efbb70c3b7bf2abeffa229bbe
Matuzalém Teles <matuzalemteles@gmail.com>
2023-05-11 21:02:25 -0500

    feat(@clayui/core): add basic snapshot tests for Vertical Nav

---
+++ packages/clay-core/src/vertical-nav/__tests__/BasicRendering.tsx
@@ -0,0 +1,66 @@
+/**
+ * SPDX-FileCopyrightText: © 2023 Liferay, Inc. <https://liferay.com>
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+import {cleanup, render} from '@testing-library/react';
+import React from 'react';
+
+import {VerticalNav} from '../';
+
+describe('VerticalNav basic rendering', () => {
+	afterEach(() => cleanup());
+
+	it('render static content', () => {
+		const {container} = render(
+			<VerticalNav>
+				<VerticalNav.Item active key="home">
+					Home
+				</VerticalNav.Item>
+				<VerticalNav.Item key="about">About</VerticalNav.Item>
+				<VerticalNav.Item key="contact">Contact</VerticalNav.Item>
+			</VerticalNav>
+		);
+
+		expect(container).toMatchSnapshot();
+	});
+
+	it('render dynamic content', () => {
+		const {container} = render(
+			<VerticalNav
+				items={[
+					{
+						id: 1,
+						items: [
+							{
+								active: true,
+								href: '#',
+								id: 2,
+								label: 'Nested1',
+							},
+						],
+						label: 'Home',
+					},
+					{
+						href: '#',
+						id: 3,
+						label: 'About',
+					},
+					{
+						href: '#',
+						id: 4,
+						label: 'Contact',
+					},
+				]}
+			>
+				{(item: any) => (
+					<VerticalNav.Item active={item.active} items={item.items}>
+						{item.label}
+					</VerticalNav.Item>
+				)}
+			</VerticalNav>
+		);
+
+		expect(container).toMatchSnapshot();
+	});
+});
