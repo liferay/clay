0ba2c2ae933cc84b75bd2641724fffe9af9fd519
Matuzalém Teles <matuzalemteles@gmail.com>
2023-07-18 12:48:45 -0500

    fix(@clayui/core): fix error not having forwardRef for list

--- packages/clay-core/src/tree-view/TreeViewGroup.tsx
+++ packages/clay-core/src/tree-view/TreeViewGroup.tsx
@@ -17,16 +17,19 @@ interface ITreeViewGroupProps<T extends Record<string, any>>
 	extends ICollectionProps<T>,
 		Omit<React.HTMLAttributes<HTMLDivElement>, 'children'> {}
 
-function List({
-	children,
-	...otherProps
-}: React.HTMLAttributes<HTMLUListElement>) {
+const List = React.forwardRef<
+	HTMLUListElement,
+	React.HTMLAttributes<HTMLUListElement>
+>(function ListInner(
+	{children, ...otherProps}: React.HTMLAttributes<HTMLUListElement>,
+	ref
+) {
 	return (
-		<ul {...otherProps} className="treeview-group" role="group">
+		<ul {...otherProps} className="treeview-group" ref={ref} role="group">
 			{children}
 		</ul>
 	);
-}
+});
 
 export function TreeViewGroup<T extends Record<string, any>>(
 	props: ITreeViewGroupProps<T>
