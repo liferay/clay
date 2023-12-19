35db8422e552512a16ea5f080444df8ffa616064
Matuzalém Teles <matuzalemteles@gmail.com>
2023-12-18 19:38:09 -0600

    chore: add svg4everybody to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,16 +1,25 @@
 import '@clayui/css/lib/css/atlas.css';
 const spritemap = require('@clayui/css/lib/images/icons/icons.svg');
-import React from 'react';
+import React, {useEffect} from 'react';
+import svg4everybody from 'svg4everybody';
 import {Provider} from '@clayui/provider';
 
 export const decorators = [
-	(Story) => (
-		<Provider spritemap={spritemap}>
-			<div>
-				<Story />
-			</div>
-		</Provider>
-	),
+	(Story) => {
+		useEffect(() => {
+			svg4everybody({
+				polyfill: true,
+			});
+		}, []);
+
+		return (
+			<Provider spritemap={spritemap}>
+				<div>
+					<Story />
+				</div>
+			</Provider>
+		);
+	},
 ];
 
 export const parameters = {
