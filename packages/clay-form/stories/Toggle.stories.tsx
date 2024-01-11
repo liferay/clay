07879ae8490ee3bd7304f563fd7fbf26e3d35c68
Patrick Yeo <patrick.yeo@liferay.com>
2024-01-11 12:35:33 -0800

    chore(@clayui/form): Adds sizing attribute to Toggle Switch storybook

--- packages/clay-form/stories/Toggle.stories.tsx
+++ packages/clay-form/stories/Toggle.stories.tsx
@@ -8,20 +8,28 @@ import React, {useState} from 'react';
 import {ClayRadioGroup, ClayToggle} from '../src';
 
 export default {
+	argTypes: {
+		sizing: {
+			control: {type: 'select'},
+			options: [undefined, 'sm'],
+		},
+	},
 	component: ClayToggle,
 	title: 'Design System/Components/Toggle',
 };
 
-export const Checkbox = () => {
-	const [toggled, setToggle] = useState<boolean>(false);
+export const Checkbox = (args: any) => {
+	const [toggled1, setToggle1, toggled2, setToggle2, toggled3, setToggle3] =
+		useState<boolean>(false);
 
 	return (
 		<>
 			<ClayToggle
 				aria-label="Toggle checkbox"
 				label="Checkbox"
-				onToggle={setToggle}
-				toggled={toggled}
+				onToggle={setToggle1}
+				sizing={args.sizing}
+				toggled={toggled1}
 			/>
 
 			<br />
@@ -30,12 +38,13 @@ export const Checkbox = () => {
 			<ClayToggle
 				aria-label="Toggle checkbox with symbol"
 				label="with symbol"
-				onToggle={setToggle}
+				onToggle={setToggle2}
+				sizing={args.sizing}
 				symbol={{
 					off: 'times',
 					on: 'check',
 				}}
-				toggled={toggled}
+				toggled={toggled2}
 			/>
 
 			<br />
@@ -45,14 +54,15 @@ export const Checkbox = () => {
 				aria-label="Toggle checkbox disabled"
 				disabled
 				label="Disabled"
-				onToggle={setToggle}
-				toggled={toggled}
+				onToggle={setToggle3}
+				sizing={args.sizing}
+				toggled={toggled3}
 			/>
 		</>
 	);
 };
 
-export const RadioGroup = () => {
+export const RadioGroup = (args: any) => {
 	const [value, setValue] = useState<string>('foo');
 
 	return (
@@ -60,11 +70,26 @@ export const RadioGroup = () => {
 			onSelectedValueChange={(val) => setValue(val as string)}
 			selectedValue={value}
 		>
-			<ClayToggle aria-label="Radio Foo" label="Foo" value="foo" />
+			<ClayToggle
+				aria-label="Radio Foo"
+				label="Foo"
+				sizing={args.sizing}
+				value="foo"
+			/>
 
-			<ClayToggle aria-label="Radio Bar" label="Bar" value="bar" />
+			<ClayToggle
+				aria-label="Radio Bar"
+				label="Bar"
+				sizing={args.sizing}
+				value="bar"
+			/>
 
-			<ClayToggle aria-label="Radio Baz" label="Baz" value="baz" />
+			<ClayToggle
+				aria-label="Radio Baz"
+				label="Baz"
+				sizing={args.sizing}
+				value="baz"
+			/>
 		</ClayRadioGroup>
 	);
 };
