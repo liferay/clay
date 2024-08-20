77803fdbdc708e660311f9fb8a6cf7a0e61187a4
Matuzalém Teles <matuzalemteles@gmail.com>
2024-08-19 20:06:09 -0500

    chore(www): move the empty state document to the new pattern

--- www/data.ts
+++ www/data.ts
@@ -5,7 +5,7 @@ export const docs = createSource('./docs/**/*.mdx', {
 });
 
 export const documents = createSource(
-	'../packages/clay-(core|alert|autocomplete|badge|breadcrumb|button|card|color-picker|data-provider|date-picker|drop-down)/docs/**/*.mdx',
+	'../packages/clay-(core|alert|autocomplete|badge|breadcrumb|button|card|color-picker|data-provider|date-picker|drop-down|empty-state)/docs/**/*.mdx',
 	{
 		baseDirectory: 'docs',
 		basePathname: 'components',
