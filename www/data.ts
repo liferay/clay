97d14830536303624bfab2af9a306de1835f9955
Matuzalém Teles <matuzalemteles@gmail.com>
2024-08-19 19:17:38 -0500

    chore(www): move the drop down document to the new pattern

--- www/data.ts
+++ www/data.ts
@@ -5,7 +5,7 @@ export const docs = createSource('./docs/**/*.mdx', {
 });
 
 export const documents = createSource(
-	'../packages/clay-(core|alert|autocomplete|badge|breadcrumb|button|card|color-picker|data-provider|date-picker)/docs/**/*.mdx',
+	'../packages/clay-(core|alert|autocomplete|badge|breadcrumb|button|card|color-picker|data-provider|date-picker|drop-down)/docs/**/*.mdx',
 	{
 		baseDirectory: 'docs',
 		basePathname: 'components',
