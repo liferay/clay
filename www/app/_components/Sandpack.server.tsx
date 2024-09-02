aa73451d56fed56efdab35c493cbfbb3fa56e3f5
Matuzalém Teles <matuzalemteles@gmail.com>
2024-09-02 17:35:17 -0500

    chore(www): move the list document to the new pattern

--- www/app/_components/Sandpack.server.tsx
+++ www/app/_components/Sandpack.server.tsx
@@ -45,6 +45,7 @@ export async function Sandpack({language, children}: Props) {
 					'@clayui/date-picker': 'latest',
 					'@clayui/empty-state': 'latest',
 					'@clayui/link': 'latest',
+					'@clayui/list': 'latest',
 				},
 			}}
 			files={{
