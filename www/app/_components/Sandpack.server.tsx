f3b86dc32cc424e9e834268620cc39a5bf7630e5
Matuzalém Teles <matuzalemteles@gmail.com>
2024-09-12 16:51:44 -0500

    chore(www): move the popover document to the new pattern

--- www/app/_components/Sandpack.server.tsx
+++ www/app/_components/Sandpack.server.tsx
@@ -58,6 +58,7 @@ export async function Sandpack({language, children}: Props) {
 					'@clayui/pagination': 'latest',
 					'@clayui/pagination-bar': 'latest',
 					'@clayui/panel': 'latest',
+					'@clayui/popover': 'latest',
 				},
 			}}
 			files={{
