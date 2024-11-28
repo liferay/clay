bab23223a8e3e9d2fa4bcd49eabb8c48b4b78e2d
Matuzalém Teles <matuzalemteles@gmail.com>
2024-11-28 16:00:57 -0600

    feat(@clayui/tabs): improves component typing to create API Tabs

--- packages/clay-tabs/src/Content.tsx
+++ packages/clay-tabs/src/Content.tsx
@@ -34,45 +34,49 @@ export interface IProps extends React.HTMLAttributes<HTMLDivElement> {
 	tabsId?: string;
 }
 
-const Content = React.forwardRef<HTMLDivElement, IProps>(function Content(
-	{
-		active,
-		activeIndex = 0,
-		children,
-		className,
-		fade = false,
-		tabsId,
-		...otherProps
-	},
-	ref
-) {
-	return (
-		<div
-			className={classNames('tab-content', className)}
-			{...otherProps}
-			ref={ref}
-		>
-			{React.Children.map(children, (child, index) => {
-				if (!React.isValidElement(child)) {
-					return child;
-				}
+export const Content = React.forwardRef<HTMLDivElement, IProps>(
+	function Content(
+		{
+			active,
+			activeIndex = 0,
+			children,
+			className,
+			fade = false,
+			tabsId,
+			...otherProps
+		},
+		ref
+	) {
+		return (
+			<div
+				className={classNames('tab-content', className)}
+				{...otherProps}
+				ref={ref}
+			>
+				{React.Children.map(children, (child, index) => {
+					if (!React.isValidElement(child)) {
+						return child;
+					}
 
-				return React.cloneElement(child, {
-					...child.props,
-					active:
-						typeof active === 'number'
-							? active === index
-							: activeIndex === index,
-					'aria-labelledby': tabsId
-						? `${tabsId}-tab-${index}`
-						: child.props['aria-labelledby'],
-					fade,
-					id: tabsId ? `${tabsId}-tabpanel-${index}` : child.props.id,
-					key: index,
-				});
-			})}
-		</div>
-	);
-});
+					return React.cloneElement(child, {
+						...child.props,
+						active:
+							typeof active === 'number'
+								? active === index
+								: activeIndex === index,
+						'aria-labelledby': tabsId
+							? `${tabsId}-tab-${index}`
+							: child.props['aria-labelledby'],
+						fade,
+						id: tabsId
+							? `${tabsId}-tabpanel-${index}`
+							: child.props.id,
+						key: index,
+					});
+				})}
+			</div>
+		);
+	}
+);
 
 export default Content;
