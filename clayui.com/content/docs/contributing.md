8242f15a0253dd4a9fcbbbeb1d2d177b729c28e6
Matuzalém Teles <matuzalemteles@gmail.com>
2025-02-12 17:58:31 -0600

    chore(clayui.com): delete the old site files

--- clayui.com/content/docs/contributing.md
+++
@@ -1,6 +0,0 @@
----
-title: 'Contributing'
-order: 4
----
-
-See [CONTRIBUTING doc](https://github.com/liferay/clay/blob/master/CONTRIBUTING.md) on our Github repository.
--- clayui.com/src/components/TrackingIssues/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * SPDX-FileCopyrightText: © 2018 Liferay, Inc. <https://liferay.com>
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
-import Tracking from './Tracking';
-
-export default Tracking;
