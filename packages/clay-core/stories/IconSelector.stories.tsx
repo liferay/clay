9db809d501a47b39eeced0982288f6b37b74af04
Ilza Medeiros <ilza.medeiros@liferay.com>
2025-02-13 13:51:09 -0300

    feat(@clayui/icon-selector): LPD-46157 Add docs and stories

---
+++ packages/clay-core/stories/IconSelector.stories.tsx
@@ -0,0 +1,20 @@
+/**
+ * SPDX-FileCopyrightText: © 2022 Liferay, Inc. <https://liferay.com>
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+import React from 'react';
+
+import {IconSelector} from '../src/icon-selector';
+
+export default {
+	title: 'Design System/Components/IconSelector',
+};
+
+export const Default = () => {
+	return (
+		<div>
+			<IconSelector />
+		</div>
+	);
+};
