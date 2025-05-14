2c6a669e784b6e783a55c0c3e6b495fa038df026
Matuzalém Teles <matuzalemteles@gmail.com>
2025-05-13 22:06:58 -0500

    feat(@clayui/core): adds SidePanel implementation

---
+++ packages/clay-core/src/side-panel/index.ts
@@ -0,0 +1,10 @@
+/**
+ * SPDX-FileCopyrightText: © 2025 Liferay, Inc. <https://liferay.com>
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+export {SidePanel} from './SidePanel';
+export {Body} from './Body';
+export {Footer} from './Footer';
+export {Header} from './Header';
+export type {Props as SidePanelProps} from './SidePanel';
---
+++ packages/clay-core/stories/SidePanel.stories.tsx
@@ -0,0 +1,44 @@
+/**
+ * SPDX-FileCopyrightText: © 2025 Liferay, Inc. <https://liferay.com>
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+import Button from '@clayui/button';
+import {useId} from '@clayui/shared';
+import React, {useState} from 'react';
+
+import {Body, Footer, Header, SidePanel} from '../src/side-panel';
+
+export default {
+	title: 'Design System/Components/SidePanel',
+};
+
+export const Default = () => {
+	const [open, setOpen] = useState(false);
+	const sidePanelId = useId();
+
+	return (
+		<>
+			<Button
+				aria-controls={sidePanelId}
+				aria-pressed={open}
+				onClick={() => setOpen(!open)}
+			>
+				Open
+			</Button>
+
+			<SidePanel id={sidePanelId} onOpenChange={setOpen} open={open}>
+				<Header>
+					<div className="component-title">Default Header</div>
+				</Header>
+				<Body>Body</Body>
+				<Footer>
+					<Button.Group spaced>
+						<Button>Primary</Button>
+						<Button displayType="secondary">Secondary</Button>
+					</Button.Group>
+				</Footer>
+			</SidePanel>
+		</>
+	);
+};
