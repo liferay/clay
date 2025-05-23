035472b29ca337016a987c9483a8afa5118c16f8
Patrick Yeo <patrick.yeo@liferay.com>
2025-05-22 17:19:21 -0700

    chore(@clayui/core): SidePanel storybook: adds left and right example

--- packages/clay-core/stories/SidePanel.stories.tsx
+++ packages/clay-core/stories/SidePanel.stories.tsx
@@ -5,6 +5,7 @@
 
 import Button from '@clayui/button';
 import {useId} from '@clayui/shared';
+import Toolbar from '@clayui/toolbar';
 import React, {useRef, useState} from 'react';
 
 import {SidePanel} from '../src/side-panel';
@@ -15,52 +16,96 @@ export default {
 
 export const Default = () => {
 	const [open, setOpen] = useState(false);
+	const [openStart, setOpenStart] = useState(false);
 	const sidePanelId = useId();
+	const sidePanelStartId = useId();
 
 	const ref = useRef<HTMLElement | null>(null);
 
 	return (
-		<div className="position-relative" ref={ref}>
-			<Button
-				aria-controls={sidePanelId}
-				aria-pressed={open}
-				onClick={() => setOpen(!open)}
-			>
-				Open
-			</Button>
+		<div className="m-n3 min-vh-100">
+			<Toolbar>
+				<Toolbar.Nav>
+					<Toolbar.Item className="ml-2">
+						<Toolbar.Section>
+							<Button.Group spaced>
+								<Button
+									aria-controls={sidePanelStartId}
+									aria-pressed={openStart}
+									onClick={() => setOpenStart(!openStart)}
+								>
+									Open Left
+								</Button>
 
-			{`Viennese flavour cup eu, percolator froth ristretto mazagran
-			caffeine. White roast seasonal, mocha trifecta, dripper caffeine
-			spoon acerbic to go macchiato strong. Viennese flavour cup eu, percolator froth ristretto mazagran
-			caffeine. White roast seasonal, mocha trifecta, dripper caffeine
-			spoon acerbic to go macchiato strong. Viennese flavour cup eu, percolator froth ristretto mazagran
-			caffeine. White roast seasonal, mocha trifecta, dripper caffeine
-			spoon acerbic to go macchiato strong. Viennese flavour cup eu, percolator froth ristretto mazagran
-			caffeine. White roast seasonal, mocha trifecta, dripper caffeine
-			spoon acerbic to go macchiato strong.`}
-			<br />
-			<img
-				alt="cat"
-				src="https://cataas.com/cat/says/it has side panel"
-			/>
+								<Button
+									aria-controls={sidePanelId}
+									aria-pressed={open}
+									onClick={() => setOpen(!open)}
+								>
+									Open Right
+								</Button>
+							</Button.Group>
+						</Toolbar.Section>
+					</Toolbar.Item>
+				</Toolbar.Nav>
+			</Toolbar>
 
-			<SidePanel
-				containerRef={ref}
-				id={sidePanelId}
-				onOpenChange={setOpen}
-				open={open}
-			>
-				<SidePanel.Header>
-					<SidePanel.Title>Title</SidePanel.Title>
-				</SidePanel.Header>
-				<SidePanel.Body>Body</SidePanel.Body>
-				<SidePanel.Footer>
-					<Button.Group spaced>
-						<Button>Primary</Button>
-						<Button displayType="secondary">Secondary</Button>
-					</Button.Group>
-				</SidePanel.Footer>
-			</SidePanel>
+			<div ref={ref}>
+				<SidePanel
+					containerRef={ref}
+					direction="left"
+					id={sidePanelStartId}
+					onOpenChange={setOpenStart}
+					open={openStart}
+				>
+					<SidePanel.Header>
+						<SidePanel.Title>Left Panel</SidePanel.Title>
+					</SidePanel.Header>
+					<SidePanel.Body>Body</SidePanel.Body>
+					<SidePanel.Footer>
+						<Button.Group spaced>
+							<Button>Primary</Button>
+							<Button displayType="secondary">Secondary</Button>
+						</Button.Group>
+					</SidePanel.Footer>
+				</SidePanel>
+
+				<div className="container-fluid">
+					{`Viennese flavour cup eu, percolator froth ristretto mazagran
+					caffeine. White roast seasonal, mocha trifecta, dripper caffeine
+					spoon acerbic to go macchiato strong. Viennese flavour cup eu, percolator froth ristretto mazagran
+					caffeine. White roast seasonal, mocha trifecta, dripper caffeine
+					spoon acerbic to go macchiato strong. Viennese flavour cup eu, percolator froth ristretto mazagran
+					caffeine. White roast seasonal, mocha trifecta, dripper caffeine
+					spoon acerbic to go macchiato strong. Viennese flavour cup eu, percolator froth ristretto mazagran
+					caffeine. White roast seasonal, mocha trifecta, dripper caffeine
+					spoon acerbic to go macchiato strong.`}
+					<br />
+					<img
+						alt="cat"
+						className="img-fluid"
+						src="https://cataas.com/cat/says/it has side panel"
+					/>
+				</div>
+
+				<SidePanel
+					containerRef={ref}
+					id={sidePanelId}
+					onOpenChange={setOpen}
+					open={open}
+				>
+					<SidePanel.Header>
+						<SidePanel.Title>Right Panel</SidePanel.Title>
+					</SidePanel.Header>
+					<SidePanel.Body>Body</SidePanel.Body>
+					<SidePanel.Footer>
+						<Button.Group spaced>
+							<Button>Primary</Button>
+							<Button displayType="secondary">Secondary</Button>
+						</Button.Group>
+					</SidePanel.Footer>
+				</SidePanel>
+			</div>
 		</div>
 	);
 };
