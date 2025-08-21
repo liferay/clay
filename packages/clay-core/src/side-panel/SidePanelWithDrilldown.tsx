bf1b04f083ca041c20133adc67ecf64cd2719518
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2025-08-21 11:04:13 +0200

    feat(@clayui/core): LPD-48310 Add new component for side panel with drilldown

---
+++ packages/clay-core/src/side-panel/SidePanelWithDrilldown.tsx
@@ -0,0 +1,91 @@
+/**
+ * SPDX-FileCopyrightText: © 2025 Liferay, Inc. <https://liferay.com>
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+import {InternalDispatch} from '@clayui/shared';
+import classnames from 'classnames';
+import React from 'react';
+import {CSSTransition} from 'react-transition-group';
+
+import {Messages} from './Header';
+import {Props as SidePanelProps, SidePanel} from './SidePanel';
+
+export type Props = {
+	/**
+	 * Callback is called when the selectedPanelKey prop changes (controlled).
+	 */
+
+	onSelectedPanelKeyChange: InternalDispatch<string>;
+
+	/**
+	 * Group of panels to be rendered
+	 */
+
+	panels: Panels;
+
+	/**
+	 * Selected panel key
+	 */
+
+	selectedPanelKey: keyof Panels;
+} & Omit<SidePanelProps, 'children'>;
+
+type Panel = {
+	component: React.ReactNode;
+	headerMessages?: Messages;
+	parentKey?: string;
+	title: string;
+};
+
+type Panels = {
+	[key: React.Key]: Panel;
+};
+
+export function SidePanelWithDrilldown({
+	onSelectedPanelKeyChange,
+	panels,
+	selectedPanelKey,
+	...otherProps
+}: Props) {
+	return (
+		<SidePanel {...otherProps} aria-label={panels[selectedPanelKey]?.title}>
+			<div className="drilldown-inner">
+				{Object.entries(panels).map(([panelKey, panel]) => {
+					const active = selectedPanelKey === panelKey;
+					const parentPanelKey = panel.parentKey;
+
+					return (
+						<CSSTransition
+							aria-hidden={!active}
+							className={classnames('drilldown-item', {
+								'drilldown-current': active,
+							})}
+							in={active}
+							key={panelKey}
+							timeout={250}
+						>
+							<div className="drilldown-item-inner">
+								<SidePanel.Header
+									messages={panel.headerMessages}
+									{...(parentPanelKey && {
+										onBack: () =>
+											onSelectedPanelKeyChange(
+												parentPanelKey
+											),
+									})}
+								>
+									<SidePanel.Title>
+										{panel.title}
+									</SidePanel.Title>
+								</SidePanel.Header>
+
+								{panel.component}
+							</div>
+						</CSSTransition>
+					);
+				})}
+			</div>
+		</SidePanel>
+	);
+}
