3111bc5fce1ba050a36b4e661bf7ad0e6df6b34e
Mario Leandro <mario.leandro@liferay.com>
2025-11-13 09:38:48 -0300

    fix(@clayui/autocomplete): LPD-66892 Deprecating `active` prop in ClayAutocomplete.Item because it is no longer needed due to the use of `selectKeys` in the root component.

--- packages/clay-autocomplete/src/Item.tsx
+++ packages/clay-autocomplete/src/Item.tsx
@@ -47,6 +47,8 @@ export interface IProps
 
 	/**
 	 * Flag that indicates if item is selected.
+	 * @deprecated since v3.151.0 - uses the `selectedKeys` property on the
+	 * root component.
 	 */
 	active?: boolean;
 
