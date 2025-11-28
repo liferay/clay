6c4af2a60b6b8247b88df785836e13a2aee36c28
Gabriel Lima <gabriel.lima@liferay.com>
2025-11-28 10:33:38 -0300

    feat(@clayui/autocomplete): LPD-55597 Improve loading indicator appearance with controlled scrolling and use debouncing to stabilize network loading

--- packages/clay-autocomplete/src/useInfiniteScrolling.tsx
+++ packages/clay-autocomplete/src/useInfiniteScrolling.tsx
@@ -5,7 +5,7 @@
 
 import {AnnouncerAPI} from '@clayui/core';
 import LoadingIndicator from '@clayui/loading-indicator';
-import {sub} from '@clayui/shared';
+import {sub, useDebounce} from '@clayui/shared';
 import React, {RefObject, useEffect, useRef} from 'react';
 
 import {AutocompleteMessages} from './Autocomplete';
@@ -32,12 +32,16 @@ export function useInfiniteScrolling({
 	onLoadMore: externalOnLoadMore,
 }: IProps) {
 	const isLoading = Boolean(loadingState !== undefined && loadingState < 4);
+	const debouncedLoading = useDebounce(isLoading, 200);
 
 	const triggerRef = useRef<HTMLDivElement>(null);
 
-	const isInitialLoadAnnouncementPending = useRef<boolean>(true);
 	const isFetchingMoreData = useRef<boolean>(false);
-	const previousCount = useRef<number>();
+	const isInitialLoadAnnouncementPending = useRef<boolean>(true);
+	const isTriggerActive = useRef<boolean>(false);
+	const lastCountAnnounced = useRef<number | null>(null);
+	const lastPositionBeforeLoad = useRef<number | null>(null);
+	const lastTriggerCount = useRef<number | null>(null);
 
 	const onLoadMore = async () => {
 		isFetchingMoreData.current = externalOnLoadMore !== undefined;
@@ -47,58 +51,89 @@ export function useInfiniteScrolling({
 		isFetchingMoreData.current = false;
 	};
 
-	const announceInfiniteScrollingOnLoad = (count?: number) => {
-		const message =
-			count !== undefined
-				? count === 1
-					? sub(messages.infiniteScrollingOnLoad, [count])
-					: sub(messages.infiniteScrollingOnLoadPlural, [count])
-				: messages.infiniteScrollingOnLoadIndeterminate;
+	useEffect(() => {
+		if (active && isLoading && isFetchingMoreData.current) {
+			const message =
+				loadCount !== undefined
+					? loadCount === 1
+						? sub(messages.infiniteScrollingOnLoad, [loadCount])
+						: sub(messages.infiniteScrollingOnLoadPlural, [
+								loadCount,
+						  ])
+					: messages.infiniteScrollingOnLoadIndeterminate;
 
-		console.log(message);
-		announcer.current?.announce(message);
-	};
+			console.log(message);
+			announcer.current?.announce(message);
+			lastCountAnnounced.current = null;
 
-	const announceInfiniteScrollingOnLoaded = (count: number) => {
-		const singular = isInitialLoadAnnouncementPending.current
-			? messages.infiniteScrollingInitialLoad
-			: messages.infiniteScrollingOnLoaded;
-
-		const plural = isInitialLoadAnnouncementPending.current
-			? messages.infiniteScrollingInitialLoadPlural
-			: messages.infiniteScrollingOnLoadedPlural;
-
-		const message = sub(count === 1 ? singular : plural, [count]);
-
-		console.log(message);
-		announcer.current?.announce(message);
-	};
+			lastPositionBeforeLoad.current = menuRef.current?.scrollTop ?? null;
+			menuRef.current?.scrollTo({
+				behavior: 'instant',
+				top: menuRef.current?.scrollHeight,
+			});
+		}
+	}, [active, isLoading]);
 
 	useEffect(() => {
 		if (active) {
-			if (isLoading && isFetchingMoreData.current) {
-				announceInfiniteScrollingOnLoad(loadCount);
-				previousCount.current = undefined;
-			}
+			if (!isLoading && lastCountAnnounced.current !== currentCount) {
+				const singular = isInitialLoadAnnouncementPending.current
+					? messages.infiniteScrollingInitialLoad
+					: messages.infiniteScrollingOnLoaded;
 
-			if (!isLoading && previousCount.current !== currentCount) {
-				announceInfiniteScrollingOnLoaded(currentCount);
-				previousCount.current = currentCount;
+				const plural = isInitialLoadAnnouncementPending.current
+					? messages.infiniteScrollingInitialLoadPlural
+					: messages.infiniteScrollingOnLoadedPlural;
+
+				const message = sub(currentCount === 1 ? singular : plural, [
+					currentCount,
+				]);
+
+				console.log(message);
+				announcer.current?.announce(message);
+				lastCountAnnounced.current = currentCount;
+				isInitialLoadAnnouncementPending.current = false;
+
+				if (lastPositionBeforeLoad.current !== null) {
+					menuRef.current?.scrollTo({
+						behavior: 'instant',
+						top: lastPositionBeforeLoad.current,
+					});
+
+					lastPositionBeforeLoad.current = null;
+				}
 			}
+		} else {
+			isInitialLoadAnnouncementPending.current = true;
 		}
-
-		isInitialLoadAnnouncementPending.current = !active;
 	}, [active, isLoading]);
 
 	useEffect(() => {
 		if (active && triggerRef.current && menuRef.current) {
-			const callback: IntersectionObserverCallback = (entries) => {
-				if (entries[0]?.isIntersecting && loadingState !== 1) {
-					onLoadMore();
+			let timeoutId: NodeJS.Timeout | null = null;
+
+			const handleIntersect: IntersectionObserverCallback = (entries) => {
+				if (timeoutId) {
+					clearTimeout(timeoutId);
 				}
+
+				timeoutId = setTimeout(() => {
+					const isIntersecting = entries[0]?.isIntersecting ?? false;
+					const countHasChanged =
+						currentCount !== lastTriggerCount.current;
+
+					if (!isLoading && isIntersecting && countHasChanged) {
+						isTriggerActive.current = true;
+						lastTriggerCount.current = currentCount;
+
+						onLoadMore().finally(() => {
+							isTriggerActive.current = false;
+						});
+					}
+				}, 200);
 			};
 
-			const observer = new IntersectionObserver(callback, {
+			const observer = new IntersectionObserver(handleIntersect, {
 				root: menuRef.current,
 				threshold: 1.0,
 			});
@@ -107,11 +142,13 @@ export function useInfiniteScrolling({
 
 			return () => observer.disconnect();
 		}
-	}, [active, onLoadMore, loadingState]);
+	}, [active, isLoading, onLoadMore]);
 
 	const InfiniteScrollingTrigger = () => (
 		<div aria-hidden="true" className="mt-2" ref={triggerRef}>
-			{isLoading && <LoadingIndicator className="mb-2" size="sm" />}
+			{(isLoading || debouncedLoading) && (
+				<LoadingIndicator className="mb-2" size="sm" />
+			)}
 		</div>
 	);
 
